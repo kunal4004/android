1edc5249e766256a8ac6ef0b163cd62643e97d5a
WOOLWORTHS\W7099877 <bitbucket053>
2016-11-28 16:49:07 +0200

    My account designs. Loan withdrawal page design

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/MyAccountCardsActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/MyAccountCardsActivity.java
@@ -1,8 +1,10 @@
 package za.co.woolworths.financial.services.android.ui.activities;
 
+import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -15,11 +17,13 @@ import za.co.woolworths.financial.services.android.ui.views.WFragmentViewPager;
 import za.co.woolworths.financial.services.android.util.WCustomViewPager;
 
 import static android.R.attr.width;
+import static za.co.woolworths.financial.services.android.ui.activities.StoreLocatorActivity.toolbar;
 
 public class MyAccountCardsActivity extends AppCompatActivity {
 
     ViewPager pager;
     WFragmentViewPager fragmentPager;
+    public Toolbar toolbar;
 
 
 
@@ -27,6 +31,10 @@ public class MyAccountCardsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account_cards);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(null);
         pager = (ViewPager) findViewById(R.id.myAccountsCardPager);
         fragmentPager=(WFragmentViewPager) findViewById(R.id.fragmentpager);
         pager.setAdapter(new MyAccountsCardsAdapter(MyAccountCardsActivity.this));
