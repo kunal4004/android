6494372b1e5230e2453c9f2f8ff97bbf2ee7b06a
WOOLWORTHS\W7099877 <bitbucket053>
2016-11-30 12:23:31 +0200

    Contact us designs

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/WContactUsActivity.java
@@ -0,0 +1,15 @@
+package za.co.woolworths.financial.services.android.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.awfs.coordination.R;
+
+public class WContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us_activity);
+    }
+}
--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/WPersonalLoanFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/WPersonalLoanFragment.java
@@ -18,7 +18,7 @@ public class WPersonalLoanFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.logged_out_state_personal_loan, container, false);
+        View view=inflater.inflate(R.layout.cards_common_fragment, container, false);
 
 
         return view;    }
