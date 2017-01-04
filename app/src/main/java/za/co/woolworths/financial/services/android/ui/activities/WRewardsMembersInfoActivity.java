36232f7a61647c4570ad965742c41312e9da20dd
WOOLWORTHS\W7099877 <bitbucket053>
2017-01-04 17:07:26 +0200

    Wrewards UI desgin -Logged out Page -Loggedin not linked -overview fragment -info page

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/WRewardsMembersInfoActivity.java
@@ -0,0 +1,15 @@
+package za.co.woolworths.financial.services.android.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.awfs.coordination.R;
+
+public class WRewardsMembersInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wrewards_members_info_activity);
+    }
+}
