5da7d87ca6cf765302104a4cefded805cfeba158
DimitriJ <DimitriJ@wigroupinternational.com>
2017-01-21 13:39:32 +0200

    - Remove commit from WOP 647

--- app/src/main/java/za/co/woolworths/financial/services/android/util/PopWindowValidationMessage.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/PopWindowValidationMessage.java
@@ -1,9 +1,6 @@
 package za.co.woolworths.financial.services.android.util;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -15,9 +12,6 @@ import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 
 import com.awfs.coordination.R;
-
-import java.util.Locale;
-
 import za.co.woolworths.financial.services.android.ui.views.WButton;
 import za.co.woolworths.financial.services.android.ui.views.WTextView;
 
@@ -30,15 +24,12 @@ public class PopWindowValidationMessage {
     private Animation mPopEnterAnimation;
     private RelativeLayout mRelPopContainer;
     private RelativeLayout mRelRootContainer;
-    private double mLatitude;
-    private double mLongiude;
 
-
-    public enum OVERLAY_TYPE {CONFIDENTIAL, INSOLVENCY, INFO, EMAIL, ERROR, MANDATORY_FIELD, STORE_LOCATOR_DIRECTION}
+    public enum OVERLAY_TYPE {CONFIDENTIAL, INSOLVENCY, INFO, EMAIL, ERROR, MANDATORY_FIELD}
 
     public PopWindowValidationMessage(Context context) {
         this.mContext = context;
-        this.mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.mLayoutInflater = (LayoutInflater)mContext.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
     }
 
     public String getString(int id) {
@@ -138,40 +129,6 @@ public class PopWindowValidationMessage {
                             }
                         });
                 break;
-
-            case STORE_LOCATOR_DIRECTION:
-                Log.e("store_locator_direction","popLocator");
-                mView = mLayoutInflater.inflate(R.layout.popup_view, null);
-                popupWindowSetting(mView);
-                WTextView nativeMap = (WTextView) mView.findViewById(R.id.nativeGoogleMap);
-                WTextView cancel = (WTextView) mView.findViewById(R.id.cancel);
-                setAnimation();
-                mRelPopContainer.setAnimation(mFadeInAnimation);
-                mRelRootContainer.setAnimation(mPopEnterAnimation);
-                //touchToDismiss();
-                mRelPopContainer.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startExitAnimation();
-                    }
-                });
-                cancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startExitAnimation();
-                    }
-                });
-                nativeMap.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String uri = String.format(Locale.ENGLISH,"","http://maps.google.com/maps?daddr=%f,%f (%s)",getmLatitude(), getmLongiude(), "");
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                        intent.setClassName("com.google.android.apps.maps","com.google.android.maps.MapsActivity");
-                        mContext.startActivity(intent);
-                        dismissLayout();
-                    }
-                });
-                break;
         }
 
         return mDarkenScreen;
@@ -209,7 +166,7 @@ public class PopWindowValidationMessage {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                dismissLayout();
+                mDarkenScreen.dismiss();
             }
         });
         mRelRootContainer.startAnimation(animation);
@@ -230,21 +187,5 @@ public class PopWindowValidationMessage {
         if (mDarkenScreen != null) {
             mDarkenScreen.dismiss();
         }
-    }
-
-    public double getmLatitude() {
-        return mLatitude;
-    }
-
-    public void setmLatitude(double mLatitude) {
-        this.mLatitude = mLatitude;
-    }
-
-    public double getmLongiude() {
-        return mLongiude;
-    }
-
-    public void setmLongiude(double mLongiude) {
-        this.mLongiude = mLongiude;
     }
 }
