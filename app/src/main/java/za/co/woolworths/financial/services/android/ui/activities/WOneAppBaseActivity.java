c067b0d3c0cac9279404c542e9205bd39794587d
DimitriJ <DimitriJ@wigroupinternational.com>
2017-01-27 10:36:37 +0200

    - Add a white background to NestedScrollview to remove the default grey color when toolbar is animated - Use white back icon in MyAccountCard Activity. - Make scrolling smoother in my MyAccounts

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/WOneAppBaseActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/WOneAppBaseActivity.java
@@ -5,13 +5,18 @@ import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.os.Bundle;
 import android.view.animation.AccelerateInterpolator;
+
+
 import com.awfs.coordination.R;
 
 import java.util.ArrayList;
@@ -26,11 +31,12 @@ import za.co.woolworths.financial.services.android.ui.fragments.WProductsFragmen
 import za.co.woolworths.financial.services.android.ui.fragments.WRewardsFragment;
 import za.co.woolworths.financial.services.android.ui.fragments.WTodayFragment;
 import za.co.woolworths.financial.services.android.ui.views.WTextView;
+import za.co.woolworths.financial.services.android.util.HideActionBar;
 import za.co.woolworths.financial.services.android.util.JWTHelper;
 import za.co.woolworths.financial.services.android.util.SharePreferenceHelper;
 import za.co.woolworths.financial.services.android.util.Utils;
 
-public class WOneAppBaseActivity extends AppCompatActivity implements WFragmentDrawer.FragmentDrawerListener{
+public class WOneAppBaseActivity extends AppCompatActivity implements WFragmentDrawer.FragmentDrawerListener, HideActionBar {
 
     public static Toolbar mToolbar;
     public static AppBarLayout appbar;
@@ -39,6 +45,8 @@ public class WOneAppBaseActivity extends AppCompatActivity implements WFragmentD
     private List<Fragment> fragmentList;
     public static final String TAG = "WOneAppBaseActivity";
     private SharePreferenceHelper mSharePreferenceHelper;
+    private ActionBar mActionBar;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,19 +56,21 @@ public class WOneAppBaseActivity extends AppCompatActivity implements WFragmentD
         mSharePreferenceHelper = SharePreferenceHelper.getInstance(WOneAppBaseActivity.this);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(false);
-        getSupportActionBar().setDisplayShowTitleEnabled(false); // false for hiding the title from actoinBar
-        mToolbarTitle=(WTextView)findViewById(R.id.toolbar_title);
-        appbar=(AppBarLayout)findViewById(R.id.appbar);
+        mActionBar = getSupportActionBar();
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false); // false for hiding the title from actoinBar
+        mToolbarTitle = (WTextView) findViewById(R.id.toolbar_title);
+        appbar = (AppBarLayout) findViewById(R.id.appbar);
         fragmentList = new ArrayList<>();
 
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle(null);
+        //mActionBar.setVisibility(View.GONE);
+
         mToolbar.setNavigationIcon(R.drawable.ic_drawer_menu);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         drawerFragment = (WFragmentDrawer)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, mDrawerLayout, mToolbar);
         drawerFragment.setDrawerListener(this);
         displayView(Utils.DEFAULT_SELECTED_NAVIGATION_ITEM);
 
@@ -68,6 +78,8 @@ public class WOneAppBaseActivity extends AppCompatActivity implements WFragmentD
 //        WProgressDialogFragment editNameDialogFragment = WProgressDialogFragment.newInstance("Some Title");
 //        editNameDialogFragment.show(fm, "fragment_edit_name");
 //        editNameDialogFragment.setCancelable(false);
+
+
     }
 
     @Override
@@ -138,22 +150,35 @@ public class WOneAppBaseActivity extends AppCompatActivity implements WFragmentD
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        for(Fragment fragment : fragmentList){
+        for (Fragment fragment : fragmentList) {
             fragment.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    public JWTDecodedModel getJWTDecoded(){
+    public JWTDecodedModel getJWTDecoded() {
         JWTDecodedModel result = new JWTDecodedModel();
-        try{
+        try {
             SessionDao sessionDao = new SessionDao(WOneAppBaseActivity.this, SessionDao.KEY.USER_TOKEN).get();
-            if (sessionDao.value != null && !sessionDao.value.equals("")){
+            if (sessionDao.value != null && !sessionDao.value.equals("")) {
                 result = JWTHelper.decode(sessionDao.value);
-                mSharePreferenceHelper.save(result.email,"email");
+                mSharePreferenceHelper.save(result.email, "email");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             Log.e(TAG, e.getMessage());
         }
         return result;
+    }
+
+    @Override
+    public void hideActionBar(boolean actionbarIsVisible) {
+        mToolbar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBurgerButtonPressed() {
+        if (!mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            //drawer is open
+            mDrawerLayout.openDrawer(Gravity.LEFT); //OPEN Nav Drawer!
+        }
     }
 }
