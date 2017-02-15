afd8986fe3cbcc094b558fbcc96362a258e7ad53
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-02-15 12:31:14 +0200

    Capture invalid barcode displays invalid barcode error message

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/TransludentActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/TransludentActivity.java
@@ -1,8 +1,84 @@
 package za.co.woolworths.financial.services.android.ui.activities;
 
-/**
- * Created by dimitrij on 2017/02/15.
- */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
 
-public class TransludentActivity {
+import com.awfs.coordination.R;
+
+import za.co.woolworths.financial.services.android.ui.views.WButton;
+import za.co.woolworths.financial.services.android.util.Utils;
+
+public class TransludentActivity extends AppCompatActivity {
+
+    private RelativeLayout mRelRootContainer;
+    private Animation mPopEnterAnimation;
+    private RelativeLayout mRelPopContainer;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Utils.updateStatusBarBackground(this, R.color.black);
+        setContentView(R.layout.transparent_activity);
+
+        mRelRootContainer = (RelativeLayout) findViewById(R.id.relContainerRootMessage);
+        mRelPopContainer = (RelativeLayout) findViewById(R.id.relPopContainer);
+
+        setAnimation();
+
+        WButton wButton = (WButton) findViewById(R.id.btnOk);
+        wButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startExitAnimation();
+            }
+        });
+
+        mRelPopContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startExitAnimation();
+            }
+        });
+    }
+
+    private void startExitAnimation() {
+        TranslateAnimation animation = new TranslateAnimation(0, 0, 0, mRelRootContainer.getHeight());
+        animation.setFillAfter(true);
+        animation.setDuration(600);
+        animation.setAnimationListener(new TranslateAnimation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                overridePendingTransition(0, 0);
+            }
+        });
+        mRelRootContainer.startAnimation(animation);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(0, 0);
+    }
+
+    private void setAnimation() {
+        mPopEnterAnimation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.popup_enter);
+        mRelRootContainer.startAnimation(mPopEnterAnimation);
+
+    }
+
 }
