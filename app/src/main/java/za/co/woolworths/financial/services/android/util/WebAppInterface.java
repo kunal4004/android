5ff6fede64ff9ac69a985a7f92d5c469f96da9f7
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-02-17 17:58:07 +0200

    Product link fixed

--- app/src/main/java/za/co/woolworths/financial/services/android/util/WebAppInterface.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/WebAppInterface.java
@@ -3,7 +3,11 @@ package za.co.woolworths.financial.services.android.util;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
+
+import org.w3c.dom.Text;
 
 import za.co.woolworths.financial.services.android.ui.activities.ProductViewActivity;
 
@@ -19,9 +23,11 @@ public class WebAppInterface {
 
     @JavascriptInterface   // must be added for API 17 or higher
     public void showProducts(String id, String productName) {
+
         Intent openProductName = new Intent(mContext, ProductViewActivity.class);
-        openProductName.putExtra("searchProduct", id);
-        openProductName.putExtra("title", productName);
+        openProductName.putExtra("searchProduct", "");
+        openProductName.putExtra("title", id);
+        openProductName.putExtra("titleNav", productName);
         mContext.startActivity(openProductName);
         ((AppCompatActivity) mContext).overridePendingTransition(0, 0);
     }
