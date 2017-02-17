cb785c1b6bd6e471de373b0f4658238b38f583e1
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-02-17 15:39:33 +0200

    Remove nullpointercheck

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/ProductViewActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/ProductViewActivity.java
@@ -403,11 +403,9 @@ public class ProductViewActivity extends AppCompatActivity implements SelectedPr
     }
 
     private void getProductDetail(final String productId, final String skuId, final boolean closeActivity) {
-        if (TextUtils.isEmpty(searchItem)) {
-            try {
-                mGetProgressDialog.show(fm, "v");
-            } catch (NullPointerException ignored) {
-            }
+        try {
+            mGetProgressDialog.show(fm, "v");
+        } catch (NullPointerException ignored) {
         }
         ((WoolworthsApplication) getApplication()).getAsyncApi().getProductDetail(productId, skuId, new Callback<String>() {
             @Override
