68de4e4e9a7f8fca2fbfd28f71a9573536868736
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-02-22 10:05:47 +0200

    Fixed getActivity() is null on login/logout

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/AbstractFragmentListener.java
@@ -0,0 +1,45 @@
+package za.co.woolworths.financial.services.android.util;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class AbstractFragmentListener extends Fragment {
+
+    protected IActivityEnabledListener aeListener;
+
+    protected interface IActivityEnabledListener {
+        void onActivityEnabled(AppCompatActivity activity);
+    }
+
+    protected void getAvailableActivity(IActivityEnabledListener listener) {
+        if (getActivity() == null) {
+            aeListener = listener;
+
+        } else {
+            listener.onActivityEnabled((AppCompatActivity) getActivity());
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        if (aeListener != null) {
+            aeListener.onActivityEnabled((AppCompatActivity) activity);
+            aeListener = null;
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (aeListener != null) {
+            aeListener.onActivityEnabled((AppCompatActivity) context);
+            aeListener = null;
+        }
+    }
+}
