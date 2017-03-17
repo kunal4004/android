f9b7a7b4a188fc6764085645755aa15638864664
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-03-17 13:36:21 +0400

    Replace ImageView by SimpleDraweeImage

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/FeaturedPromotionsAdapter.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/FeaturedPromotionsAdapter.java
@@ -6,9 +6,9 @@ import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.awfs.coordination.R;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.List;
 
@@ -19,11 +19,12 @@ import za.co.woolworths.financial.services.android.util.DrawImage;
 public class FeaturedPromotionsAdapter extends PagerAdapter {
     public Activity mContext;
     private List<Promotion> promotions;
-    public FeaturedPromotionsAdapter(Activity mContext, List<Promotion> promotions)
-    {
-        this.mContext=mContext;
-        this.promotions=promotions;
+
+    public FeaturedPromotionsAdapter(Activity mContext, List<Promotion> promotions) {
+        this.mContext = mContext;
+        this.promotions = promotions;
     }
+
     @Override
     public int getCount() {
         return promotions.size();
@@ -32,15 +33,15 @@ public class FeaturedPromotionsAdapter extends PagerAdapter {
     @Override
     public boolean isViewFromObject(View view, Object object) {
 
-         return view.equals(object);
+        return view.equals(object);
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, final int position) {
-        View cView=mContext.getLayoutInflater().inflate(R.layout.featured_prmotion_list_item,container,false);
-        ImageView image=(ImageView)cView.findViewById(R.id.promotionImage);
+        View cView = mContext.getLayoutInflater().inflate(R.layout.featured_prmotion_list_item, container, false);
+        SimpleDraweeView image = (SimpleDraweeView) cView.findViewById(R.id.promotionImage);
         DrawImage drawImage = new DrawImage(container.getContext());
-        drawImage.displayImage(image,promotions.get(position).image);
+        drawImage.displayImage(image, promotions.get(position).image);
         container.addView(cView);
         cView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +49,7 @@ public class FeaturedPromotionsAdapter extends PagerAdapter {
                 Intent openProductName = new Intent(mContext, ProductViewGridActivity.class);
                 openProductName.putExtra("searchProduct", "");
                 openProductName.putExtra("title", promotions.get(position).path);
-                openProductName.putExtra("titleNav",mContext.getResources().getString(R.string.featured_promotions) );
+                openProductName.putExtra("titleNav", mContext.getResources().getString(R.string.featured_promotions));
                 mContext.startActivity(openProductName);
                 mContext.overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
             }
@@ -60,6 +61,7 @@ public class FeaturedPromotionsAdapter extends PagerAdapter {
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((View) object);
     }
+
     @Override
     public void restoreState(Parcelable state, ClassLoader loader) {
         super.restoreState(state, loader);
@@ -69,6 +71,7 @@ public class FeaturedPromotionsAdapter extends PagerAdapter {
     public Parcelable saveState() {
         return null;
     }
+
     @Override
     public float getPageWidth(int position) {
         return 0.96f;
