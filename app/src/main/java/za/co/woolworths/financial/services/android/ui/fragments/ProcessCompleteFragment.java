36691ee97990e907acc23ee5d01d21751dd0a09a
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-11-07 10:17:33 +0400

    Close ProcessCompleteFragment by sliding down the activity

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/ProcessCompleteFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/ProcessCompleteFragment.java
@@ -1,5 +1,6 @@
 package za.co.woolworths.financial.services.android.ui.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 
 import com.awfs.coordination.R;
 
+import za.co.woolworths.financial.services.android.ui.activities.CLIPhase2Activity;
 import za.co.woolworths.financial.services.android.ui.views.WButton;
 import za.co.woolworths.financial.services.android.util.controller.CLIFragment;
 
@@ -28,8 +30,8 @@ public class ProcessCompleteFragment extends CLIFragment implements View.OnClick
 	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		initUI(view);
+		mCliStepIndicatorListener.onStepSelected(5);
 	}
-
 
 	private void initUI(View view) {
 		WButton btnProcessComplete = (WButton) view.findViewById(R.id.btnProcessComplete);
@@ -40,7 +42,11 @@ public class ProcessCompleteFragment extends CLIFragment implements View.OnClick
 	public void onClick(View v) {
 		switch (v.getId()) {
 			case R.id.btnProcessComplete:
-
+				Activity activity = getActivity();
+				if (activity != null && activity instanceof CLIPhase2Activity) {
+					activity.finish();
+					activity.overridePendingTransition(R.anim.stay, R.anim.slide_down_anim);
+				}
 				break;
 		}
 	}
