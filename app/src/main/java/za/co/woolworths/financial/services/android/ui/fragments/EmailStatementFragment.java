27f1b236f8717d265da05eddf055b75811871bc4
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-11-29 21:20:00 +0400

    Success email fragment

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/EmailStatementFragment.java
@@ -0,0 +1,25 @@
+package za.co.woolworths.financial.services.android.ui.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.awfs.coordination.R;
+
+public class EmailStatementFragment extends Fragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.estatement_confirmation_layout, container, false);
+		return view;
+	}
+
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+	}
+}
