71132830687e3d0b9a8e98ac202e1bc31144406f
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2017-12-12 14:44:16 +0400

    Added netMonthlyIncome

--- app/src/main/java/za/co/woolworths/financial/services/android/models/dto/CreateOfferRequest.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/CreateOfferRequest.java
@@ -6,7 +6,7 @@ public class CreateOfferRequest {
 	private boolean maxCreditRequested = true;
 	private int productOfferingId;
 	private int grossMonthlyIncome;
-	private int nettMonthlyIncome;
+	private int netMonthlyIncome;
 	private int additionalIncomeAmount;
 	private int mortgagePaymentAmount;
 	private int rentalPaymentAmount;
@@ -14,11 +14,11 @@ public class CreateOfferRequest {
 	private int totalCreditExpenseAmount;
 	private int otherExpenseAmount;
 
-	public CreateOfferRequest(int productOfferingId, int grossMonthlyIncome, int nettMonthlyIncome, int additionalIncomeAmount, int mortgagePaymentAmount, int rentalPaymentAmount, int maintenanceExpenseAmount, int totalCreditExpenseAmount, int otherExpenseAmount) {
+	public CreateOfferRequest(int productOfferingId, int grossMonthlyIncome, int netMonthlyIncome, int additionalIncomeAmount, int mortgagePaymentAmount, int rentalPaymentAmount, int maintenanceExpenseAmount, int totalCreditExpenseAmount, int otherExpenseAmount) {
 		super();
 		this.productOfferingId = productOfferingId;
 		this.grossMonthlyIncome = grossMonthlyIncome;
-		this.nettMonthlyIncome = nettMonthlyIncome;
+		this.netMonthlyIncome = netMonthlyIncome;
 		this.additionalIncomeAmount = additionalIncomeAmount;
 		this.mortgagePaymentAmount = mortgagePaymentAmount;
 		this.rentalPaymentAmount = rentalPaymentAmount;
