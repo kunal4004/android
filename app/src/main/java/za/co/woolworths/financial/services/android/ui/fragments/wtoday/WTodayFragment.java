b2f7c421895d5900b8d5b2c6cff959cf785aa934
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-01-11 11:00:01 +0400

    create wtoday package and add wtoday classes

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/wtoday/WTodayFragment.java
@@ -0,0 +1,90 @@
+package za.co.woolworths.financial.services.android.ui.fragments.wtoday;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
+
+import com.awfs.coordination.R;
+
+import za.co.woolworths.financial.services.android.models.WoolworthsApplication;
+import za.co.woolworths.financial.services.android.util.ConnectionDetector;
+import za.co.woolworths.financial.services.android.util.ErrorHandlerView;
+import za.co.woolworths.financial.services.android.util.WebAppInterface;
+
+public class WTodayFragment extends Fragment {
+	WebView webView;
+	ErrorHandlerView mErrorHandlerView;
+	WTodayNavigator mWTodayNavigator;
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		setRetainInstance(true);
+		return inflater.inflate(R.layout.wtoday_fragment, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		Activity activity = getActivity();
+		if (activity != null) {
+			mWTodayNavigator = (WTodayNavigator) activity;
+			mErrorHandlerView = new ErrorHandlerView(activity, (RelativeLayout) view.findViewById(R.id.no_connection_layout));
+			initWebView(view);
+			retryConnect(view);
+		}
+	}
+
+	@SuppressLint("SetJavaScriptEnabled")
+	private void initWebView(View view) {
+		webView = view.findViewById(R.id.wtoday_fragment_webview);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+		webView.addJavascriptInterface(new WebAppInterface(getActivity()), "Android");
+		webView.loadUrl(WoolworthsApplication.getWwTodayURI());
+		webView.setWebViewClient(new WebViewClient() {
+			@TargetApi(android.os.Build.VERSION_CODES.M)
+			@Override
+			public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+				super.onReceivedError(view, request, error);
+				mErrorHandlerView.webViewBlankPage(view);
+				mErrorHandlerView.networkFailureHandler(error.toString());
+			}
+
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				return super.shouldOverrideUrlLoading(view, url);
+			}
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+				mErrorHandlerView.webViewBlankPage(webView);
+				mErrorHandlerView.networkFailureHandler(description);
+			}
+
+		});
+	}
+
+	private void retryConnect(View view) {
+		view.findViewById(R.id.btnRetry).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (new ConnectionDetector().isOnline(getActivity())) {
+					mWTodayNavigator.switchToFragment(0);
+				}
+			}
+		});
+	}
+}
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/wtoday/WTodayNavigator.java
@@ -0,0 +1,5 @@
+package za.co.woolworths.financial.services.android.ui.fragments.wtoday;
+
+public interface WTodayNavigator {
+	void switchToFragment(int position);
+}
