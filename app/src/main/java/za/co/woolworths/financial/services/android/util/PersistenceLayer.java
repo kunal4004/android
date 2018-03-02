8e74305e8560618e39a0b0a87e2ae2d672fe700c
Eesa Jacobs <eesaj@wigroup.co.za>
2018-03-02 14:50:54 +0200

    Fixes issue where Push Notifications aren't working in Release Mode

--- app/src/main/java/za/co/woolworths/financial/services/android/util/PersistenceLayer.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/PersistenceLayer.java
@@ -83,7 +83,7 @@ public class PersistenceLayer extends SQLiteOpenHelper {
 
         if(cursor.getCount() == 0){//consider this as a failure as no rows were updated
             db.close();
-            throw new SQLiteException("Updated row count was 0. This is considered as a failed 'SQL UPDATE' transaction.");
+            return;
         }
         db.close();
     }
@@ -105,9 +105,8 @@ public class PersistenceLayer extends SQLiteOpenHelper {
 
         if(cursor.getCount() == 0){//consider this as a failure as no rows were updated
             db.close();
-            return result;
-            //throw new SQLiteException("Updated row count was 0. This is considered as a failed 'SQL UPDATE' transaction.");
-        }
+			return result;
+		}
 
         Log.d(TAG, "" + cursor.getCount());
 
