3f79fb826683d713fd73e6b3411ec85d9220f4a1
WOOLWORTHS\W7099877 <bitbucket053>
2018-03-12 09:36:36 +0200

    no message

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/ShoppingListItemsAdapter.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/ShoppingListItemsAdapter.java
@@ -1,8 +1,42 @@
-package za.co.woolworths.financial.services.android.ui.adapters.bottom_navigation;
+package za.co.woolworths.financial.services.android.ui.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.awfs.coordination.R;
+
+import za.co.woolworths.financial.services.android.ui.fragments.shoppinglist.ShoppingListNavigator;
 
 /**
  * Created by W7099877 on 2018/03/09.
  */
 
-public class ShoppingListItemsAdapter {
+public class ShoppingListItemsAdapter extends RecyclerView.Adapter<ShoppingListItemsAdapter.ViewHolder> {
+
+
+	@Override
+	public ShoppingListItemsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		return new ShoppingListItemsAdapter.ViewHolder(LayoutInflater.from(parent.getContext())
+				.inflate(R.layout.shopping_list_commerce_item, parent, false));
+	}
+
+	@Override
+	public void onBindViewHolder(ShoppingListItemsAdapter.ViewHolder holder, int position) {
+
+	}
+
+
+	@Override
+	public int getItemCount() {
+		return 5;
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder{
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+		}
+	}
 }
--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/listitems/ShoppingListItemsFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/listitems/ShoppingListItemsFragment.java
@@ -3,12 +3,16 @@ package za.co.woolworths.financial.services.android.ui.fragments.shoppinglist.li
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.awfs.coordination.R;
 import com.awfs.coordination.BR;
 import com.awfs.coordination.databinding.ShoppingListItemsFragmentBinding;
+
+import za.co.woolworths.financial.services.android.ui.adapters.ShoppingListAdapter;
+import za.co.woolworths.financial.services.android.ui.adapters.ShoppingListItemsAdapter;
 import za.co.woolworths.financial.services.android.ui.base.BaseFragment;
 
 /**
@@ -17,6 +21,7 @@ import za.co.woolworths.financial.services.android.ui.base.BaseFragment;
 
 public class ShoppingListItemsFragment extends BaseFragment<ShoppingListItemsFragmentBinding,ShoppingListItemsViewModel> implements ShoppingListItemsNavigator {
 	private ShoppingListItemsViewModel shoppingListItemsViewModel;
+	private ShoppingListItemsAdapter shoppingListItemsAdapter;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +37,7 @@ public class ShoppingListItemsFragment extends BaseFragment<ShoppingListItemsFra
 		showBackNavigationIcon(true);
 		setTitle("general Fashion");
 		showToolbar();
+		loadShoppingListItems();
 	}
 
 	@Override
@@ -48,6 +54,14 @@ public class ShoppingListItemsFragment extends BaseFragment<ShoppingListItemsFra
 	@Override
 	public int getLayoutId() {
 		return R.layout.shopping_list_items_fragment;
+	}
+
+	public void loadShoppingListItems() {
+		shoppingListItemsAdapter=new ShoppingListItemsAdapter();
+		LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+		mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+		getViewDataBinding().rcvShoppingListItems.setLayoutManager(mLayoutManager);
+		getViewDataBinding().rcvShoppingListItems.setAdapter(shoppingListItemsAdapter);
 	}
 
 }
