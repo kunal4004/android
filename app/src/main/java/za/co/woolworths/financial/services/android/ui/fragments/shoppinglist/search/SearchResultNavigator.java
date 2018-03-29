c7d83e8d2d194279c5fdc96100f22c85c00e31e3
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-03-29 11:07:47 +0200

    Progressbar is displayed on check state changed for clothing type product

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/search/SearchResultNavigator.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/search/SearchResultNavigator.java
@@ -2,10 +2,8 @@ package za.co.woolworths.financial.services.android.ui.fragments.shoppinglist.se
 
 import java.util.List;
 
-import za.co.woolworths.financial.services.android.models.dto.Data;
 import za.co.woolworths.financial.services.android.models.dto.ProductList;
 import za.co.woolworths.financial.services.android.models.dto.Response;
-import za.co.woolworths.financial.services.android.models.dto.ShoppingList;
 import za.co.woolworths.financial.services.android.models.dto.ShoppingListItem;
 import za.co.woolworths.financial.services.android.models.dto.WProduct;
 
@@ -44,7 +42,7 @@ public interface SearchResultNavigator {
 
 	void onAddToListLoadComplete(List<ShoppingListItem>  shoppingLists);
 
-	void onCheckedItem(ProductList selectedProduct);
+	void onCheckedItem(ProductList selectedProduct, boolean viewIsLoading);
 
 	void onLoadStart();
 
