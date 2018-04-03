93ca3e09e1414c442571fd4c70b609fa065671da
WOOLWORTHS\W7099877 <bitbucket053>
2018-04-03 13:57:41 +0200

    - Arrows will available when message and list counters are ZERO

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsFragment.java
@@ -803,6 +803,7 @@ public class MyAccountsFragment extends BaseFragment<MyAccountsFragmentBinding, 
 	@Override
 	public void onMessageResponse(MessageResponse messageResponse) {
 		if (messageResponse.unreadCount > 0) {
+			hideView(getViewDataBinding().messagesRightArrow);
 			showView(messageCounter);
 			int unreadCount = messageResponse.unreadCount;
 			if (TextUtils.isEmpty(String.valueOf(unreadCount)))
@@ -814,6 +815,7 @@ public class MyAccountsFragment extends BaseFragment<MyAccountsFragmentBinding, 
 			Utils.removeBadgeCounter(getActivity());
 			getBottomNavigator().addBadge(INDEX_ACCOUNT, 0);
 			hideView(messageCounter);
+			showView(getViewDataBinding().messagesRightArrow);
 		}
 	}
 
@@ -821,10 +823,12 @@ public class MyAccountsFragment extends BaseFragment<MyAccountsFragmentBinding, 
 	public void onShoppingListsResponse(ShoppingListsResponse shoppingListsResponse) {
 		this.shoppingListsResponse = shoppingListsResponse;
 		if (shoppingListsResponse.lists != null && shoppingListsResponse.lists.size() > 0) {
+			hideView(getViewDataBinding().myListRightArrow);
 			showView(getViewDataBinding().listsCounter);
 			getViewDataBinding().listsCounter.setText(String.valueOf(shoppingListsResponse.lists.size()));
 		} else {
 			hideView(getViewDataBinding().listsCounter);
+			showView(getViewDataBinding().myListRightArrow);
 		}
 	}
 
