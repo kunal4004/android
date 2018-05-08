bbe25e8acb6ef67509b14b435edc0a5681ffa903
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-05-08 08:25:30 +0400

    Added fulfillmentType

--- app/src/main/java/za/co/woolworths/financial/services/android/models/dto/WProductDetail.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/WProductDetail.java
@@ -7,26 +7,27 @@ import java.util.List;
 
 public class WProductDetail {
 
-    public String productId;
-    public String productName;
-    public String externalImageRef;
-    public String categoryName;
-    public String categoryId;
-    public String imagePath;
-    public Float fromPrice;
-    public String sku;
-    public String saveText;
-    public String checkOutLink;
-    //public AuxiliaryImages auxiliaryImages;
-    public String productType;
-    // public PromotionImages promotionImages;
-    public String longDescription;
-    //@SerializedName("otherSkus")
-    @SerializedName("otherSkus")
-    @Expose
-    public List<OtherSkus> otherSkus = null;
-    @SerializedName("promotionImages")
-    @Expose
-    public PromotionImages promotionImages;
+	public String productId;
+	public String productName;
+	public String externalImageRef;
+	public String categoryName;
+	public String categoryId;
+	public String imagePath;
+	public Float fromPrice;
+	public String sku;
+	public String saveText;
+	public String fulfillmentType;
+	public String checkOutLink;
+	//public AuxiliaryImages auxiliaryImages;
+	public String productType;
+	// public PromotionImages promotionImages;
+	public String longDescription;
+	//@SerializedName("otherSkus")
+	@SerializedName("otherSkus")
+	@Expose
+	public List<OtherSkus> otherSkus = null;
+	@SerializedName("promotionImages")
+	@Expose
+	public PromotionImages promotionImages;
 
 }
