43ccf57a2af25821387a2e11dc51ea1818365612
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-05-15 12:44:32 +0400

    Show toolbar when fragment is resumed

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/search/SearchResultFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/search/SearchResultFragment.java
@@ -743,4 +743,10 @@ public class SearchResultFragment extends BaseFragment<GridLayoutBinding, Search
 			});
 		}
 	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		showSearchResultToolbar();
+	}
 }
