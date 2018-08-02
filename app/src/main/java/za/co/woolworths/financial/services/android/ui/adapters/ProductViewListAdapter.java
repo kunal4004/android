f7b0068bfd78c5aa3f743f7530f1e73fcd1700f5
Eesa Jacobs <eesaj@wigroup.co.za>
2018-08-02 19:26:50 +0200

    https://wigroup2.atlassian.net/browse/WOP-3217

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/ProductViewListAdapter.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/ProductViewListAdapter.java
@@ -65,6 +65,12 @@ public class ProductViewListAdapter extends RecyclerView.Adapter<RecyclerView.Vi
 
 			if (!isEmpty(saveText))
 				vh.tvSaveText.setText(saveText);
+			else{
+				final String currentText = vh.tvSaveText.getText().toString();
+				if (currentText != null && !currentText.isEmpty()){
+					vh.tvSaveText.setText("");
+				}
+			}
 
 			ArrayList<Double> priceList = new ArrayList<>();
 			for (OtherSkus os : productList.otherSkus) {
