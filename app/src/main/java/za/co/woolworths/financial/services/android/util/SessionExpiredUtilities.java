e009be678084aeb04ca92afe44be89db333d7e9b
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-08-08 14:45:05 +0400

    Create custom popup dialog message using DialogFragment

--- app/src/main/java/za/co/woolworths/financial/services/android/util/SessionExpiredUtilities.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/SessionExpiredUtilities.java
@@ -1,23 +1,18 @@
 package za.co.woolworths.financial.services.android.util;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-
-import za.co.woolworths.financial.services.android.ui.activities.CustomPopUpWindow;
-import za.co.woolworths.financial.services.android.ui.activities.SSOActivity;
 
 public enum SessionExpiredUtilities {
 	INSTANCE;
 
 	public void showSessionExpireDialog(Activity activity) {
-		Utils.clearSQLLiteSearchHistory(activity);
-		Intent openMsg = new Intent(activity, CustomPopUpWindow.class);
-		Bundle args = new Bundle();
-		args.putSerializable("key", CustomPopUpWindow.MODAL_LAYOUT.SESSION_EXPIRED);
-		args.putString("description", SessionUtilities.getInstance().getSTSParameters());
-		openMsg.putExtras(args);
-		activity.startActivityForResult(openMsg, SSOActivity.SSOActivityResult.LAUNCH.rawValue());
-		activity.overridePendingTransition(0, 0);
+//		Utils.clearSQLLiteSearchHistory(activity);
+//		Intent openMsg = new Intent(activity, CustomPopUpWindow.class);
+//		Bundle args = new Bundle();
+//		args.putSerializable("key", CustomPopUpWindow.MODAL_LAYOUT.SESSION_EXPIRED);
+//		args.putString("description", SessionUtilities.getInstance().getSTSParameters());
+//		openMsg.putExtras(args);
+//		activity.startActivity(openMsg);
+//		activity.overridePendingTransition(0, 0);
 	}
 }
