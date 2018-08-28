5f5f26addba85667e88ab941b0353269cacf5e91
Eesa Jacobs <eesaj@wigroup.co.za>
2018-08-28 16:41:46 +0200

    Simplest Unit Test ever!

---
+++ app/src/test/java/za/co/woolworths/financial/services/android/SimpleTest.kt
@@ -0,0 +1,12 @@
+package za.co.woolworths.financial.services.android
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SimpleTest {
+
+    @Test
+    fun simpleTestAssertTrue(){
+        assertEquals(true, true)
+    }
+}
---
+++ app/src/test/java/za/co/woolworths/financial/services/android/za/co/woolworths/financial/services/android/ui/activities/StartupActivityTest.kt
@@ -0,0 +1,4 @@
+package za.co.woolworths.financial.services.android.za.co.woolworths.financial.services.android.ui.activities
+
+class StartupActivityTest {
+}
