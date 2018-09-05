56113dd7968952256a8fcc9efd6fe2753392c80b
Eesa Jacobs <eesaj@wigroup.co.za>
2018-09-05 11:45:58 +0200

    Removing ApiResponseHandler

--- app/src/main/java/za/co/woolworths/financial/services/android/models/dao/ApiResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package za.co.woolworths.financial.services.android.models.dao;
-
-import za.co.woolworths.financial.services.android.util.HttpAsyncTask;
-
-public interface ApiResponseHandler {
-
-	void success(Object responseObject);
-	void failure(String errorMessage, HttpAsyncTask.HttpErrorCode httpErrorCode);
-}
