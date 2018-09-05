7336ccbeb811311d0478fa5a212fab69d448579d
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-09-05 12:12:45 +0400

    Remove try catch block

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/ShoppingListFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/ShoppingListFragment.java
@@ -199,11 +199,9 @@ public class ShoppingListFragment extends BaseFragment<ShoppinglistFragmentBindi
 				displayErrorMessage("");
 				break;
 		}
-		try {
-			Utils.deliveryLocationEnabled(getActivity(), true, rlLocationSelectedLayout);
-		} catch (NullPointerException ignored) {
-
-		}
+		Activity activity = getActivity();
+		if (activity == null) return;
+		Utils.deliveryLocationEnabled(activity, true, rlLocationSelectedLayout);
 	}
 
 	@Override
