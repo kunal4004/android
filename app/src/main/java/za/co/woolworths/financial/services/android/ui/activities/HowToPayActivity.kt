70413c15347e1b81370af2ab8ff449e1fa3a1680
WOOLWORTHS\W7099877 <chandrasekhartangudu@woolworths.co.za>
2018-09-10 12:53:55 +0200

    how to pay design

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/HowToPayActivity.kt
@@ -0,0 +1,13 @@
+package za.co.woolworths.financial.services.android.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.awfs.coordination.R
+
+class HowToPayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.accounts_how_to_pay)
+    }
+}
