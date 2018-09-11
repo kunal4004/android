a9f5aa4030b3cc5fd897f1db39ffde8ab9e800a2
WOOLWORTHS\W7099877 <chandrasekhartangudu@woolworths.co.za>
2018-09-11 16:41:26 +0200

    - now how to pay data dynamic

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/HowToPayActivity.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/HowToPayActivity.kt
@@ -5,10 +5,17 @@ import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import com.awfs.coordination.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.accounts_how_to_pay.*
+import za.co.woolworths.financial.services.android.models.dto.Account
+import za.co.woolworths.financial.services.android.models.dto.PaymentMethod
 import za.co.woolworths.financial.services.android.util.Utils
+import za.co.woolworths.financial.services.android.ui.views.WTextView
 
 class HowToPayActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var accountDetails: Account
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +25,7 @@ class HowToPayActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun initViews() {
+        this.accountDetails = Gson().fromJson(intent.getStringExtra("account"), Account::class.java)
         btnClose.setOnClickListener(this)
         loadPaymentOptions()
         loadAccountDetails()
@@ -34,22 +42,34 @@ class HowToPayActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onBackPressed() {
         finish()
+        overridePendingTransition(R.anim.stay, R.anim.slide_down_anim)
     }
 
     private fun loadAccountDetails() {
         howToPayAccountDetails.removeAllViews()
         val inflater: LayoutInflater = LayoutInflater.from(this)
-        for (i in 1..5) {
+        var paymentDetails: Map<String, String> = Gson().fromJson(accountDetails.bankingDetails, object : TypeToken<Map<String, String>>() {}.type)
+
+        for (i in paymentDetails) {
             val v: View = inflater.inflate(R.layout.how_to_pay_account_details_list_item, howToPayAccountDetails, false)
+            var paymentName: WTextView = v.findViewById(R.id.paymentName)
+            var paymentValue: WTextView = v.findViewById(R.id.paymentvalue)
+            paymentName.text = i.key
+            paymentValue.text = i.value
             howToPayAccountDetails.addView(v)
         }
     }
 
     private fun loadPaymentOptions() {
+        var paymentMethods: List<PaymentMethod> = accountDetails.paymentMethods
         howToPayOptionsList.removeAllViews()
         val inflater: LayoutInflater = LayoutInflater.from(this)
-        for (i in 1..4) {
+        paymentMethods.forEachIndexed { index, paymentMethod ->
             val v: View = inflater.inflate(R.layout.how_to_pay_options_list_item, howToPayOptionsList, false)
+            var count: WTextView = v.findViewById(R.id.count)
+            var howToPayOption: WTextView = v.findViewById(R.id.howToPayOption)
+            count.text = (index + 1).toString()
+            howToPayOption.text = paymentMethod.description
             howToPayOptionsList.addView(v)
         }
     }
