d8a552e633b2cb9bf33f44bc8b817b0999a5baff
WOOLWORTHS\W7099877 <chandrasekhartangudu@woolworths.co.za>
2018-09-11 10:45:05 +0200

    - Accounts model updated

--- app/src/main/java/za/co/woolworths/financial/services/android/models/dto/Account.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/Account.java
@@ -1,15 +1,28 @@
 package za.co.woolworths.financial.services.android.models.dto;
 
+import com.google.gson.JsonElement;
+
+import java.util.List;
+
 public class Account {
-	public int productOfferingId;
-	public int creditLimit;
-	public int currentBalance;
-	public int availableFunds;
-	public int minimumAmountDue;
-	public String paymentDueDate;
-	public int minDrawDownAmount;
-	public int rpCreditLimitThreshold;
-	public boolean retrievalError;
-	public String productGroupCode;
-	public String accountNumberBin;
+    public int productOfferingId;
+    public int creditLimit;
+    public int currentBalance;
+    public int availableFunds;
+    public int minimumAmountDue;
+    public String paymentDueDate;
+    public int minDrawDownAmount;
+    public int rpCreditLimitThreshold;
+    public boolean retrievalError;
+    public String productGroupCode;
+    public String accountNumberBin;
+    public String productOfferingStatus;
+    public String productOfferingGoodStanding;
+    public String totalAmountDue;
+    public String amountOverdue;
+    public String insuranceCovered;
+    public List<PaymentMethod> paymentMethods;
+    public JsonElement bankingDetails;
+    public DebitOrder debitOrder;
+
 }
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/DebitOrder.java
@@ -0,0 +1,7 @@
+package za.co.woolworths.financial.services.android.models.dto;
+
+public class DebitOrder {
+    public boolean debitOrderActive;
+    public String debitOrderDeductionDay;
+    public float debitOrderProjectedAmount;
+}
