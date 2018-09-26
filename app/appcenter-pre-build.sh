1a476ef2b433405403f70f6d8487b2fb95f4ccb6
Eesa Jacobs <eesaj@wigroup.co.za>
2018-09-26 14:06:11 +0200

    Attempting to use BUILD_NUMBER from MS App Center for versionCode

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-echo "$APPCENTER_BUILD_ID"
-echo "$APPCENTER_ANDROID_VARIANT"
-echo "$APPCENTER_ANDROID_MODULE"
+
+echo "BUILD_NUMBER: $BUILD_NUMBER"
+# Extract build number from latest HockeyApp build.
+BUILD_NUMBER=$(curl -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions | sed -e 's/^.*"app_versions":\[{"version":"\([^"]*\)".*$/\1/')
+
+echo "BUILD_NUMBER: $BUILD_NUMBER"
+BUILD_NUMBER=$(($BUILD_NUMBER + 1))
+echo "NEXT_BUILD_NUMBER: BUILD_NUMBER"
