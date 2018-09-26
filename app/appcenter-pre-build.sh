4c8a2400651195a0a5ea54f5386fa85e99476494
Eesa Jacobs <eesaj@wigroup.co.za>
2018-09-26 14:26:03 +0200

    let's try this again

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -6,5 +6,5 @@ echo "BUILD_NUMBER: $BUILD_NUMBER"
 BUILD_NUMBER=$(curl -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions | sed -e 's/^.*"app_versions":\[{"version":"\([^"]*\)".*$/\1/')
 
 echo "BUILD_NUMBER: $BUILD_NUMBER"
-BUILD_NUMBER=$(($BUILD_NUMBER + 1))
+export BUILD_NUMBER=$(($BUILD_NUMBER + 1))
 echo "NEXT_BUILD_NUMBER: $BUILD_NUMBER"
