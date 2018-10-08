2051c02fef5a61eace0d4afd1d339a73cf11fa4b
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-10-08 15:21:10 +0400

    BPI - Create BPI Overview Fragment and Adapter

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/BPIOverviewAdapter.kt
@@ -0,0 +1,74 @@
+package za.co.woolworths.financial.services.android.ui.adapters
+
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.bpi_overview_row.view.*
+import za.co.woolworths.financial.services.android.models.dto.BPIOverview
+import za.co.woolworths.financial.services.android.models.dto.InsuranceType
+import za.co.woolworths.financial.services.android.util.setOverviewConstraint
+
+internal class BPIOverviewAdapter(private val bpiOverviewList: ArrayList<BPIOverview>, val onBPIAdapterClickListener: OnBPIAdapterClickListener)
+    : RecyclerView.Adapter<BPIOverviewAdapter.ViewHolder>() {
+
+    companion object {
+        var mOnBPIOverviewAdapter: OnBPIAdapterClickListener? = null
+    }
+
+    interface OnBPIAdapterClickListener {
+        fun onItemViewClicked(bpiOverview: BPIOverview)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BPIOverviewAdapter.ViewHolder {
+        val v: View = LayoutInflater.from(parent.context).inflate(R.layout.bpi_overview_row, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: BPIOverviewAdapter.ViewHolder, position: Int) {
+        val bpiOverview: BPIOverview = bpiOverviewList[position]
+        val insuranceType: InsuranceType = bpiOverview.insuranceType!!
+        mOnBPIOverviewAdapter = onBPIAdapterClickListener
+        holder.bindItems(bpiOverview)
+        holder.verticalBarBackground(insuranceType)
+        holder.itemIsCovered(insuranceType)
+        holder.onItemClick(bpiOverview)
+    }
+
+    override fun getItemCount(): Int {
+        return bpiOverviewList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(bpiOverview: BPIOverview) {
+            itemView.clBalanceOverview.setOverviewConstraint(adapterPosition, R.dimen.seventeen_dp, R.dimen.sixteen_dp)
+            itemView.tvTitle.text = bpiOverview.insuranceType!!.description
+            itemView.tvDescription.text = bpiOverview.overviewDescription
+            itemView.imOverViewDescImage.setImageResource(bpiOverview.overviewDrawable!!)
+        }
+
+        fun verticalBarBackground(insuranceType: InsuranceType) {
+            itemView.spVerticalOrangeDeco.setBackgroundColor(if (insuranceType.covered) ContextCompat.getColor(itemView.context, R.color.bpi_orange)
+            else ContextCompat.getColor(itemView.context, R.color.black))
+        }
+
+        fun itemIsCovered(insuranceType: InsuranceType) {
+            if (insuranceType.covered) {
+                itemView.tvCover.visibility = View.VISIBLE
+                itemView.imRightArrow.visibility = View.GONE
+            } else {
+                itemView.tvCover.visibility = View.GONE
+                itemView.imRightArrow.visibility = View.VISIBLE
+            }
+        }
+
+        fun onItemClick(bpiOverview: BPIOverview) {
+            itemView.setOnClickListener {
+                if (mOnBPIOverviewAdapter != null)
+                    mOnBPIOverviewAdapter?.onItemViewClicked(bpiOverview)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/bpi/BPIOverviewFragment.kt
@@ -0,0 +1,99 @@
+package za.co.woolworths.financial.services.android.ui.fragments.bpi
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.bpi_overview_fragment.*
+import za.co.woolworths.financial.services.android.models.dto.BPIOverview
+import za.co.woolworths.financial.services.android.ui.activities.bpi.BPIBalanceProtectionActivity
+import za.co.woolworths.financial.services.android.ui.adapters.BPIOverviewAdapter
+import za.co.woolworths.financial.services.android.util.createBPIList
+import za.co.woolworths.financial.services.android.util.navigateToBalanceProtectionActivity
+import za.co.woolworths.financial.services.android.util.replaceFragment
+
+
+class BPIOverviewFragment : Fragment(), BPIOverviewAdapter.OnBPIAdapterClickListener, View.OnClickListener {
+
+    companion object {
+        fun newInstance(accountInfo: String?): BPIOverviewFragment {
+            val bpiOverviewFragment = BPIOverviewFragment()
+            val bundle = Bundle()
+            bundle.putString("accountInfo", accountInfo)
+            bpiOverviewFragment.arguments = bundle
+            return bpiOverviewFragment
+        }
+    }
+
+    private var mBPIOverviewAdapter: BPIOverviewAdapter? = null
+    private var mLayoutManager: RecyclerView.LayoutManager? = null
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.bpi_overview_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+        setupList()
+        listener()
+        setClaimButtonVisibility(createBPIList())
+    }
+
+    private fun initialize() {
+        mLayoutManager = LinearLayoutManager(this.activity!!, LinearLayout.VERTICAL, false)
+        mBPIOverviewAdapter = BPIOverviewAdapter(createBPIList(), this)
+    }
+
+    private fun listener() {
+        imGoBack.setOnClickListener(this)
+        tvBPIOverviewClaim.setOnClickListener(this)
+    }
+
+    private fun setupList() {
+        rvBPIOverview!!.layoutManager = mLayoutManager
+        rvBPIOverview!!.adapter = mBPIOverviewAdapter
+    }
+
+    private fun setClaimButtonVisibility(bpiOverviewList: ArrayList<BPIOverview>) {
+        val insuranceCoveredList = arrayListOf<Boolean>()
+        for (overview in bpiOverviewList) {
+            insuranceCoveredList.add(overview.insuranceType!!.covered)
+        }
+
+        tvBPIOverviewClaim.visibility = if (insuranceCoveredList.contains(true)) View.VISIBLE else View.GONE
+    }
+
+    override fun onItemViewClicked(bpiOverview: BPIOverview) {
+        replaceFragment(
+                fragment = BPIOverviewDetailFragment.newInstance(bpiOverview),
+                tag = BPIOverviewDetailFragment::class.java.simpleName,
+                containerViewId = R.id.flBPIContainer,
+                allowStateLoss = true,
+                enterAnimation = R.anim.slide_in_from_right,
+                exitAnimation = R.anim.slide_to_left,
+                popEnterAnimation = R.anim.slide_from_left,
+                popExitAnimation = R.anim.slide_to_right
+        )
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.tvBPIOverviewClaim -> {
+                navigateToBalanceProtectionActivity()
+            }
+
+            R.id.imGoBack -> {
+                if (activity is BPIBalanceProtectionActivity) {
+                    val bpiBalanceProtectionActivity = activity as BPIBalanceProtectionActivity
+                    bpiBalanceProtectionActivity.finishActivity()
+                }
+            }
+        }
+    }
+}
