7312f6ff9f9d6336784cc1b05006861a844055db
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2018-11-27 13:29:26 +0200

    Statement - Cannot create document: File not in PDF format or corrupted.

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/statement/PreviewStatement.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/statement/PreviewStatement.java
@@ -3,7 +3,7 @@ package za.co.woolworths.financial.services.android.ui.fragments.statement;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +15,12 @@ import com.github.barteksc.pdfviewer.listener.OnErrorListener;
 import java.io.File;
 
 
+import za.co.woolworths.financial.services.android.ui.views.WTextView;
+
+
 public class PreviewStatement extends Fragment {
+
+    private WTextView tvTitle;
 
     @Nullable
     @Override
@@ -27,11 +32,15 @@ public class PreviewStatement extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initView(view);
+
+        tvTitle = view.findViewById(R.id.tvTitle);
     }
 
     private void initView(View view) {
-        File file = new File(getActivity().getExternalFilesDir("woolworth") + "/Files/" + "statement.pdf");
-        PDFView pdfView = (PDFView) view.findViewById(R.id.pdfView);
+        final FragmentActivity activity = getActivity();
+        if (activity == null) return;
+        File file = new File(activity.getExternalFilesDir("woolworth") + "/Files/" + "statement.pdf");
+        PDFView pdfView = view.findViewById(R.id.pdfView);
         pdfView.fromFile(file)
                 .enableDoubletap(true)
                 .defaultPage(0)
@@ -42,7 +51,7 @@ public class PreviewStatement extends Fragment {
                 .onError(new OnErrorListener() {
                     @Override
                     public void onError(Throwable t) {
-                        Log.e("onError", t.toString());
+                        tvTitle.setVisibility(View.VISIBLE);
                     }
                 })
                 .load();
