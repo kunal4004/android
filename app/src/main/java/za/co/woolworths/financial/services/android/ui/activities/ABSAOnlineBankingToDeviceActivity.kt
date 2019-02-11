043abe2bdb0d5c52c3894d03b5795342ada9f2f2
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-02-11 17:22:12 +0400

    ABSA - Key In ATM PIN screen

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/ABSAOnlineBankingToDeviceActivity.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/ABSAOnlineBankingToDeviceActivity.kt
@@ -2,16 +2,20 @@ package za.co.woolworths.financial.services.android.ui.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.absa_online_banking_to_device_activity.*
 import za.co.woolworths.financial.services.android.ui.extension.addFragment
 import za.co.woolworths.financial.services.android.ui.fragments.absa.ABSABiometricFragment
+import za.co.woolworths.financial.services.android.util.Utils
 
 class ABSAOnlineBankingToDeviceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.absa_online_banking_to_device_activity)
-
+        Utils.updateStatusBarBackground(this)
+        actionBar()
         if (savedInstanceState == null) {
             addFragment(
                     fragment = ABSABiometricFragment.newInstance(),
@@ -19,5 +23,28 @@ class ABSAOnlineBankingToDeviceActivity : AppCompatActivity() {
                     containerViewId = R.id.flAbsaOnlineBankingToDevice
             )
         }
+    }
+
+    private fun actionBar() {
+        setSupportActionBar(tbOnlineBanking)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(false)
+            setDisplayUseLogoEnabled(false)
+            setHomeAsUpIndicator(R.drawable.back24)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                this.overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_to_right)
+                return true
+            }
+            else -> {
+            }
+        }
+        return false
     }
 }
