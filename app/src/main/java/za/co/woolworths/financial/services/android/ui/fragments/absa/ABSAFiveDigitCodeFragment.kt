c81d2b1f163e7303f3025e139bff662c867e2254
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-02-12 10:13:22 +0400

    ABSA - Added confirm 5 digits code fragment

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/absa/ABSAFiveDigitCodeFragment.kt
@@ -0,0 +1,117 @@
+package za.co.woolworths.financial.services.android.ui.fragments.absa
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.ImageView
+import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.absa_five_digit_code_fragment.*
+import za.co.woolworths.financial.services.android.ui.extension.replaceFragment
+
+class ABSAFiveDigitCodeFragment : ABSAFragmentExtension(), View.OnClickListener {
+
+    var mPinImageViewList: MutableList<ImageView>? = null
+
+    companion object {
+        fun newInstance() = ABSAFiveDigitCodeFragment()
+        const val MAXIMUM_PIN_ALLOWED: Int = 4
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.absa_five_digit_code_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewsAndEvents()
+        createTextListener(edtEnterATMPin)
+        clearPinImage(mPinImageViewList!!)
+    }
+
+    private fun initViewsAndEvents() {
+        mPinImageViewList = mutableListOf(ivPin1, ivPin2, ivPin3, ivPin4, ivPin5)
+        flEnterFiveDigitCode.setOnClickListener(this)
+        edtEnterATMPin.setOnKeyPreImeListener { activity?.onBackPressed() }
+        edtEnterATMPin.setOnEditorActionListener { _, actionId, _ ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                handled = true
+                navigateToConfirmFiveDigitCodeFragment()
+            }
+            handled
+        }
+
+    }
+
+    private fun navigateToConfirmFiveDigitCodeFragment() {
+        if ((edtEnterATMPin.length() - 1) == MAXIMUM_PIN_ALLOWED) {
+            val enteredPin = edtEnterATMPin.text.toString()
+            replaceFragment(
+                    fragment = ABSAConfirmFiveDigitCodeFragment.newInstance(enteredPin.toInt()),
+                    tag = ABSAConfirmFiveDigitCodeFragment::class.java.simpleName,
+                    containerViewId = R.id.flAbsaOnlineBankingToDevice,
+                    allowStateLoss = true,
+                    enterAnimation = R.anim.slide_in_from_right,
+                    exitAnimation = R.anim.slide_to_left,
+                    popEnterAnimation = R.anim.slide_from_left,
+                    popExitAnimation = R.anim.slide_to_right
+            )
+        }
+    }
+
+    private fun createTextListener(edtEnterATMPin: EditText?) {
+        var previousLength = 0
+        edtEnterATMPin?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                previousLength = s.length
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (previousLength > edtEnterATMPin.length()) { // detect backspace
+                    deletePin((edtEnterATMPin.length()), mPinImageViewList!!)
+                } else {
+                    updateEnteredPin((edtEnterATMPin.length() - 1), mPinImageViewList!!)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+    }
+
+    private fun updateEnteredPin(pinEnteredLength: Int, listOfPin: MutableList<ImageView>) {
+        listOfPin[pinEnteredLength].setImageResource(R.drawable.pin_fill)
+        if (pinEnteredLength == MAXIMUM_PIN_ALLOWED) {
+            ivEnterFiveDigitCode.setImageResource(R.drawable.next_button_circular_bg_active)
+            flEnterFiveDigitCode.isEnabled = true
+        }
+    }
+
+    private fun deletePin(pinEnteredLength: Int, listOfPin: MutableList<ImageView>) {
+        listOfPin[pinEnteredLength].setImageResource(R.drawable.pin_empty)
+        if (pinEnteredLength <= MAXIMUM_PIN_ALLOWED) {
+            ivEnterFiveDigitCode.setImageResource(R.drawable.next_button_circular_bg_inactive)
+            flEnterFiveDigitCode.isEnabled = false
+        }
+    }
+
+    private fun clearPinImage(listOfPin: MutableList<ImageView>) {
+        listOfPin.forEach {
+            it.setImageResource(R.drawable.pin_empty)
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.flEnterFiveDigitCode -> {
+                navigateToConfirmFiveDigitCodeFragment()
+            }
+        }
+    }
+}
