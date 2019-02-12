52dfe88ea9109cd743471bb693621488a5c2a833
Chandra Sekhar <chandrasekhartangudu@woolworths.co.za>
2019-02-12 10:51:40 +0530

    Revert "Revert "Revert "Revert "- order details design""""
    This reverts commit 38bc43ba9749e6969f7e70f7e7055246f1adf5ab.

--- app/src/main/java/za/co/woolworths/financial/services/android/models/ApiInterface.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/ApiInterface.java
@@ -48,6 +48,7 @@ import za.co.woolworths.financial.services.android.models.dto.LoginResponse;
 import za.co.woolworths.financial.services.android.models.dto.MessageReadRequest;
 import za.co.woolworths.financial.services.android.models.dto.MessageResponse;
 import za.co.woolworths.financial.services.android.models.dto.OfferActive;
+import za.co.woolworths.financial.services.android.models.dto.OrderDetailsResponse;
 import za.co.woolworths.financial.services.android.models.dto.OrdersResponse;
 import za.co.woolworths.financial.services.android.models.dto.ProductDetailResponse;
 import za.co.woolworths.financial.services.android.models.dto.ProductView;
@@ -958,4 +959,17 @@ public interface ApiInterface {
 			@Header("os") String os,
 			@Header("osVersion") String osVersion,
 			@Header("sessionToken") String sessionToken);
+
+	@Headers({"Content-Type: application/json", "Accept: application/json", "Media-Type: application/json"})
+	@GET("/order/{id}")
+	OrderDetailsResponse getOrderDetails(
+			@Header("apiId") String apiId,
+			@Header("sha1Password") String sha1Password,
+			@Header("deviceVersion") String deviceVersion,
+			@Header("deviceModel") String deviceModel,
+			@Header("network") String network,
+			@Header("os") String os,
+			@Header("osVersion") String osVersion,
+			@Header("sessionToken") String sessionToken,
+			@Path("id") String id);
 }
