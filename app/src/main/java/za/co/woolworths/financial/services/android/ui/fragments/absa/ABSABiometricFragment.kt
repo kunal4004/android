8a4279539e2d514ba96ae526946d570db45beabd
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-02-13 11:25:04 +0400

    ABSA - PNG images for biometric active, inactive and globe

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/absa/ABSABiometricFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/absa/ABSABiometricFragment.kt
@@ -2,7 +2,6 @@ package za.co.woolworths.financial.services.android.ui.fragments.absa
 
 import android.graphics.Paint
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +9,7 @@ import com.awfs.coordination.R
 import kotlinx.android.synthetic.main.absa_biometric_fragment.*
 import za.co.woolworths.financial.services.android.ui.extension.replaceFragment
 
-class ABSABiometricFragment : Fragment() {
+class ABSABiometricFragment : ABSAFragmentExtension() {
 
     companion object {
         fun newInstance() = ABSABiometricFragment()
@@ -31,10 +30,12 @@ class ABSABiometricFragment : Fragment() {
             ->
             if (isChecked) {
                 imFootPrintLogo.setImageResource(R.drawable.biometric_active)
+                imFootPrintLogo.alpha = 1f
                 ivNavigateToNextFragment.alpha = 1f
                 ivNavigateToNextFragment.isEnabled = true
             } else {
                 imFootPrintLogo.setImageResource(R.drawable.biometric_inactive)
+                imFootPrintLogo.alpha = 0.5f
                 ivNavigateToNextFragment.alpha = 0.5f
                 ivNavigateToNextFragment.isEnabled = false
             }
@@ -55,5 +56,10 @@ class ABSABiometricFragment : Fragment() {
                 popEnterAnimation = R.anim.slide_from_left,
                 popExitAnimation = R.anim.slide_to_right
         )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        alwaysHideWindowSoftInputMode()
     }
 }
--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/absa/ABSAFragmentExtension.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/absa/ABSAFragmentExtension.kt
@@ -2,6 +2,7 @@ package za.co.woolworths.financial.services.android.ui.fragments.absa
 
 import android.content.Context
 import android.support.v4.app.Fragment
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 
@@ -23,6 +24,12 @@ open class ABSAFragmentExtension : Fragment() {
                 (it.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
                         .hideSoftInputFromWindow(it.currentFocus.windowToken, 0)
             }
+        }
+    }
+
+    fun alwaysHideWindowSoftInputMode() {
+        activity?.apply {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
         }
     }
 }
