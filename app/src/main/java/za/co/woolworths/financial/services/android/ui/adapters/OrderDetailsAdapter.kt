711ce596b7f83904e2952f7564c999c391ca9c35
Chandra Sekhar <chandrasekhartangudu@woolworths.co.za>
2019-02-13 16:21:33 +0530

    - order details to PDP added

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/OrderDetailsAdapter.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/OrderDetailsAdapter.kt
@@ -1,22 +1,23 @@
 package za.co.woolworths.financial.services.android.ui.adapters
 
+import android.app.Activity
 import android.content.Context
+import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.awfs.coordination.R
+import com.google.gson.Gson
 import za.co.woolworths.financial.services.android.models.dto.OrderDetailsItem
 import za.co.woolworths.financial.services.android.ui.adapters.holder.OrdersBaseViewHolder
-import kotlinx.android.synthetic.main.my_orders_upcoming_order_item.*
-import kotlinx.android.synthetic.main.my_orders_past_orders_header.*
-import kotlinx.android.synthetic.main.add_item_to_shoppinglist_layout.*
 import kotlinx.android.synthetic.main.my_orders_past_orders_header.view.*
 import kotlinx.android.synthetic.main.my_orders_upcoming_order_item.view.*
-import kotlinx.android.synthetic.main.order_details_commerce_item.*
 import kotlinx.android.synthetic.main.order_details_commerce_item.view.*
 import za.co.woolworths.financial.services.android.models.dto.CommerceItem
 import za.co.woolworths.financial.services.android.models.dto.Order
+import za.co.woolworths.financial.services.android.models.dto.ProductDetails
+import za.co.woolworths.financial.services.android.util.ScreenManager
 import za.co.woolworths.financial.services.android.util.WFormatter
 
 class OrderDetailsAdapter(val context: Context, var dataList: ArrayList<OrderDetailsItem>) : RecyclerView.Adapter<OrdersBaseViewHolder>() {
@@ -62,6 +63,8 @@ class OrderDetailsAdapter(val context: Context, var dataList: ArrayList<OrderDet
             val item = dataList[position].item as CommerceItem
             itemView.itemName.text = item.commerceItemInfo.productDisplayName
             itemView.price.text = WFormatter.formatAmount(item.priceInfo.amount)
+
+            itemView.setOnClickListener { presentProductDetailsPage(item) }
         }
 
     }
@@ -82,5 +85,21 @@ class OrderDetailsAdapter(val context: Context, var dataList: ArrayList<OrderDet
 
     override fun getItemViewType(position: Int): Int {
         return dataList[position].type.value
+    }
+
+    fun presentProductDetailsPage(commerceItem: CommerceItem) {
+        val productList = ProductDetails()
+        val commerceItemInfo = commerceItem.commerceItemInfo
+        productList.externalImageRef = commerceItemInfo.externalImageURL
+        productList.productName = commerceItemInfo.productDisplayName
+        productList.fromPrice = commerceItem.priceInfo.getAmount().toFloat()
+        productList.productId = commerceItemInfo.productId
+        productList.sku = commerceItemInfo.catalogRefId
+        val gson = Gson()
+        val strProductList = gson.toJson(productList)
+        val bundle = Bundle()
+        bundle.putString("strProductList", strProductList)
+        bundle.putString("strProductCategory", "")
+        ScreenManager.presentProductDetails(context as Activity, bundle)
     }
 }
