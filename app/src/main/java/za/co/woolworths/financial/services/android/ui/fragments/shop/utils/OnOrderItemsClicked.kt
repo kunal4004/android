01eb06ace53fed5301b3b00cec94f2d665bd82e3
Chandra Sekhar <chandrasekhartangudu@woolworths.co.za>
2019-02-19 22:27:13 +0530

    no message

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shop/utils/OnOrderItemsClicked.kt
+++
@@ -1,7 +0,0 @@
-package za.co.woolworths.financial.services.android.ui.fragments.shop.utils
-
-import za.co.woolworths.financial.services.android.models.dto.OrderDetailsResponse
-
-interface OnOrderItemsClicked {
-    fun onOrderItemsClicked(orderDetailsResponse: OrderDetailsResponse)
-}
