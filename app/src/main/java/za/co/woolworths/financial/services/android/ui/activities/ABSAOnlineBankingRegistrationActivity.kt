c8a3ef3c140ef4b5ef39bea5c1e21b487b4acfd1
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-03-07 10:55:27 +0400

    - ABSA - Format Credit card number - Minor bug fix

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/ABSAOnlineBankingRegistrationActivity.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/ABSAOnlineBankingRegistrationActivity.kt
@@ -15,6 +15,7 @@ import za.co.woolworths.financial.services.android.util.Utils
 class ABSAOnlineBankingRegistrationActivity : AppCompatActivity() {
 
     private var mShouldDisplayABSALogin: Boolean? = false
+    private var mCreditAccountInfo: String? = ""
 
     companion object {
         const val SHOULD_DISPLAY_LOGIN_SCREEN = "SHOULD_DISPLAY_LOGIN_SCREEN"
@@ -34,7 +35,7 @@ class ABSAOnlineBankingRegistrationActivity : AppCompatActivity() {
                         containerViewId = R.id.flAbsaOnlineBankingToDevice)
             } else {
                 addFragment(
-                        fragment = AbsaEnterAtmPinCodeFragment.newInstance(),
+                        fragment = AbsaEnterAtmPinCodeFragment.newInstance(mCreditAccountInfo),
                         tag = AbsaEnterAtmPinCodeFragment::class.java.simpleName,
                         containerViewId = R.id.flAbsaOnlineBankingToDevice)
             }
@@ -42,8 +43,9 @@ class ABSAOnlineBankingRegistrationActivity : AppCompatActivity() {
     }
 
     private fun getBundleArgument() {
-        val bundle: Bundle? = intent?.extras!!
-        mShouldDisplayABSALogin = bundle!!.getBoolean(SHOULD_DISPLAY_LOGIN_SCREEN, false)
+        val bundle: Bundle? = intent?.extras
+        mShouldDisplayABSALogin = bundle?.getBoolean(SHOULD_DISPLAY_LOGIN_SCREEN, false)
+        var creditAccountInfo = bundle?.getString("account_info")
     }
 
     private fun actionBar() {
@@ -61,8 +63,6 @@ class ABSAOnlineBankingRegistrationActivity : AppCompatActivity() {
             android.R.id.home -> {
                 navigateBack()
                 return true
-            }
-            else -> {
             }
         }
         return false
