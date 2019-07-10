6844bf1121df9fef3192bec1369b32833d5d71bb
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-07-10 13:04:08 +0400

    Enable ABSA services when  minSupportedAppVersion is true and add authenticVersionReleaseNote note on update dialog

--- app/src/main/java/za/co/woolworths/financial/services/android/models/dto/AbsaBankingOpenApiServices.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/AbsaBankingOpenApiServices.kt
@@ -1,3 +1,4 @@
 package za.co.woolworths.financial.services.android.models.dto
 
-data class AbsaBankingOpenApiServices(val isEnabled: Boolean, val baseURL: String, val appPublicKey: String, val contentEncryptionPublicKey: String)
+
+data class AbsaBankingOpenApiServices(var isEnabled: Boolean = false, val baseURL: String, val appPublicKey: String, val contentEncryptionPublicKey: String, val minSupportedAppVersion: String)
