a085595e77ada67b7fa57a8393015bf0bbd347cb
Chandra Sekhar <chandrasekhartangudu@woolworths.co.za>
2019-07-12 16:02:40 +0200

    - RxJava2CallAdapterFactory added

--- app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitConfig.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitConfig.kt
@@ -4,6 +4,7 @@ import com.awfs.coordination.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import za.co.wigroup.androidutils.Util
 import java.util.concurrent.TimeUnit
@@ -31,6 +32,7 @@ abstract class RetrofitConfig : NetworkConfig() {
         mApiInterface = Retrofit.Builder()
                 .baseUrl(BuildConfig.HOST + "/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(httpBuilder.build())
                 .build()
                 .create(ApiInterface::class.java)
