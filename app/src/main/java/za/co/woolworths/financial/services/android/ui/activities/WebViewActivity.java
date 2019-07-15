7cf298ed1728168a69c625218dd7fdf19101c265
Eesa Jacobs <eesaj@wigroup.co.za>
2019-07-15 11:58:54 +0200

    Removing support for Brian's 'Update Details' work

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/WebViewActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/WebViewActivity.java
@@ -1,6 +1,5 @@
 package za.co.woolworths.financial.services.android.ui.activities;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
@@ -29,7 +28,6 @@ import java.util.Map;
 
 import za.co.woolworths.financial.services.android.ui.views.WTextView;
 import za.co.woolworths.financial.services.android.util.ErrorHandlerView;
-import za.co.woolworths.financial.services.android.util.SessionUtilities;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -141,7 +139,7 @@ public class WebViewActivity extends AppCompatActivity {
 
 	private Map<String, String> getExtraHeader() {
 		Map<String, String> extraHeaders = new HashMap<>();
-		extraHeaders.put("bearer", SessionUtilities.getInstance().getSessionToken());
+		//extraHeaders.put("bearer", SessionUtilities.getInstance().getSessionToken());
 		return extraHeaders;
 	}
 }
