9cc058158c6faac8bf1b80fc7f30685c463d582b
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-08-06 10:10:28 +0400

    Benefit Reward Activity added

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/WRewardBenefitActivity.kt
@@ -0,0 +1,80 @@
+package za.co.woolworths.financial.services.android.ui.activities
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.awfs.coordination.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.reward_benefit_activity.*
+import za.co.woolworths.financial.services.android.ui.fragments.wreward.RewardBenefitFragment
+import za.co.woolworths.financial.services.android.ui.fragments.wreward.VIPExclusiveFragment
+import za.co.woolworths.financial.services.android.util.Utils
+
+
+class WRewardBenefitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reward_benefit_activity)
+        Utils.updateStatusBarBackground(this, android.R.color.transparent)
+        window?.decorView?.systemUiVisibility = 0
+        init()
+        closeIcon.setOnClickListener { onBackPressed() }
+    }
+
+    private fun init() {
+        vpRewardBenefit.adapter = object : FragmentStateAdapter(this) {
+            override fun createFragment(position: Int): Fragment {
+                return when (position) {
+                    0 -> RewardBenefitFragment.newInstance()
+                    else -> VIPExclusiveFragment.newInstance()
+                }
+            }
+
+            override fun getItemCount(): Int {
+                return 2
+            }
+        }
+
+        TabLayoutMediator(tabs, vpRewardBenefit) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.benefits)
+                else -> getString(R.string.vip_exclusive)
+            }
+        }.attach()
+
+        updateTabFont(0, true)
+
+        tabs?.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                updateTabFont(tab?.position ?: 0, false)
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                updateTabFont(tab?.position ?: 0, true)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+
+    private fun updateTabFont(position: Int, tabIsSelected: Boolean) {
+        val tabLayout = (tabs.getChildAt(0) as? ViewGroup)?.getChildAt(position) as? LinearLayout
+        val tabTextView = tabLayout?.getChildAt(1) as? AppCompatTextView
+        val typeface = ResourcesCompat.getFont(this@WRewardBenefitActivity, if (tabIsSelected) R.font.futura_semi_bold_ttf else R.font.futura_medium_ttf)
+        tabTextView?.setTypeface(typeface, Typeface.NORMAL)
+    }
+
+    override fun onBackPressed() {
+        finish()
+        overridePendingTransition(R.anim.stay, R.anim.slide_down_anim)
+    }
+}
