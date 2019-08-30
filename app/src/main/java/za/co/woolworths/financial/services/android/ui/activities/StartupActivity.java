375844943aa27227f38a798260ce2fe586bd855d
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-08-30 06:54:16 +0400

    Uncomment root check permission

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/StartupActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/StartupActivity.java
@@ -263,13 +263,13 @@ public class StartupActivity extends AppCompatActivity implements MediaPlayer.On
 	@Override
 	protected void onStart() {
 		super.onStart();
-//		if (Utils.checkForBinarySu() && CommonUtils.isRooted(this) && getSupportFragmentManager() != null) {
-//			if (pBar != null)
-//				pBar.setVisibility(View.GONE);
-//			RootedDeviceInfoFragment rootedDeviceInfoFragment = RootedDeviceInfoFragment.Companion.newInstance(getString(R.string.rooted_phone_desc));
-//			rootedDeviceInfoFragment.show(getSupportFragmentManager(), RootedDeviceInfoFragment.class.getSimpleName());
-//			return;
-//		}
+		if (Utils.checkForBinarySu() && CommonUtils.isRooted(this) && getSupportFragmentManager() != null) {
+			if (pBar != null)
+				pBar.setVisibility(View.GONE);
+			RootedDeviceInfoFragment rootedDeviceInfoFragment = RootedDeviceInfoFragment.Companion.newInstance(getString(R.string.rooted_phone_desc));
+			rootedDeviceInfoFragment.show(getSupportFragmentManager(), RootedDeviceInfoFragment.class.getSimpleName());
+			return;
+		}
 
 		FirebaseDynamicLinks.getInstance()
 				.getDynamicLink(getIntent())
