321bd1ff8fafc3dee83d17dca69b875b8b293c46
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-09-12 12:52:37 +0400

    Fit product image once ready and placeholder in the center but of smaller size

--- app/src/main/java/za/co/woolworths/financial/services/android/util/ImageManager.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/ImageManager.kt
@@ -1,19 +1,34 @@
 package za.co.woolworths.financial.services.android.util
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import com.awfs.coordination.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 
 class ImageManager {
 
     companion object {
         fun setPicture(productImage: ImageView?, img_location: String) = productImage?.let { image ->
+            productImage.scaleType = ImageView.ScaleType.CENTER
             productImage.context?.apply {
                 Glide.with(this)
                         .load(img_location)
+                        .listener(object : RequestListener<Drawable> {
+                            override fun onLoadFailed(p0: GlideException?, p1: Any?, p2: Target<Drawable>?, p3: Boolean): Boolean {
+
+                                return false
+                            }
+                            override fun onResourceReady(drawable: Drawable?, p1: Any?, p2: Target<Drawable>?, p3: DataSource?, p4: Boolean): Boolean {
+                                productImage.adjustViewBounds = true
+                                return false
+                            }
+                        })
                         .format(DecodeFormat.PREFER_ARGB_8888)
                         .placeholder(R.drawable.woolworth_logo_icon)
                         .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
