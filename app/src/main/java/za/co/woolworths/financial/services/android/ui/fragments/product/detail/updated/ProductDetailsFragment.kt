a68b058cf912d857063483cb1340cace3cbf7d12
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-11-25 16:12:28 +0200

    Remove not nullable constraint on externalImageRef

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/product/detail/updated/ProductDetailsFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/product/detail/updated/ProductDetailsFragment.kt
@@ -299,14 +299,14 @@ class ProductDetailsFragment : Fragment(), ProductDetailsContract.ProductDetails
         }
     }
 
-    override fun getImageByWidth(imageUrl: String, context: Context): String {
+    override fun getImageByWidth(imageUrl: String?, context: Context): String {
         (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).apply {
+            var imageLink = imageUrl
             val deviceHeight = this.defaultDisplay
             val size = Point()
             deviceHeight.getSize(size)
             val width = size.x
-            var imageLink = imageUrl
-            if (imageLink.isEmpty()) imageLink = "https://images.woolworthsstatic.co.za/"
+          if (imageLink == null) imageLink = "https://images.woolworthsstatic.co.za/"
             return imageLink + "" + if (imageLink.contains("jpg")) "" else "?w=$width&q=85"
         }
     }
