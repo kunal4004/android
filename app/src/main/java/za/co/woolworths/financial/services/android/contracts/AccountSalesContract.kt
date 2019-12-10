5d170319f7a7fcf13f90f07a84c5577e19050deb
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2019-12-10 14:11:15 +0200

    Account Sales Presenter Layout

--- app/src/main/java/za/co/woolworths/financial/services/android/contracts/AccountSalesContract.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/contracts/AccountSalesContract.kt
@@ -2,15 +2,14 @@ package za.co.woolworths.financial.services.android.contracts
 
 import za.co.woolworths.financial.services.android.models.dto.account.AccountSales
 import za.co.woolworths.financial.services.android.models.dto.account.ApplyNowState
+import za.co.woolworths.financial.services.android.models.dto.account.CardHeader
 
 
 interface AccountSalesContract {
 
     interface AccountSalesView {
-        fun displayHeaderItems(headerItems: AccountSales)
-        fun hideCardCollectionLayout()
+        fun displayHeaderItems(headerItems: CardHeader)
         fun displayAccountSalesBlackInfo(storeCard: AccountSales)
-        fun displayCreditCardFrontUI(position: Int)
         fun displayCreditCard(goldCreditCard: AccountSales, blackCreditCard: AccountSales, position: Int)
     }
 
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/account/AccountSalesPresenterImpl.kt
@@ -0,0 +1,77 @@
+package za.co.woolworths.financial.services.android.ui.activities.account
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.navigation.NavController
+import com.awfs.coordination.R
+import com.google.gson.Gson
+import za.co.woolworths.financial.services.android.contracts.AccountSalesContract
+import za.co.woolworths.financial.services.android.models.WoolworthsApplication
+import za.co.woolworths.financial.services.android.models.dto.account.AccountSales
+import za.co.woolworths.financial.services.android.models.dto.account.ApplyNowState
+import za.co.woolworths.financial.services.android.util.Utils
+
+class AccountSalesPresenterImpl(private var mainView: AccountSalesContract.AccountSalesView?, private var model: AccountSalesContract.AccountSalesModel) : AccountSalesContract.AccountSalesPresenter, AccountSalesContract.AccountSalesModel {
+
+    companion object {
+        const val ACCOUNT_SALES_CREDIT_CARD = "ACCOUNT_SALES_CREDIT_CARD"
+    }
+
+    override fun switchAccountSalesProduct(applyNowState: ApplyNowState) {
+        mainView?.apply {
+            when (applyNowState) {
+                ApplyNowState.STORE_CARD -> {
+                    val storeCard = getStoreCard()
+                    displayHeaderItems(storeCard.cardHeader)
+                    displayAccountSalesBlackInfo(storeCard)
+                }
+
+                ApplyNowState.GOLD_CREDIT_CARD, ApplyNowState.BLACK_CREDIT_CARD -> {
+                    // Gold vs Black credit card
+                    val creditCard = getCreditCard()
+                    displayCreditCard(creditCard[0], creditCard[1], visibleApplyNowState(applyNowState))
+                }
+
+                ApplyNowState.PERSONAL_LOAN -> {
+                    val personalLoan = getPersonalLoan()
+                    displayHeaderItems(personalLoan.cardHeader)
+                    displayAccountSalesBlackInfo(personalLoan)
+                }
+            }
+        }
+    }
+
+    private fun visibleApplyNowState(applyNowState: ApplyNowState) = if (applyNowState == ApplyNowState.GOLD_CREDIT_CARD) 0 else 1
+
+    override fun getCreditCard(): MutableList<AccountSales> = model.getCreditCard()
+
+    override fun getStoreCard(): AccountSales = model.getStoreCard()
+
+    override fun getPersonalLoan(): AccountSales = model.getPersonalLoan()
+
+    fun getOverlayDisplayedHeight(): Int? {
+        val activity = WoolworthsApplication.getInstance()?.currentActivity
+        val height: Int? = activity?.resources?.displayMetrics?.heightPixels ?: 0
+        return height?.div(3)?.plus(Utils.dp2px(activity, 18f)) ?: 0
+    }
+
+    fun onApplyNowButtonTapped(activity: Activity?) =
+            Utils.openExternalLink(activity, WoolworthsApplication.getApplyNowLink())
+
+    fun setAccountSalesDetailPage(storeCard: AccountSales, navDetailController: NavController) {
+        val bundle = Bundle()
+        bundle.putString(ACCOUNT_SALES_CREDIT_CARD, Gson().toJson(storeCard))
+        navDetailController.setGraph(navDetailController.graph, bundle)
+    }
+
+    fun onDestroy() {
+        mainView = null
+    }
+
+    fun onBackPressed(activity: Activity?) {
+        activity?.apply {
+            finish()
+            overridePendingTransition(R.anim.stay, R.anim.slide_down_anim)
+        }
+    }
+}
