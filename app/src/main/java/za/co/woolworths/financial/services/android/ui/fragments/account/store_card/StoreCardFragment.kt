6cb93b95c23c37ea51e16616b86b06fe36614dc4
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2020-01-08 13:18:41 +0400

    Account Refresh - Available Fund Views

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/store_card/StoreCardFragment.kt
@@ -0,0 +1,15 @@
+package za.co.woolworths.financial.services.android.ui.fragments.account.store_card
+
+import android.os.Bundle
+import android.view.View
+import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.account_available_fund_overview_fragment.*
+import za.co.woolworths.financial.services.android.ui.fragments.account.AvailableFundFragment
+
+class StoreCardFragment : AvailableFundFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        accountOverviewRootLayout?.setBackgroundResource(R.drawable.store_card_background)
+    }
+}
