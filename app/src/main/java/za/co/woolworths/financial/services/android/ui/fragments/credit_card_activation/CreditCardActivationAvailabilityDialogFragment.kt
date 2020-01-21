80a26c89102cf10f5a2cd5fd7201664a6bfdb464
Shekhar <chandrasekhartangudu@woolworths.co.za>
2020-01-22 00:57:40 +0530

    - copy updated

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/credit_card_activation/CreditCardActivationAvailabilityDialogFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/credit_card_activation/CreditCardActivationAvailabilityDialogFragment.kt
@@ -1,5 +1,6 @@
 package za.co.woolworths.financial.services.android.ui.fragments.credit_card_activation
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import com.awfs.coordination.R
 import kotlinx.android.synthetic.main.credit_card_activation_availability_dialog_fragment.*
 import za.co.woolworths.financial.services.android.ui.views.actionsheet.WBottomSheetDialogFragment
+import za.co.woolworths.financial.services.android.util.Utils
 
 class CreditCardActivationAvailabilityDialogFragment : WBottomSheetDialogFragment() {
 
@@ -20,8 +22,9 @@ class CreditCardActivationAvailabilityDialogFragment : WBottomSheetDialogFragmen
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        cancel.setOnClickListener {
-            dismissAllowingStateLoss()
+        callUsOnButton?.apply {
+            paintFlags = Paint.UNDERLINE_TEXT_FLAG
+            setOnClickListener { activity?.apply { Utils.makeCall(this, "0861 50 20 20") } }
         }
     }
 
