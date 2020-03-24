c8c234026703b069a4a203337914f2e2cab7f0f1
Shekhar <chandrasekhartangudu@woolworths.co.za>
2020-03-24 09:59:56 +0530

    - Enable/Disable Credit card activation using config

--- app/src/main/java/za/co/woolworths/financial/services/android/models/dto/CreditCardActivation.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/CreditCardActivation.kt
@@ -1,3 +1,3 @@
 package za.co.woolworths.financial.services.android.models.dto
 
-data class CreditCardActivation(val otpEnabledForCreditCardActivation: Boolean = false, val endpointAvailabilityTimes: CreditCardActivationAvailabilityTimes)
+data class CreditCardActivation(val otpEnabledForCreditCardActivation: Boolean = false, val endpointAvailabilityTimes: CreditCardActivationAvailabilityTimes, val minimumSupportedAppBuildNumber: String, var isEnabled: Boolean = false)
