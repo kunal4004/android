c8f446bf4c695fd38f762ced0351fab0e2b439fa
Shekhar <chandrasekhartangudu@woolworths.co.za>
2020-03-30 13:19:49 +0530

    - credit card boarding, nav graph, fragment and activity added

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/credit_card_delivery/CreditCardDeliveryAddressDetailsFragment.kt
@@ -0,0 +1,6 @@
+package za.co.woolworths.financial.services.android.ui.fragments.credit_card_delivery
+
+import androidx.fragment.app.Fragment
+
+class CreditCardDeliveryAddressDetailsFragment : Fragment() {
+}
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/credit_card_delivery/CreditCardDeliveryStatusFragment.kt
@@ -0,0 +1,15 @@
+package za.co.woolworths.financial.services.android.ui.fragments.credit_card_delivery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.awfs.coordination.R
+
+class CreditCardDeliveryStatusFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.credit_card_delivery_status_layout, container, false)
+    }
+}
