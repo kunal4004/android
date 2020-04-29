c678b7567f68fc0be1f1a6659becf11b935e7534
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2020-04-29 23:17:40 +0400

    Rename branch

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsFragment.java
@@ -398,7 +398,6 @@ public class MyAccountsFragment extends Fragment implements View.OnClickListener
 			}
 
 		}
-
 		//hide content for unavailable products
 		boolean sc = true, cc = true, pl = true;
 		for (String s : unavailableAccounts) {
