404a5638da9737cfc80098d479de93ae021652be
Shekhar <chandrasekhartangudu@woolworths.co.za>
2020-06-28 17:59:08 +0530

    -  API been added for CCD

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/credit_card_delivery/AddressDetailsResponse.kt
@@ -0,0 +1,9 @@
+package za.co.woolworths.financial.services.android.models.dto.credit_card_delivery
+
+import za.co.woolworths.financial.services.android.models.dto.Response
+
+class AddressDetailsResponse {
+    var addressDetails: AddressDetails? = null
+    var httpCode: Int = 0
+    var response: Response? = null
+}
--- app/src/main/java/za/co/woolworths/financial/services/android/models/dto/credit_card_delivery/StatusResponse.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/credit_card_delivery/StatusResponse.kt
@@ -1,3 +1,3 @@
 package za.co.woolworths.financial.services.android.models.dto.credit_card_delivery
 
-data class StatusResponse(val deliveryStatus: DeliveryStatus?, val isCardNew: Boolean?, val appointment: Appointment?)
+data class StatusResponse(val deliveryStatus: DeliveryStatus?, val isCardNew: Boolean?, val appointment: Appointment?, var name: String?, var deliverTo: String?, var isThirdPartyRecipient: Boolean?, var receivedDate: String?, var deliveredDate: String?)
