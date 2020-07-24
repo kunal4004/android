a3fc9f4eb397dba28313d6697c65e0b8d3b9ed73
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2020-07-24 16:28:22 +0400

    Strings added for pay my account

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/detail/pay_my_account/CreditAndDebitCardPaymentModelImpl.kt
@@ -0,0 +1,5 @@
+package za.co.woolworths.financial.services.android.ui.fragments.account.detail.pay_my_account
+
+class CreditAndDebitCardPaymentModelImpl {
+
+}
--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/detail/pay_my_account/CreditAndDebitCardPaymentsFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/detail/pay_my_account/CreditAndDebitCardPaymentsFragment.kt
@@ -1,17 +1,19 @@
 package za.co.woolworths.financial.services.android.ui.fragments.account.detail.pay_my_account
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.awfs.coordination.R
-import kotlinx.android.synthetic.main.account_detail_header_fragment.*
 
 class CreditAndDebitCardPaymentsFragment : Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.credit_and_debit_card_payments_fragment, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        cardDetailImageView?.setImageResource(R.drawable.w_black_credit_card)
-
-
     }
 }
