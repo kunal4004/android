a9f8c615458285fef8ac4f4bdf636399026f3bd8
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2020-10-22 13:27:03 +0400

    Feature Branch : Pay My Account Bug Fixing

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/account/sign_in/AccountSignedInActivity.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/account/sign_in/AccountSignedInActivity.kt
@@ -217,7 +217,7 @@ class AccountSignedInActivity : AppCompatActivity(), IAccountSignedInContract.My
 
                     // on back to my account pressed (R.string.back_to_my_account_button)
                     PMA_TRANSACTION_COMPLETED_RESULT_CODE -> {
-                        payMyAccountViewModel.setNavigationResult(PayMyAccountViewModel.OnBackNavigation.ADD)
+                        payMyAccountViewModel.setNavigationResult(PayMyAccountViewModel.OnBackNavigation.RETRY)
                         mAvailableFundsNavHost?.navController?.navigateUp()
                     }
                 }
