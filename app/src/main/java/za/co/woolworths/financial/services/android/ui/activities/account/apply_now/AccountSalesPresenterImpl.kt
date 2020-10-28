45a074270d68a69c5397ef45e65d7eaa1512028a
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2020-10-28 13:07:55 +0400

    Apply Now Links becomes null

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/account/apply_now/AccountSalesPresenterImpl.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/account/apply_now/AccountSalesPresenterImpl.kt
@@ -14,7 +14,6 @@ import za.co.woolworths.financial.services.android.models.WoolworthsApplication
 import za.co.woolworths.financial.services.android.models.dto.account.AccountSales
 import za.co.woolworths.financial.services.android.models.dto.account.ApplyNowState
 import za.co.woolworths.financial.services.android.util.KotlinUtils
-import za.co.woolworths.financial.services.android.util.Utils
 
 class AccountSalesPresenterImpl(private var mainView: IAccountSalesContract.AccountSalesView?, private var model: IAccountSalesContract.AccountSalesModel) : IAccountSalesContract.AccountSalesPresenter, IAccountSalesContract.AccountSalesModel {
 
@@ -35,14 +34,14 @@ class AccountSalesPresenterImpl(private var mainView: IAccountSalesContract.Acco
     override fun getPersonalLoan(): AccountSales = model.getPersonalLoan()
 
     @Throws(RuntimeException::class)
-    fun onApplyNowButtonTapped() {
+    fun onApplyNowButtonTapped(): String? {
         val applyNowLinks = WoolworthsApplication.getApplyNowLink()
-        Utils.openBrowserWithUrl(when (getApplyNowState()) {
-            ApplyNowState.STORE_CARD -> applyNowLinks.storeCard
-            ApplyNowState.GOLD_CREDIT_CARD, ApplyNowState.BLACK_CREDIT_CARD, ApplyNowState.SILVER_CREDIT_CARD -> applyNowLinks.creditCard
-            ApplyNowState.PERSONAL_LOAN -> applyNowLinks.personalLoan
+       return when (getApplyNowState()) {
+            ApplyNowState.STORE_CARD -> applyNowLinks?.storeCard
+            ApplyNowState.GOLD_CREDIT_CARD, ApplyNowState.BLACK_CREDIT_CARD, ApplyNowState.SILVER_CREDIT_CARD -> applyNowLinks?.creditCard
+            ApplyNowState.PERSONAL_LOAN -> applyNowLinks?.personalLoan
             else -> throw RuntimeException("OnApplyNowButtonTapped:: Invalid ApplyNowState ## : ${getApplyNowState()}")
-        })
+        }
     }
 
     fun onBackPressed(activity: Activity?) = KotlinUtils.onBackPressed(activity)
--- app/src/main/java/za/co/woolworths/financial/services/android/util/KotlinUtils.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/KotlinUtils.kt
@@ -433,5 +433,13 @@ class KotlinUtils {
                 Crashlytics.log(t.message)
             }
         }
+
+        fun openBrowserWithUrl(urlString: String?, activity: Activity?) {
+            urlString?.apply {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(this))
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                activity?.startActivity(intent)
+            }
+        }
     }
 }
