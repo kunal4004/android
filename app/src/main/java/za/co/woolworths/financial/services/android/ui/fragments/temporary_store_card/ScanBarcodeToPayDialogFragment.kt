0adc0e4473755ce68ef1817d21cbcb2b825048cb
Jean-Paul Manuel <jean-paulm@yoyowallet.com>
2021-06-23 13:44:00 +0400

    Temp Store Card - Barcode dialog UI updated

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/temporary_store_card/ScanBarcodeToPayDialogFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/temporary_store_card/ScanBarcodeToPayDialogFragment.kt
@@ -1,8 +1,9 @@
 package za.co.woolworths.financial.services.android.ui.fragments.temporary_store_card
 
 import android.content.DialogInterface
-import android.content.res.Resources
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +11,9 @@ import com.awfs.coordination.R
 import com.google.gson.Gson
 import com.google.zxing.BarcodeFormat
 import kotlinx.android.synthetic.main.scan_barcode_to_pay_dialog.*
-import kotlinx.android.synthetic.main.scan_barcode_to_pay_dialog.barCodeImage
 import za.co.woolworths.financial.services.android.models.dto.temporary_store_card.StoreCardsResponse
 import za.co.woolworths.financial.services.android.ui.activities.card.MyCardDetailActivity.Companion.STORE_CARD_DETAIL
 import za.co.woolworths.financial.services.android.ui.extension.withArgs
-import za.co.woolworths.financial.services.android.ui.fragments.npc.MyCardDetailFragment
-import za.co.woolworths.financial.services.android.ui.fragments.npc.MyCardExtension
 import za.co.woolworths.financial.services.android.ui.views.actionsheet.WBottomSheetDialogFragment
 import za.co.woolworths.financial.services.android.util.Utils
 
@@ -25,6 +23,8 @@ class ScanBarcodeToPayDialogFragment : WBottomSheetDialogFragment() {
     private var mStoreCardsResponse: StoreCardsResponse? = null
     private var listener: IOnTemporaryStoreCardDialogDismiss? = null
     private var isRegenerateBarcode: Boolean = false
+    private var isCardDetailsVisible: Boolean = false
+    private val timerCardDetailsVisibility = Handler(Looper.getMainLooper())
 
     interface IOnTemporaryStoreCardDialogDismiss {
         fun onDialogDismiss()
@@ -32,6 +32,7 @@ class ScanBarcodeToPayDialogFragment : WBottomSheetDialogFragment() {
     }
 
     companion object {
+        private const val DURATION_FADE: Long = 300
 
         fun newInstance(mStoreCardsResponse: String) = ScanBarcodeToPayDialogFragment().withArgs {
             putString(STORE_CARD_DETAIL, mStoreCardsResponse)
@@ -61,16 +62,52 @@ class ScanBarcodeToPayDialogFragment : WBottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        regenerateBarcode.setOnClickListener { regenerateBarcode() }
-        configureUI()
         setUniqueIds()
     }
 
+    override fun onResume() {
+        super.onResume()
+        configureUI()
+    }
+
     private fun configureUI() {
+        btnDismissDialog.setOnClickListener { dismiss() }
+        btnToggleCardDetails.setOnClickListener { toggleCardDetailsVisibility() }
+
         mStoreCardsResponse?.storeCardsData?.virtualCard?.let {
-            barCodeImage.setImageBitmap(Utils.encodeAsBitmap(it.number.plus(it.sequence), BarcodeFormat.CODE_128, Resources.getSystem().displayMetrics.widthPixels, 60))
+            tvCardNumber.text = it.number.chunked(4).joinToString("  ")
+            tvCardholderValue.text = it.embossedName
+            tvSequenceNumberValue.text = it.sequence
+            ivBarcode.post {
+                ivBarcode.setImageBitmap(Utils.encodeAsBitmap(it.number.plus(it.sequence), BarcodeFormat.CODE_128, ivBarcode.width, 60))
+            }
         }
-        cardHolderName.text = MyCardExtension.toTitleCase(MyCardDetailFragment.cardName())
+
+        rlCardBarcode.alpha = 1f
+        rlCardDetails.alpha = 0f
+        isCardDetailsVisible = false
+    }
+
+    private fun toggleCardDetailsVisibility() {
+        isCardDetailsVisible = !isCardDetailsVisible
+        btnToggleCardDetails.text = getString(if (isCardDetailsVisible) R.string.dialog_scan_barcode_to_pay_toggle_details_hidden else R.string.dialog_scan_barcode_to_pay_toggle_details_visible)
+
+        rlCardBarcode
+                .animate()
+                .setDuration(DURATION_FADE)
+                .alpha(if (isCardDetailsVisible) 0f else 1f)
+                .start()
+        rlCardDetails
+                .animate()
+                .setDuration(DURATION_FADE)
+                .alpha(if (isCardDetailsVisible) 1f else 0f)
+                .start()
+
+        timerCardDetailsVisibility.removeCallbacksAndMessages(null)
+        if (isCardDetailsVisible) {
+            // TODO: get value from app config
+            timerCardDetailsVisibility.postDelayed({ toggleCardDetailsVisibility() }, 5000)
+        }
     }
 
     private fun regenerateBarcode() {
@@ -85,11 +122,15 @@ class ScanBarcodeToPayDialogFragment : WBottomSheetDialogFragment() {
 
     private fun setUniqueIds() {
         activity?.resources?.apply {
-            title?.contentDescription = getString(R.string.scan_barcode_title)
-            description?.contentDescription = getString(R.string.scan_barcode_description)
-            barCodeImage?.contentDescription = getString(R.string.barcode_image)
-            regenerateBarcode?.contentDescription = getString(R.string.scan_barcode_regenerate)
-            cardHolderName?.contentDescription = getString(R.string.text_cardHolderName)
+//            tvTitle?.contentDescription = getString(R.string.scan_barcode_title)
+//            tvDescription?.contentDescription = getString(R.string.scan_barcode_description)
+//            ivBarcode?.contentDescription = getString(R.string.barcode_image)
+//            cardHolderName?.contentDescription = getString(R.string.text_cardHolderName)
         }
+    }
+
+    override fun onDetach() {
+        timerCardDetailsVisibility.removeCallbacksAndMessages(null)
+        super.onDetach()
     }
 }
