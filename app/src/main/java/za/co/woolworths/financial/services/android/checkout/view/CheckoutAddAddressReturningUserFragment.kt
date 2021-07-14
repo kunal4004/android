3209e8898f79a3e6c6bbb068ee1b21538ac93bd9
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2021-07-14 11:37:29 +0530

    Added delivery instructions views and layouts.

--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutAddAddressReturningUserFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutAddAddressReturningUserFragment.kt
@@ -6,16 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.layout_native_checkout_delivery_instructions.*
+import za.co.woolworths.financial.services.android.util.Utils
 
 /**
  * Created by Kunal Uttarwar on 27/05/21.
  */
-class CheckoutAddAddressReturningUserFragment: Fragment() {
+class CheckoutAddAddressReturningUserFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.checkout_add_address_retuning_user, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.apply {
+            view.setOnClickListener {
+                Utils.hideSoftKeyboard(this)
+            }
+        }
+
+        switchSpecialDeliveryInstruction?.setOnCheckedChangeListener { buttonView, isChecked ->
+            edtTxtSpecialDeliveryInstruction?.visibility =
+                if (isChecked) View.VISIBLE else View.GONE
+        }
+
+        switchGiftInstructions?.setOnCheckedChangeListener { buttonView, isChecked ->
+            edtTxtGiftInstructions?.visibility =
+                if (isChecked) View.VISIBLE else View.GONE
+        }
     }
 }
