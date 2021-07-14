450d9217bdf9cb6e780113b84188464168adde8f
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2021-07-14 16:01:48 +0530

    Added strings and order summary data.

--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutAddAddressReturningUserFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutAddAddressReturningUserFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.layout_native_checkout_delivery_order_summary.*
+import za.co.woolworths.financial.services.android.models.dto.OrderSummary
 
 /**
  * Created by Kunal Uttarwar on 27/05/21.
  */
-class CheckoutAddAddressReturningUserFragment: Fragment() {
+class CheckoutAddAddressReturningUserFragment : Fragment() {
 
     companion object {
         const val KEY_ARGS_ORDER_SUMMARY = "ORDER_SUMMARY"
@@ -22,5 +24,29 @@ class CheckoutAddAddressReturningUserFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.checkout_add_address_retuning_user, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initOrderSummary()
+    }
+
+    /**
+     * Initializes Order Summary data from argument.
+     */
+    private fun initOrderSummary() {
+        arguments?.apply {
+            val orderSummary = getSerializable(KEY_ARGS_ORDER_SUMMARY) as? OrderSummary
+            orderSummary?.let { orderSummary ->
+                txtOrderSummaryYourCartValue?.text = "R ${orderSummary.basketTotal}"
+                orderSummary?.discountDetails?.let { discountDetails ->
+                    txtOrderSummaryDiscountValue?.text =
+                        "- R ${discountDetails.otherDiscount}"
+                    txtOrderSummaryTotalDiscountValue?.text =
+                        "- R ${discountDetails.totalDiscount}"
+                }
+            }
+        }
     }
 }
