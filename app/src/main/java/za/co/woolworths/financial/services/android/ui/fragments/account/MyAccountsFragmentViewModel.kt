261ba0c12ae95da1f025f463f14c26cac9cb0ceb
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2021-07-27 11:03:09 +0400

    Added App Version Name

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsFragmentViewModel.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsFragmentViewModel.kt
@@ -3,13 +3,14 @@ package za.co.woolworths.financial.services.android.ui.fragments.account
 import androidx.lifecycle.ViewModel
 import za.co.woolworths.financial.services.android.models.dto.AccountsResponse
 import za.co.woolworths.financial.services.android.ui.fragments.account.apply_now.ViewApplicationStatusImpl
+import za.co.woolworths.financial.services.android.util.application.ApplicationInfoImpl
 
 class MyAccountsFragmentViewModel : ViewModel() {
 
     var myAccountsPresenter : MyAccountsPresenter? = null
 
     fun getAccountPresenter(accountsResponse: AccountsResponse?): MyAccountsPresenter? {
-        myAccountsPresenter =  MyAccountsPresenter(ViewApplicationStatusImpl(accountsResponse = accountsResponse))
+        myAccountsPresenter =  MyAccountsPresenter(ViewApplicationStatusImpl(accountsResponse = accountsResponse), ApplicationInfoImpl())
         return myAccountsPresenter
     }
 }
--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsPresenter.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/MyAccountsPresenter.kt
@@ -1,7 +1,10 @@
 package za.co.woolworths.financial.services.android.ui.fragments.account
 
 import za.co.woolworths.financial.services.android.ui.fragments.account.apply_now.ViewApplicationStatusInterface
+import za.co.woolworths.financial.services.android.util.application.ApplicationInfoInterface
 
-class MyAccountsPresenter(private val viewApplicationStatus: ViewApplicationStatusInterface) :
-    ViewApplicationStatusInterface by viewApplicationStatus
+class MyAccountsPresenter(private val viewApplicationStatus: ViewApplicationStatusInterface,
+private val appVersion: ApplicationInfoInterface) :
+    ViewApplicationStatusInterface by viewApplicationStatus,
+        ApplicationInfoInterface by appVersion
 
