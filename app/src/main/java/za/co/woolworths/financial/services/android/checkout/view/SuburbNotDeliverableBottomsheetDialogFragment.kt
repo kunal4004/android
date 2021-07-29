a26fac7ab64d1489b34a47cc785be0876bfef5b0
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2021-07-29 13:38:11 +0530

    Added suburb not deliverable popup.

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/SuburbNotDeliverableBottomsheetDialogFragment.kt
@@ -0,0 +1,56 @@
+package za.co.woolworths.financial.services.android.checkout.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.credit_card_cancel_delivery_confirmation_dialog.*
+import kotlinx.android.synthetic.main.credit_card_cancel_delivery_confirmation_dialog.callCallCenter
+import kotlinx.android.synthetic.main.fragment_suburb_not_deliverable_bottomsheet_dialog.*
+import za.co.woolworths.financial.services.android.models.WoolworthsApplication
+import za.co.woolworths.financial.services.android.ui.activities.credit_card_delivery.CreditCardDeliveryActivity
+import za.co.woolworths.financial.services.android.ui.extension.withArgs
+import za.co.woolworths.financial.services.android.ui.fragments.credit_card_delivery.CancelOrToLateDeliveryDialog
+import za.co.woolworths.financial.services.android.ui.views.actionsheet.WBottomSheetDialogFragment
+import za.co.woolworths.financial.services.android.util.Utils
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SuburbNotDeliverableBottomsheetDialogFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SuburbNotDeliverableBottomsheetDialogFragment : WBottomSheetDialogFragment(),
+    View.OnClickListener {
+
+    companion object {
+        fun newInstance() = SuburbNotDeliverableBottomsheetDialogFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_suburb_not_deliverable_bottomsheet_dialog,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        tvDismiss.setOnClickListener(this)
+        buttonChangeAddress.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        dismiss()
+    }
+}
