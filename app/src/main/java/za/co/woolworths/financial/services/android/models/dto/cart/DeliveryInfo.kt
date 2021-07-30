20ec9fb98bfa76785dba156b59bf961b5e3f18d8
jyotsna <jyotsanr@yoyowallet.com>
2021-07-30 15:26:13 +0400

    [WOP-11978] response classes

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/cart/DeliveryInfo.kt
@@ -0,0 +1,9 @@
+package za.co.woolworths.financial.services.android.models.dto.cart
+
+class DeliveryInfo {
+    var date: String = ""
+    var slotId: String = ""
+    var time: String = ""
+    var type: String = ""
+    var deliveryDateAndTime: String = ""
+}
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/cart/OrderItem.kt
@@ -0,0 +1,27 @@
+package za.co.woolworths.financial.services.android.models.dto.cart
+
+class OrderItem {
+    var commerceItemInfo: CommerceItemInfo? = null
+    var brandName: String = ""
+    var quantity: Int = 0
+    var productId: String = ""
+    var color: String = ""
+    var primarySize: String = ""
+    var internalSwatchImage: String = ""
+    var isGWP: Boolean? =null
+    var promotionalMessage: String = ""
+    var internalImageURL: String = ""
+    var commerceItemClassType: String = ""
+    var catalogRefId: String = ""
+    var fulfillmentType: String = ""
+    var thresholdQuantity: String = ""
+    var priceInfo: PriceInfo? = null
+    var size: String = ""
+    var productseoURL: String = ""
+    var externalImageURL: String = ""
+    var productDisplayName: String = ""
+    var siteId: String = ""
+    var fulfillerType: String = ""
+    var id: String = ""
+    var externalSwatchImageURL: String = ""
+}
