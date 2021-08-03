5be3b9c09222010263b78fe09a00d96c350b279f
jyotsna <jyotsanr@yoyowallet.com>
2021-08-03 10:54:19 +0400

    [WOP-11978] delivery or collection details

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/product/shop/OrderConfirmationFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/product/shop/OrderConfirmationFragment.kt
@@ -1,17 +1,24 @@
 package za.co.woolworths.financial.services.android.ui.fragments.product.shop
 
+import android.graphics.Typeface.BOLD
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.delivering_to_collection_from.*
 import kotlinx.android.synthetic.main.fragment_order_confirmation.*
 import za.co.woolworths.financial.services.android.contracts.IResponseListener
 import za.co.woolworths.financial.services.android.models.dto.cart.SubmittedOrderResponse
 import za.co.woolworths.financial.services.android.models.network.CompletionHandler
 import za.co.woolworths.financial.services.android.models.network.OneAppService
 import za.co.woolworths.financial.services.android.ui.extension.bindString
+
 
 class OrderConfirmationFragment : Fragment()  {
 
@@ -31,6 +38,8 @@ class OrderConfirmationFragment : Fragment()  {
             .enqueue(CompletionHandler(object : IResponseListener<SubmittedOrderResponse> {
                 override fun onSuccess(response: SubmittedOrderResponse?) {
                     response?.orderSummary?.orderId?.let { setToolbar(it) }
+
+                    setupDeliveryOrCollectionDetails(response)
                 }
 
                 override fun onFailure(error: Throwable?) {
@@ -43,4 +52,42 @@ class OrderConfirmationFragment : Fragment()  {
         orderIdTextView.text = bindString(R.string.order_details_toolbar_title, orderId)
     }
 
+    private fun setupDeliveryOrCollectionDetails(response: SubmittedOrderResponse?) {
+        context?.let {
+            deliveryCollectionDetails.visibility = View.VISIBLE
+            if (response?.orderSummary?.store?.name != null) {
+                optionImage.background =
+                    AppCompatResources.getDrawable(it, R.drawable.icon_collection_grey_bg)
+                optionTitle.text = it.getText(R.string.collecting_from)
+                optionLocation.text = response.orderSummary?.store?.name
+
+            } else {
+                optionImage.background =
+                    AppCompatResources.getDrawable(it, R.drawable.icon_delivery_grey_bg)
+                optionTitle.text = it.getText(R.string.delivering_to)
+                optionLocation.text = response?.deliveryDetails?.shippingAddress?.address1
+            }
+
+            if(response?.deliveryDetails?.deliveryInfos?.size!! >= 1){
+                val splitDateTime = response.deliveryDetails?.deliveryInfos?.get(0)?.deliveryDateAndTime?.split(",", ignoreCase = false, limit = 2)
+
+                if(splitDateTime?.size == 2){
+                    val wordSpan: Spannable =
+                        SpannableString(response.deliveryDetails?.deliveryInfos?.get(0)?.deliveryDateAndTime)
+
+                    wordSpan.setSpan(
+                        StyleSpan(BOLD),
+                        0,
+                        splitDateTime[0].length+1,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                    deliveryDateTime.text = wordSpan
+                }
+                else{
+                    deliveryDateTime.text =
+                        response.deliveryDetails?.deliveryInfos?.get(0)?.deliveryDateAndTime
+                }
+            }
+        }
+    }
 }
