91b49e1b16127732806091b656ecb819afe4532a
bhagyashripawar <bhagyashripawar@woolworths.co.za>
2021-09-21 11:51:18 +0530

    WOP-12319

--- app/src/main/java/za/co/woolworths/financial/services/android/util/ProductTypeDetails.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/ProductTypeDetails.kt
@@ -1,5 +1,5 @@
 package za.co.woolworths.financial.services.android.util
 
-enum class ProductTypeDetails(val value: String, val longHeader: String, val shortHeader: String) {
-    DEFAULT("default", "YOUR GENERAL ITEM", "GENERAL"), HOME_COMMERCE_ITEM("home", "YOUR HOME ITEM", "HOME"), CLOTHING_COMMERCE_ITEM("clothing", "YOUR CLOTHING ITEM", "CLOTHING"), PREMIUM_BRAND_COMMERCE_ITEM("premium", "YOUR PREMIUM BRAND ITEM", "PREMIUM BRAND"), FOOD_COMMERCE_ITEM("food", "YOUR FOOD ITEM", "FOOD"), GIFT_COMMERCE_ITEM("gwp", "YOUR GIFT ITEM", "GIFT"), OTHER_ITEMS("", "YOUR OTHER ITEM", "OTHER")
+enum class ProductTypeDetails(val value: String, val longHeader: String) {
+    DEFAULT("default", "YOUR GENERAL ITEM"), HOME_COMMERCE_ITEM("home", "YOUR HOME ITEM"), CLOTHING_COMMERCE_ITEM("clothing", "YOUR CLOTHING ITEM"), PREMIUM_BRAND_COMMERCE_ITEM("premium", "YOUR PREMIUM BRAND ITEM"), FOOD_COMMERCE_ITEM("food", "YOUR FOOD ITEM"), OTHER_ITEMS("", "YOUR OTHER ITEM")
 }
