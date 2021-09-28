e39efe740b148a5f8526c259ace7619f83532817
Dimitri Jacques <dimitri.jacques.pro@gmail.com>
2021-09-28 10:27:34 +0400

    Text should be Credit Card for credit card section

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/detail/pay_my_account/PayMyAccountViewModel.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/detail/pay_my_account/PayMyAccountViewModel.kt
@@ -211,7 +211,8 @@ class PayMyAccountViewModel : ViewModel() {
 
     fun getProductLabelId() = when (getApplyNowState()) {
         ApplyNowState.STORE_CARD -> R.string.store_card_title
-        else -> R.string.personalLoanCard_title
+        ApplyNowState.PERSONAL_LOAN -> R.string.personalLoanCard_title
+        else ->  R.string.credit_card_title
     }
 
     fun getAccountWithApplyNowState() = getCardDetail()?.account
