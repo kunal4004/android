4a5abb74cbf05f13dcd3b9478d8924c7219f179c
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2021-09-28 11:46:37 +0530

    Removed unnecessary classes as the response of shipping details changed.

--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/service/network/DiscountDetails.kt
+++
@@ -1,9 +0,0 @@
-package za.co.woolworths.financial.services.android.checkout.service.network
-
-import com.google.gson.annotations.SerializedName
-import java.io.Serializable
-
-class DiscountDetails : Serializable {
-    @SerializedName("totalOrderDiscount")
-    var totalOrderDiscount: Long? = null
-}
--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/service/network/FailedAttemptsSC.kt
+++
@@ -1,5 +0,0 @@
-package za.co.woolworths.financial.services.android.checkout.service.network
-
-import java.io.Serializable
-
-class FailedAttemptsSC : Serializable
