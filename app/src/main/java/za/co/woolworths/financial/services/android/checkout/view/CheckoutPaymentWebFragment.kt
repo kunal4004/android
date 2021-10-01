bf802e8c9e70438cf0b032f4c16beea34ed39576
Eesa Jacobs <eesaj@wigroup.co.za>
2021-10-01 11:13:21 +0200

    it works

--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutPaymentWebFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutPaymentWebFragment.kt
@@ -1,16 +1,10 @@
 package za.co.woolworths.financial.services.android.checkout.view
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.SpannableStringBuilder
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -21,15 +15,8 @@ import androidx.fragment.app.setFragmentResult
 import androidx.navigation.findNavController
 import com.awfs.coordination.R
 import kotlinx.android.synthetic.main.fragment_checkout_payment_web.*
-import okhttp3.OkHttpClient
 import za.co.woolworths.financial.services.android.checkout.service.network.ShippingDetailsResponse
 import za.co.woolworths.financial.services.android.models.WoolworthsApplication
-import za.co.woolworths.financial.services.android.util.SessionUtilities
-import android.webkit.WebResourceResponse
-
-import com.google.common.net.HttpHeaders
-import okhttp3.Request
-import okhttp3.Response
 
 
 class CheckoutPaymentWebFragment : Fragment(), WebViewCallbackListener {
@@ -82,26 +69,24 @@ class CheckoutPaymentWebFragment : Fragment(), WebViewCallbackListener {
                     false; // disable the default zoom controls on the page
 
                 webViewClient = CheckoutPaymentWebViewClient(this@CheckoutPaymentWebFragment)
-//                webViewClient = CustomWebViewClient(this@CheckoutPaymentWebFragment)
                 val paymentUrl = WoolworthsApplication.getNativeCheckout()?.checkoutPaymentURL ?: ""
                 val webTokens =
                     arguments?.getSerializable(KEY_ARGS_WEB_TOKEN) as? ShippingDetailsResponse
                 if (webTokens != null) {
 
                     val cookies = "TOKEN=${webTokens.jsessionId};AUTHENTICATION=${webTokens.auth}"
-                    Log.d("EJ", "New Cookies >> $cookies")
 
                     val cookieManager = CookieManager.getInstance()
                     cookieManager.acceptCookie()
+                    cookieManager.setAcceptThirdPartyCookies(this, true)
 
                     val uri = Uri.parse(paymentUrl)
 
                     cookies.split(";").forEach { item ->
-                        cookieManager.setCookie("${uri.scheme}://${uri.host}", "$item; path=${uri.path}; domain=${uri.host}")
+                        cookieManager.setCookie("${uri.scheme}://${uri.host}", item)
                     }
 
-                    cookieManager.setAcceptThirdPartyCookies(this, true)
-
+                    cookieManager.flush()
                     loadUrl(paymentUrl)
                 } else {
                     view?.findNavController()?.navigateUp()
@@ -112,30 +97,14 @@ class CheckoutPaymentWebFragment : Fragment(), WebViewCallbackListener {
 
     class CheckoutPaymentWebViewClient internal constructor(private val listener: WebViewCallbackListener): WebViewClient(){
 
-//        private var isPageReloadRequired = false
 
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
             super.onPageStarted(view, url, favicon)
-
-            view?.evaluateJavascript("(function(){return document.cookie; })()") { cookie ->
-                Log.d("EJ", "onPageStarted Document Cookie >> $cookie")
-//                if ("null".equals(cookie)){
-//                    isPageReloadRequired = true
-//                }
-            }
             listener.onPageStarted(view, url, favicon)
         }
 
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
-
-            view?.evaluateJavascript("(function(){return document.URL + document.cookie; })()") { cookie ->
-                Log.d("EJ", "onPageFinished Document Cookie >> $cookie")
-//                if (!"null".equals(cookie) && isPageReloadRequired){
-//                    isPageReloadRequired = false
-//                    view?.reload()
-//                }
-            }
 
             listener.onPageFinished(view, url)
         }
@@ -155,8 +124,8 @@ class CheckoutPaymentWebFragment : Fragment(), WebViewCallbackListener {
     }
 
     private fun clearCookies() {
-        CookieManager.getInstance().removeSessionCookies(null)
         CookieManager.getInstance().removeAllCookies(null)
+        CookieManager.getInstance().removeSessionCookies(null)
         CookieManager.getInstance().flush()
     }
 
