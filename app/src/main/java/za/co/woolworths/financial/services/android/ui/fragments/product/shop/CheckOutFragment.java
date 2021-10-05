6f7c6fa4e202f73922625fbc0aafb1fa932ee75b
KU59319 <>
2021-10-05 10:36:32 +0530

    indentations

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/product/shop/CheckOutFragment.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/product/shop/CheckOutFragment.java
@@ -6,9 +6,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -48,149 +50,150 @@ import za.co.woolworths.financial.services.android.util.Utils;
 
 public class CheckOutFragment extends Fragment {
 
-	public static int REQUEST_CART_REFRESH_ON_DESTROY = 9;
-	public static String ORDER_CONFIRMATION = "order-confirmation.jsp";
+    public static int REQUEST_CART_REFRESH_ON_DESTROY = 9;
+    public static String ORDER_CONFIRMATION = "order-confirmation.jsp";
 
-	private enum QueryString {
-		COMPLETE("goto=complete"),
-		ABANDON("goto=abandon");
+    private enum QueryString {
+        COMPLETE("goto=complete"),
+        ABANDON("goto=abandon");
 
-		private String value;
+        private String value;
 
-		QueryString(String value) {
-			this.value = value;
-		}
+        QueryString(String value) {
+            this.value = value;
+        }
 
-		public String getValue() {
-			return value;
-		}
-	}
+        public String getValue() {
+            return value;
+        }
+    }
 
-	private WebView mWebCheckOut;
-	private String TAG = this.getClass().getSimpleName();
-	private ProgressBar mProgressLayout;
-	private ErrorHandlerView mErrorHandlerView;
-	private QueryString closeOnNextPage;
+    private WebView mWebCheckOut;
+    private String TAG = this.getClass().getSimpleName();
+    private ProgressBar mProgressLayout;
+    private ErrorHandlerView mErrorHandlerView;
+    private QueryString closeOnNextPage;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.checkout_fragment, container, false);
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.checkout_fragment, container, false);
+    }
 
-	@SuppressLint({"SetJavaScriptEnabled", "JavascriptInterface"})
-	@Override
-	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		mWebCheckOut = view.findViewById(R.id.webCheckout);
-		mProgressLayout = view.findViewById(R.id.progressCreditLimit);
-		mErrorHandlerView = new ErrorHandlerView(getActivity(), (RelativeLayout) view.findViewById(R.id.no_connection_layout));
-		mErrorHandlerView.setMargin(view.findViewById(R.id.no_connection_layout), 0, 0, 0, 0);
-		setWebSetting(mWebCheckOut);
-		Activity activity = getActivity();
-		if (activity != null) {
-			setWebViewClient();
-			setWebChromeClient();
-			mWebCheckOut.loadUrl(getUrl(), getExtraHeader());
-			retryConnect(view);
-		}
-	}
+    @SuppressLint({"SetJavaScriptEnabled", "JavascriptInterface"})
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mWebCheckOut = view.findViewById(R.id.webCheckout);
+        mProgressLayout = view.findViewById(R.id.progressCreditLimit);
+        mErrorHandlerView = new ErrorHandlerView(getActivity(), (RelativeLayout) view.findViewById(R.id.no_connection_layout));
+        mErrorHandlerView.setMargin(view.findViewById(R.id.no_connection_layout), 0, 0, 0, 0);
+        setWebSetting(mWebCheckOut);
+        Activity activity = getActivity();
+        if (activity != null) {
+            setWebViewClient();
+            setWebChromeClient();
+            mWebCheckOut.loadUrl(getUrl(), getExtraHeader());
+            retryConnect(view);
+        }
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		Utils.setScreenName(getActivity(), FirebaseManagerAnalyticsProperties.ScreenNames.CART_CHECKOUT);
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        Utils.setScreenName(getActivity(), FirebaseManagerAnalyticsProperties.ScreenNames.CART_CHECKOUT);
+    }
 
-	@NonNull
-	private String getUrl() {
-		return WoolworthsApplication.getCartCheckoutLinkWithParams();
-	}
+    @NonNull
+    private String getUrl() {
+        return WoolworthsApplication.getCartCheckoutLinkWithParams();
+    }
 
-	@SuppressLint("SetJavaScriptEnabled")
-	private void setWebSetting(WebView webCheckout) {
-		WebSettings ws = webCheckout.getSettings();
-		ws.setJavaScriptEnabled(true);
-		ws.setUseWideViewPort(true);
-		ws.setLoadWithOverviewMode(true);
-		ws.setDomStorageEnabled(true);
-		clearWebViewCachesCustom(getActivity());
-		clearCookie();
-		ws.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
-	}
+    @SuppressLint("SetJavaScriptEnabled")
+    private void setWebSetting(WebView webCheckout) {
+        WebSettings ws = webCheckout.getSettings();
+        ws.setJavaScriptEnabled(true);
+        ws.setUseWideViewPort(true);
+        ws.setLoadWithOverviewMode(true);
+        ws.setDomStorageEnabled(true);
+        clearWebViewCachesCustom(getActivity());
+        clearCookie();
+        ws.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+    }
 
-	private void clearCookie() {
-		CookieManager cookieManager = CookieManager.getInstance();
+    private void clearCookie() {
+        CookieManager cookieManager = CookieManager.getInstance();
 
-		// a callback which is executed when the cookies have been removed
-		cookieManager.removeAllCookies(aBoolean -> { });
-	}
+        // a callback which is executed when the cookies have been removed
+        cookieManager.removeAllCookies(aBoolean -> {
+        });
+    }
 
-	private void setWebViewClient() {
-		mProgressLayout.setVisibility(View.VISIBLE);
-		mWebCheckOut.setWebViewClient(new WebViewClient() {
-			@TargetApi(android.os.Build.VERSION_CODES.M)
-			@Override
-			public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-				super.onReceivedError(view, request, error);
+    private void setWebViewClient() {
+        mProgressLayout.setVisibility(View.VISIBLE);
+        mWebCheckOut.setWebViewClient(new WebViewClient() {
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
                 handleNetworkConnectionError(view, error.getErrorCode(), error.toString());
             }
 
-			@SuppressWarnings("deprecation")
-			@Override
-			public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
                 handleNetworkConnectionError(webView, errorCode, description);
             }
 
-			@Override
-			public void onLoadResource(WebView view, String url) {
-				super.onLoadResource(view, url);
-			}
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+            }
 
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				Activity activity = getActivity();
-				if (activity == null) return true;
-				if (url.contains(QueryString.COMPLETE.getValue())) {
-					Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_COMPLETE, activity);
-					closeOnNextPage = QueryString.COMPLETE;
-				} else if (url.contains(QueryString.ABANDON.getValue())) {
-					Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_ABANDON, activity);
-					closeOnNextPage = QueryString.ABANDON;
-				}
-				view.loadUrl(url);
-				return true;
-			}
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Activity activity = getActivity();
+                if (activity == null) return true;
+                if (url.contains(QueryString.COMPLETE.getValue())) {
+                    Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_COMPLETE, activity);
+                    closeOnNextPage = QueryString.COMPLETE;
+                } else if (url.contains(QueryString.ABANDON.getValue())) {
+                    Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_ABANDON, activity);
+                    closeOnNextPage = QueryString.ABANDON;
+                }
+                view.loadUrl(url);
+                return true;
+            }
 
-			@Override
-			public void onPageStarted(WebView view, String url, Bitmap favicon) {
-				Activity activity = getActivity();
-				if (activity == null) return;
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                Activity activity = getActivity();
+                if (activity == null) return;
 
-				if (url.contains(QueryString.COMPLETE.getValue())) {
-					Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_COMPLETE, activity);
-					closeOnNextPage = QueryString.COMPLETE;
-				} else if (url.contains(QueryString.ABANDON.getValue())) {
-					Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_ABANDON, activity);
-					closeOnNextPage = QueryString.ABANDON;
-				} else if (url.contains(ORDER_CONFIRMATION)) {
-					Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_ORDER_CONFIRMATION, activity);
-					initPostCheckout();
-				}
-				// close cart activity if current url equals next url
-				if (closeOnNextPage != null && !url.contains(closeOnNextPage.getValue())) {
-					mWebCheckOut.stopLoading();
-					finishCartActivity();
-				}
-			}
+                if (url.contains(QueryString.COMPLETE.getValue())) {
+                    Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_COMPLETE, activity);
+                    closeOnNextPage = QueryString.COMPLETE;
+                } else if (url.contains(QueryString.ABANDON.getValue())) {
+                    Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_CHECKOUT_ABANDON, activity);
+                    closeOnNextPage = QueryString.ABANDON;
+                } else if (url.contains(ORDER_CONFIRMATION)) {
+                    Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.CART_ORDER_CONFIRMATION, activity);
+                    initPostCheckout();
+                }
+                // close cart activity if current url equals next url
+                if (closeOnNextPage != null && !url.contains(closeOnNextPage.getValue())) {
+                    mWebCheckOut.stopLoading();
+                    finishCartActivity();
+                }
+            }
 
-			public void onPageFinished(WebView view, String url) {
-				mProgressLayout.setVisibility(View.GONE);
-				if (closeOnNextPage != null && !url.contains(closeOnNextPage.getValue())) {
-					finishCartActivity();
-				}
-			}
-		});
-	}
+            public void onPageFinished(WebView view, String url) {
+                mProgressLayout.setVisibility(View.GONE);
+                if (closeOnNextPage != null && !url.contains(closeOnNextPage.getValue())) {
+                    finishCartActivity();
+                }
+            }
+        });
+    }
 
     private void handleNetworkConnectionError(WebView view, int errorCode, String s) {
         switch (errorCode) {
@@ -205,92 +208,91 @@ public class CheckOutFragment extends Fragment {
     }
 
     private void finishCartActivity() {
-		Activity activity = getActivity();
-		if (activity != null) {
-			if (closeOnNextPage == QueryString.COMPLETE) {
-				activity.setResult(Activity.RESULT_OK);
-			} else if (closeOnNextPage == QueryString.ABANDON) {
-				activity.setResult(Activity.RESULT_CANCELED);
-			}
-			activity.finish();
-			activity.overridePendingTransition(R.anim.slide_down_anim, R.anim.stay);
-		}
-	}
+        Activity activity = getActivity();
+        if (activity != null) {
+            if (closeOnNextPage == QueryString.COMPLETE) {
+                activity.setResult(Activity.RESULT_OK);
+            } else if (closeOnNextPage == QueryString.ABANDON) {
+                activity.setResult(Activity.RESULT_CANCELED);
+            }
+            activity.finish();
+            activity.overridePendingTransition(R.anim.slide_down_anim, R.anim.stay);
+        }
+    }
 
-	private void setWebChromeClient() {
-		mWebCheckOut.setWebChromeClient(new WebChromeClient() {
+    private void setWebChromeClient() {
+        mWebCheckOut.setWebChromeClient(new WebChromeClient() {
 
-			@Override
-			public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
-				Log.e("onJsPrompt", String.valueOf(result) + " message " + message + " url " + url);
-				return super.onJsPrompt(view, url, message, defaultValue, result);
-			}
+            @Override
+            public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
+                Log.e("onJsPrompt", String.valueOf(result) + " message " + message + " url " + url);
+                return super.onJsPrompt(view, url, message, defaultValue, result);
+            }
 
-			@Override
-			public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-				Log.e("onJsAlert", message);
-				result.confirm();
-				return true;
-			}
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Log.e("onJsAlert", message);
+                result.confirm();
+                return true;
+            }
 
-			@Override
-			public boolean onConsoleMessage(ConsoleMessage cm) {
-				Log.e(TAG, String.format("%s @ %d: %s", cm.message(),
-						cm.lineNumber(), cm.sourceId()));
-				return true;
-			}
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage cm) {
+                Log.e(TAG, String.format("%s @ %d: %s", cm.message(),
+                        cm.lineNumber(), cm.sourceId()));
+                return true;
+            }
 
-			@Override
-			public void onReceivedTitle(WebView view, String title) {
-				super.onReceivedTitle(view, title);
-				Log.e("currentTitle", title);
-			}
-		});
-	}
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                Log.e("currentTitle", title);
+            }
+        });
+    }
 
 
-	public static void clearWebViewCachesCustom(Context context) {
-		try {
-			String dataDir = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).applicationInfo.dataDir;
-			new File(dataDir + "/app_webview/").delete();
-		} catch (Exception e) {
-			Log.e("clearWebViewCaches", e.getMessage());
-		}
-	}
+    public static void clearWebViewCachesCustom(Context context) {
+        try {
+            String dataDir = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).applicationInfo.dataDir;
+            new File(dataDir + "/app_webview/").delete();
+        } catch (Exception e) {
+            Log.e("clearWebViewCaches", e.getMessage());
+        }
+    }
 
-	private void retryConnect(View view) {
-		view.findViewById(R.id.btnRetry).setOnClickListener(v -> {
-			if (NetworkManager.getInstance().isConnectedToNetwork(getActivity())) {
-				mErrorHandlerView.hideErrorHandler();
-				mProgressLayout.setVisibility(View.VISIBLE);
-				mWebCheckOut.loadUrl(getUrl(), getExtraHeader());
-			}
-		});
-	}
+    private void retryConnect(View view) {
+        view.findViewById(R.id.btnRetry).setOnClickListener(v -> {
+            if (NetworkManager.getInstance().isConnectedToNetwork(getActivity())) {
+                mErrorHandlerView.hideErrorHandler();
+                mProgressLayout.setVisibility(View.VISIBLE);
+                mWebCheckOut.loadUrl(getUrl(), getExtraHeader());
+            }
+        });
+    }
 
-	private Map<String, String> getExtraHeader() {
-		Map<String, String> extraHeaders = new HashMap<>();
-		Activity activity = getActivity();
-		if (activity != null) {
-			extraHeaders.put("token", SessionUtilities.getInstance().getSessionToken());
-		}
-		return extraHeaders;
-	}
+    private Map<String, String> getExtraHeader() {
+        Map<String, String> extraHeaders = new HashMap<>();
+        Activity activity = getActivity();
+        if (activity != null) {
+            extraHeaders.put("token", SessionUtilities.getInstance().getSessionToken());
+        }
+        return extraHeaders;
+    }
 
-	public void initPostCheckout()
-	{
-		QueryBadgeCounter.getInstance().setCartCount(0);
-		ShoppingDeliveryLocation preferredDeliveryLocation = Utils.getPreferredDeliveryLocation();
-		Call<Void> checkoutSuccess = OneAppService.INSTANCE.postCheckoutSuccess(new CheckoutSuccess(preferredDeliveryLocation.storePickup ? preferredDeliveryLocation.store.getId() : preferredDeliveryLocation.suburb.id));
-		checkoutSuccess.enqueue(new CompletionHandler<>(new IResponseListener<Void>() {
-			@Override
-			public void onSuccess(Void response) {
-			}
+    public void initPostCheckout() {
+        QueryBadgeCounter.getInstance().setCartCount(0);
+        ShoppingDeliveryLocation preferredDeliveryLocation = Utils.getPreferredDeliveryLocation();
+        Call<Void> checkoutSuccess = OneAppService.INSTANCE.postCheckoutSuccess(new CheckoutSuccess(preferredDeliveryLocation.storePickup ? preferredDeliveryLocation.store.getId() : preferredDeliveryLocation.suburb.id));
+        checkoutSuccess.enqueue(new CompletionHandler<>(new IResponseListener<Void>() {
+            @Override
+            public void onSuccess(Void response) {
+            }
 
-			@Override
-			public void onFailure(Throwable error) {
+            @Override
+            public void onFailure(Throwable error) {
 
-			}
-		},Void.class));
-	}
+            }
+        }, Void.class));
+    }
 }
