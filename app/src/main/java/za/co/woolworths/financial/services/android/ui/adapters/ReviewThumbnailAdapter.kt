10f5a5ca05e99f76601b6179d10f687b31ea83e2
jitendrajswl <jitendrajaiswal@woolworths.co.za>
2021-10-21 23:43:53 +0530

    plus icon with gradient added on third image of review

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/ReviewThumbnailAdapter.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/ReviewThumbnailAdapter.kt
@@ -3,10 +3,14 @@ package za.co.woolworths.financial.services.android.ui.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.review_thumbnail_row_item.view.*
 import za.co.woolworths.financial.services.android.models.dto.Thumbnail
 
 class ReviewThumbnailAdapter(var context: Context, var thumbnailClickListener: ThumbnailClickListener) : RecyclerView.Adapter<ReviewThumbnailAdapter.ViewHolder>() {
@@ -22,20 +26,25 @@ class ReviewThumbnailAdapter(var context: Context, var thumbnailClickListener: T
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var image: ImageView = itemView.findViewById(R.id.image)
+        var flShowMore: FrameLayout = itemView.findViewById(R.id.flShowMore)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.review_thumbnail_row_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.review_thumbnail_row_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var data = dataList[position]
+        val data = dataList[position]
         holder.image.setImageResource(data.url)
+        if (position == 2 && itemCount==3)
+            holder.flShowMore.visibility = VISIBLE
         holder.image.setOnClickListener(View.OnClickListener {
-            if (position == 2)
+            if (position == 2) {
+                holder.flShowMore.visibility = GONE
                 thumbnailClickListener.thumbnailClicked()
+            }
         })
     }
 
