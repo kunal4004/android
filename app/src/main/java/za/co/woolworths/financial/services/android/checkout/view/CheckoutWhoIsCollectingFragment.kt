530b529fe04146133dd0dfcd0688b4697d71404c
KU59319 <>
2021-10-27 11:55:56 +0530

    Edit text Validations

--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutWhoIsCollectingFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutWhoIsCollectingFragment.kt
@@ -6,12 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import com.awfs.coordination.R
 import kotlinx.android.synthetic.main.checkout_new_user_recipient_details.*
+import kotlinx.android.synthetic.main.vehicle_details_layout.*
+import za.co.woolworths.financial.services.android.ui.extension.afterTextChanged
 import za.co.woolworths.financial.services.android.ui.extension.bindString
+import java.util.regex.Pattern
 
 /**
  * Created by Kunal Uttarwar on 26/10/21.
  */
 class CheckoutWhoIsCollectingFragment : CheckoutAddressManagementBaseFragment() {
+
+    companion object {
+        const val REGEX_VEHICLE_TEXT: String = "^\$|^[a-zA-Z0-9\\s<!>@#\$&().+,-/\\\"']+\$"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,5 +35,49 @@ class CheckoutWhoIsCollectingFragment : CheckoutAddressManagementBaseFragment() 
 
     private fun initView() {
         recipientDetailsTitle.text = bindString(R.string.who_is_collecting)
+
+        recipientNameEditText?.apply {
+            afterTextChanged {
+                val length = it.length
+                if (length > 0 && !Pattern.matches(REGEX_VEHICLE_TEXT, it)) {
+                    text?.delete(length - 1, length)
+                }
+                /*if (it.isNotEmpty())
+                    showErrorInputField(this, View.GONE)*/
+            }
+        }
+
+        vehicleColourEditText?.apply {
+            afterTextChanged {
+                val length = it.length
+                if (length > 0 && !Pattern.matches(REGEX_VEHICLE_TEXT, it)) {
+                    text?.delete(length - 1, length)
+                }
+                /*if (it.isNotEmpty())
+                    showErrorInputField(this, View.GONE)*/
+            }
+        }
+
+        vehicleModelEditText?.apply {
+            afterTextChanged {
+                val length = it.length
+                if (length > 0 && !Pattern.matches(REGEX_VEHICLE_TEXT, it)) {
+                    text?.delete(length - 1, length)
+                }
+                /*if (it.isNotEmpty())
+                    showErrorInputField(this, View.GONE)*/
+            }
+        }
+
+        vehicleRegistrationEditText?.apply {
+            afterTextChanged {
+                val length = it.length
+                if (length > 0 && !Pattern.matches(REGEX_VEHICLE_TEXT, it)) {
+                    text?.delete(length - 1, length)
+                }
+                /*if (it.isNotEmpty())
+                    showErrorInputField(this, View.GONE)*/
+            }
+        }
     }
 }
