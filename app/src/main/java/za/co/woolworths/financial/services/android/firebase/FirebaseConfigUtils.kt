fbcb926e89e1e304543cd40bbd401a9363f3ebea
AC62703 <anandchincholkar@woolworths.co.za>
2021-10-28 15:55:44 +0530

    reviewers comments resolved

--- app/src/main/java/za/co/woolworths/financial/services/android/firebase/FirebaseConfigUtils.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/firebase/FirebaseConfigUtils.kt
@@ -19,7 +19,6 @@ object FirebaseConfigUtils {
         try {
             jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
         } catch (ioException: IOException) {
-            ioException.printStackTrace()
             return null
         }
         return jsonString
