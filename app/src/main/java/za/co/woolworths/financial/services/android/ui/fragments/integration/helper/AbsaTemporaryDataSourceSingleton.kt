f16b88e100a9c354804c060a1121db2cb8abdfdd
Dimitri Jacques <DimitriJ@wigroupinternational.com>
2021-11-16 10:52:21 +0400

    Clean up, add absa login ui

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/integration/helper/AbsaTemporaryDataSourceSingleton.kt
@@ -0,0 +1,16 @@
+package za.co.woolworths.financial.services.android.ui.fragments.integration.helper
+
+import za.co.absa.openbankingapi.SessionKey
+
+object AbsaTemporaryDataSourceSingleton {
+    var cookie: String? = null
+    var deriveSeeds: ByteArray? = null
+    var keyId: String? = null
+    var contentLength: Int? = 0
+    var xEncrypted: String? = null
+    var deviceId: String? = null
+    var jsessionId: String? = null
+    var sessionKey: SessionKey? = null
+    var wfpt: String? = null
+    var xfpt:String? = null
+}
