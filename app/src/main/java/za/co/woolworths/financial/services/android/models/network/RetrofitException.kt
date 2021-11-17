9e828a6cf542e2be7458a7a09ad219b907065033
AC62703 <anandchincholkar@woolworths.co.za>
2021-11-17 22:20:12 +0530

    firebase config called for error scenario for mobile config api

--- app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitException.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitException.kt
@@ -3,7 +3,7 @@ package za.co.woolworths.financial.services.android.models.network
 import za.co.woolworths.financial.services.android.ui.activities.maintenance.NetworkRuntimeExceptionViewController
 import java.net.SocketTimeoutException
 
-class RetrofitException(private var exception: Any?) {
+class RetrofitException(private var url:String, private var exception: Any?) {
     fun show(): Boolean {
         return when {
             exception is SocketTimeoutException -> {
@@ -11,7 +11,9 @@ class RetrofitException(private var exception: Any?) {
                 true
             }
             (exception == 404) or (exception == 503) -> {
-                NetworkRuntimeExceptionViewController().openMaintenanceView()
+                if(!url.contains("mobileconfigs")) {
+                    NetworkRuntimeExceptionViewController().openMaintenanceView()
+                }
                 true
             }
             else -> false
