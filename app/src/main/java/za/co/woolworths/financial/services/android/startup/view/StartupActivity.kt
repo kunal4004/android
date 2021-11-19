04640235eaa3ec23d9a904fdcb6dd332af4398be
AC62703 <anandchincholkar@woolworths.co.za>
2021-11-19 11:44:12 +0530

    reviewers comment resolved

--- app/src/main/java/za/co/woolworths/financial/services/android/startup/view/StartupActivity.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/startup/view/StartupActivity.kt
@@ -9,7 +9,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import androidx.annotation.VisibleForTesting
