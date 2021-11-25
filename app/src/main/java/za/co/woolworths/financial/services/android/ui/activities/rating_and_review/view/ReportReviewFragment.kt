ee2af8ec62db89fd626c0c714646908ee0f47fd7
AC62703 <anandchincholkar@woolworths.co.za>
2021-11-25 14:37:18 +0530

    Report success screen refactored

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/rating_and_review/view/ReportReviewFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/rating_and_review/view/ReportReviewFragment.kt
@@ -1,27 +1,24 @@
 package za.co.woolworths.financial.services.android.ui.activities.rating_and_review.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.awfs.coordination.R
 import kotlinx.android.synthetic.main.fragment_report_review.*
-import kotlinx.android.synthetic.main.review_detail_layout.*
-import kotlinx.android.synthetic.main.skin_profile_layout.view.*
 import za.co.woolworths.financial.services.android.ui.activities.rating_and_review.view.adapter.ReportReviewsAdapter
-import za.co.woolworths.financial.services.android.ui.activities.rating_and_review.view.adapter.SkinProfileAdapter
 import za.co.woolworths.financial.services.android.util.KotlinUtils
-import java.util.ArrayList
 
 class ReportReviewFragment: Fragment() , ReportReviewsAdapter.ReportItemClick {
 
     companion object {
         fun newInstance() = ReportReviewFragment()
     }
+
+    var reportSuccessFragment: ReportSuccessFragment? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -47,6 +44,20 @@ class ReportReviewFragment: Fragment() , ReportReviewsAdapter.ReportItemClick {
         recyler_report.setLayoutManager(llm)
         val reportReviewsAdapter = ReportReviewsAdapter(reportReviewList, this)
         recyler_report.setAdapter(reportReviewsAdapter)
+        btn_submit_report.setOnClickListener {
+            openReportScreenFragment()
+        }
+    }
+
+    private fun openReportScreenFragment() {
+        activity?.apply {
+            reportSuccessFragment = ReportSuccessFragment.newInstance()
+            val fragmentManager = getSupportFragmentManager()
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.content_main_frame, reportSuccessFragment!!)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
 
     override fun reportItemClicked(reportItem: String, isChecked: Boolean) {
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/rating_and_review/view/ReportSuccessFragment.kt
@@ -0,0 +1,27 @@
+package za.co.woolworths.financial.services.android.ui.activities.rating_and_review.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.awfs.coordination.R
+
+class ReportSuccessFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ReportSuccessFragment()
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_report_review_success, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
