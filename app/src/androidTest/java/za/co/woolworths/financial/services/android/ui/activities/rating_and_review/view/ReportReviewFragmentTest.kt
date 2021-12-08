51df05a0d4be28064531c07ff48ecd82e03cca13
AC62703 <anandchincholkar@woolworths.co.za>
2021-12-08 14:18:35 +0530

    updated

---
+++ app/src/androidTest/java/za/co/woolworths/financial/services/android/ui/activities/rating_and_review/view/ReportReviewFragmentTest.kt
@@ -0,0 +1,8 @@
+package za.co.woolworths.financial.services.android.ui.activities.rating_and_review.view
+
+import org.junit.jupiter.api.Assertions.*
+
+class ReportReviewFragmentTest {
+
+
+ }
---
+++ app/src/androidTest/java/za/co/woolworths/financial/services/android/ui/activities/rating_and_review/view/ReportSuccessFragmentTest.kt
@@ -0,0 +1,6 @@
+package za.co.woolworths.financial.services.android.ui.activities.rating_and_review.view
+
+class ReportSuccessFragmentTest {
+
+
+}
