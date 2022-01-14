583024b353df7139c0f727bc6e3af55dca8a1503
Dimitri Jacques <dimitri.jacques.pro@gmail.com>
2022-01-14 17:11:40 +0400

    Otp Error message not showing up

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/bpi/presentation/opt_in/otp/BpiEnterOtpFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/bpi/presentation/opt_in/otp/BpiEnterOtpFragment.kt
@@ -2,7 +2,6 @@ package za.co.woolworths.financial.services.android.ui.fragments.bpi.presentatio
 
 import android.graphics.Paint
 import android.os.Bundle
-import android.text.InputType
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -17,11 +16,10 @@ import com.awfs.coordination.R
 import kotlinx.android.synthetic.main.fragment_enter_otp.*
 import za.co.woolworths.financial.services.android.models.dto.npc.OTPMethodType
 import za.co.woolworths.financial.services.android.ui.fragments.bpi.presentation.BalanceProtectionInsuranceActivity
-import za.co.woolworths.financial.services.android.ui.fragments.otp.EnterOTPFragmentExtension
 import za.co.woolworths.financial.services.android.ui.fragments.otp.ResendOTPDialogFragment
 
 
-class BpiEnterOtpFragment : EnterOTPFragmentExtension(),
+class BpiEnterOtpFragment : BPIEnterOTPFragmentExtension(),
     ResendOTPDialogFragment.IResendOTPOptionSelection {
 
     private var navController: NavController? = null
@@ -53,6 +51,11 @@ class BpiEnterOtpFragment : EnterOTPFragmentExtension(),
         clickEvent()
         configureUI()
         shouldBackPressed = true
+        setOnFocusChangeListener(edtVerificationCode1)
+        setOnFocusChangeListener(edtVerificationCode2)
+        setOnFocusChangeListener(edtVerificationCode3)
+        setOnFocusChangeListener(edtVerificationCode4)
+        setOnFocusChangeListener(edtVerificationCode5)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -113,15 +116,7 @@ class BpiEnterOtpFragment : EnterOTPFragmentExtension(),
         }
     }
 
-    private fun requestEditTextFocus() {
-        (activity as? AppCompatActivity)?.let { activity ->
-            edtVerificationCode1?.apply {
-                requestFocus()
-                showSoftKeyboard(activity, this)
-                inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            }
-        }
-    }
+
 
     private fun showWrongOTP() {
         if (!TextUtils.isEmpty(otpValue)) {
@@ -136,12 +131,9 @@ class BpiEnterOtpFragment : EnterOTPFragmentExtension(),
         }
         otpErrorTextView?.visibility = View.VISIBLE
         setOtpErrorBackground(R.drawable.otp_box_error_background)
+        removeFocus()
     }
 
-    override fun onResume() {
-        super.onResume()
-        requestEditTextFocus()
-    }
 
     override fun onDestroy() {
         super.onDestroy()
