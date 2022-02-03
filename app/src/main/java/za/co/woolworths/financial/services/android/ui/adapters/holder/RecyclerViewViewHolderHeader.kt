cf201bbeca7043fd8a3f2d35c08b5c4f27a145e6
AC62703 <anandchincholkar@woolworths.co.za>
2022-02-03 10:20:10 +0530

    PLP : chanel banner added

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/holder/RecyclerViewViewHolderHeader.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/adapters/holder/RecyclerViewViewHolderHeader.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import com.awfs.coordination.R
+import kotlinx.android.synthetic.main.chanel_logo_view.view.*
 import kotlinx.android.synthetic.main.item_found_layout.view.*
 import za.co.woolworths.financial.services.android.models.dto.ProductList
 
@@ -11,6 +12,7 @@ class RecyclerViewViewHolderHeader(parent: ViewGroup) : RecyclerViewViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.item_found_layout, parent, false)
 ) {
     fun setNumberOfItems(activity: FragmentActivity?, productList: ProductList?) {
+        itemView?.chanel_logo_header?.tv_logo_name?.text = "CHANEL"
         when (productList?.numberOfItems) {
             1 -> productList?.numberOfItems?.toString()?.let { numberOfItems ->
                 itemView.tvNumberOfItem.text = numberOfItems; itemView.tvFoundItem.text =
