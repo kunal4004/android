4c1769e8596b4dd76afc1bfc64f5c82b615a899f
KU59319 <>
2022-02-14 19:08:04 +0530

    BLP Unit test case

--- app/src/test/java/za/co/woolworths/financial/services/android/chanel/ProductListingFragmentTest.kt
+++ app/src/test/java/za/co/woolworths/financial/services/android/chanel/ProductListingFragmentTest.kt
@@ -1,22 +1,20 @@
 package za.co.woolworths.financial.services.android.chanel
 
-import android.content.Context
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert
 import org.junit.Before
-import org.junit.Rule
+import org.junit.Ignore
 import org.junit.Test
-import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.mockito.Mockito
-import org.powermock.modules.junit4.PowerMockRunner
+import org.mockito.Mockito.CALLS_REAL_METHODS
+import org.mockito.junit.MockitoJUnitRunner
+import org.powermock.api.mockito.PowerMockito
 import za.co.woolworths.financial.services.android.models.dto.ProductList
 import za.co.woolworths.financial.services.android.models.dto.ProductView
+import za.co.woolworths.financial.services.android.models.dto.brandlandingpage.Navigation
+import za.co.woolworths.financial.services.android.ui.activities.dashboard.BottomNavigationActivity
 import za.co.woolworths.financial.services.android.ui.fragments.product.grid.ProductListingFragment
-import za.co.woolworths.financial.services.android.utils.TestCoroutineRule
 import za.co.woolworths.financial.services.android.utils.mock
 
 
@@ -24,23 +22,14 @@ import za.co.woolworths.financial.services.android.utils.mock
  * Created by Kunal Uttarwar on 10/02/22.
  */
 
-@ExperimentalCoroutinesApi
-@RunWith(PowerMockRunner::class)
+@RunWith(MockitoJUnitRunner::class)
 class ProductListingFragmentTest : Fragment() {
-    @get:Rule
-    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
-
-    @get:Rule
-    val testCoroutineRule = TestCoroutineRule()
-
     private lateinit var productListingFragment: ProductListingFragment
-    private lateinit var instrumentationContext: Context
 
     @Before
     fun init() {
         productListingFragment =
-            Mockito.mock(ProductListingFragment::class.java, Mockito.CALLS_REAL_METHODS)
-        instrumentationContext = Mockito.mock(Context::class.java, Mockito.RETURNS_DEEP_STUBS)
+            Mockito.mock(ProductListingFragment::class.java, CALLS_REAL_METHODS)
     }
 
     @Test
@@ -62,5 +51,24 @@ class ProductListingFragmentTest : Fragment() {
         productListingFragment.openProductDetailsView(productList)
         Mockito.verify(productListingFragment, Mockito.times(1))
             .openProductDetailView(productList)
+    }
+
+    @Ignore
+    fun clickCategoryListViewCellTest() {
+        val mockActivity = Mockito.mock(BottomNavigationActivity::class.java, CALLS_REAL_METHODS)
+        mockActivity.mNavController = mock()
+        Mockito.`when`(productListingFragment.activity)
+            .thenReturn(mockActivity)
+        val mockNavState: Navigation =
+            Mockito.mock(Navigation::class.java, CALLS_REAL_METHODS)
+        mockNavState.navigationState = "state"
+
+        val mockFragment: ProductListingFragment = mock()
+        PowerMockito.whenNew(ProductListingFragment::class.java).withAnyArguments()
+            .thenReturn(mockFragment)
+        Mockito.doNothing().`when`(mockActivity.pushFragment(mockFragment))
+        productListingFragment.clickCategoryListViewCell(mockNavState, "", "", true)
+        Mockito.verify(mockActivity, Mockito.times(1))
+            .pushFragment(mockFragment)
     }
 }
