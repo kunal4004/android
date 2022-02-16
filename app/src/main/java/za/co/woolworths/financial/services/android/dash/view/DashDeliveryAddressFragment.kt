730bca60c9099fbc1345e0ddbd334c3015af3654
AC62703 <anandchincholkar@woolworths.co.za>
2022-02-16 18:32:50 +0530

    custom tab created DashDeliveryAddressFragment.kt added SetAddressFragement added

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/dash/view/DashDeliveryAddressFragment.kt
@@ -0,0 +1,24 @@
+package za.co.woolworths.financial.services.android.dash.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.awfs.coordination.R
+
+class DashDeliveryAddressFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.layout_dash_set_address_fragment, container, false)
+
+    }
+}
