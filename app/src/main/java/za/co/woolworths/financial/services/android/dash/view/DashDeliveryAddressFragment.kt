87642f9663d65fcdbf1c5882bc41942e80831593
AC62703 <anandchincholkar@woolworths.co.za>
2022-02-23 18:56:07 +0530

    code cleaning

--- app/src/main/java/za/co/woolworths/financial/services/android/dash/view/DashDeliveryAddressFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/dash/view/DashDeliveryAddressFragment.kt
@@ -1,9 +1,5 @@
 package za.co.woolworths.financial.services.android.dash.view
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.awfs.coordination.R
 
