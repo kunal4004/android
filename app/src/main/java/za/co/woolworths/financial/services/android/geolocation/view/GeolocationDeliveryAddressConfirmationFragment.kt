028996605b445f18920720335889956f50a50a68
KU59319 <>
2022-02-24 18:14:03 +0530

    Minor Changes in UI

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/geolocation/view/GeolocationDeliveryAddressConfirmationFragment.kt
@@ -0,0 +1,30 @@
+package za.co.woolworths.financial.services.android.geolocation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.awfs.coordination.R
+
+/**
+ * Created by Kunal Uttarwar on 24/02/22.
+ */
+class GeolocationDeliveryAddressConfirmationFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.geolocation_deliv_click_collect, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
