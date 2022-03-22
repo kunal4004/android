94fd653594c78919293d66ec415aa1ada186df2f
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2022-03-22 12:18:24 +0530

    Resolved comments on PR.

--- app/src/main/java/za/co/woolworths/financial/services/android/models/dto/OtherSkus.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/OtherSkus.kt
@@ -8,45 +8,33 @@ import com.google.gson.annotations.SerializedName
 @Parcelize
 data class OtherSkus (
     @JvmField
-	@SerializedName("sku")
     var sku: String? = null,
 
-    @SerializedName("externalColourRef")
     var externalColourRef: String? = null,
 
-    @SerializedName("colourImagePath")
     var colourImagePath: String? = null,
 
     @JvmField
-	@SerializedName("price")
     var price: String? = null,
 
-    @SerializedName("displayName")
     var displayName: String? = null,
 
     @JvmField
-	@SerializedName("size")
     var size: String? = null,
 
     @JvmField
-	@SerializedName("colour")
     var colour: String? = null,
 
-    @SerializedName("imagePath")
     var imagePath: String? = null,
 
-    @SerializedName("externalImageRefV2")
     var externalImageRefV2: String? = null,
 
-    @SerializedName("wasPrice")
     var wasPrice: String? = null,
 
     @JvmField
 	var quantity: Int = 0,
 
-    @SerializedName("kilogramPrice")
     var kilogramPrice: String? = null,
 
-    @SerializedName("styleIdOnSale")
     var styleIdOnSale: Boolean? = null
 ): Parcelable
