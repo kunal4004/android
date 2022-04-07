2db456e7a5cc0c908fb8b3fabae6509d9ccd91bb
Jean-Paul Manuel <jean-paulm@yoyowallet.com>
2022-04-07 11:45:26 +0400

    Multiple dimensions workaround

---
+++ app/src/googleQa/java/com/huawei/hms/api/HuaweiApiAvailability.kt
@@ -0,0 +1,22 @@
+package com.huawei.hms.api
+
+import android.content.Context
+import com.google.android.gms.common.ConnectionResult
+
+/*
+ * Dummy class to prevent build errors for "google" product flavor
+ * since Huawei dependencies are restricted for that specific flavor only
+ */
+class HuaweiApiAvailability {
+    companion object {
+        @JvmStatic
+        fun getInstance(): HuaweiApiAvailability {
+            return HuaweiApiAvailability()
+        }
+
+        @JvmStatic
+        fun isHuaweiMobileServicesAvailable(context: Context): Int {
+            return ConnectionResult.API_UNAVAILABLE
+        }
+    }
+}
