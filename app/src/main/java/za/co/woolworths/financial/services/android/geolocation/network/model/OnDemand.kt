af3269251e4d95c8d3b8dea48cb2b518a733b6c7
AC62703 <anandchincholkar@woolworths.co.za>
2022-04-13 21:13:58 +0530

    code reverted

--- app/src/main/java/za/co/woolworths/financial/services/android/geolocation/network/model/OnDemand.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/geolocation/network/model/OnDemand.kt
@@ -4,34 +4,34 @@ import com.google.gson.annotations.SerializedName
 import za.co.woolworths.financial.services.android.checkout.service.network.DeliveryStatus
 import java.io.Serializable
 
-data class OnDemand(
+class OnDemand : Serializable {
     @SerializedName("unDeliverableCommerceItems")
-    var unDeliverableCommerceItems: List<Any?>? = null,
+    var unDeliverableCommerceItems: List<Any?>? = null
 
     @SerializedName("firstAvailableFoodDeliveryTime")
-    var firstAvailableFoodDeliveryTime: String? = null,
+    var firstAvailableFoodDeliveryTime: String? = null
 
     @SerializedName("quantityLimit")
-    var quantityLimit: QuantityLimit? = null,
+    var quantityLimit: QuantityLimit? = null
 
     @SerializedName("deliverable")
-    var deliverable: Boolean? = null,
+    var deliverable: Boolean? = null
 
     @SerializedName("storeName")
-    var storeName: String? = null,
+    var storeName: String? = null
 
     @SerializedName("deliveryTimeSlots")
-    var deliveryTimeSlots: List<DeliveryTimeSlot?>? = null,
+    var deliveryTimeSlots: List<DeliveryTimeSlot?>? = null
 
     @SerializedName("storeId")
-    var storeId: String? = null,
+    var storeId: String? = null
 
     @SerializedName("firstAvailableFoodDeliveryCost")
-    var firstAvailableFoodDeliveryCost: Int? = null,
+    var firstAvailableFoodDeliveryCost: Int? = null
 
     @SerializedName("unSellableCommerceItems")
-    var unSellableCommerceItems: List<Any?>? = null,
+    var unSellableCommerceItems: List<Any?>? = null
 
     @SerializedName("deliveryStatus")
     var deliveryStatus: DeliveryStatus? = null
-): Serializable
+}
