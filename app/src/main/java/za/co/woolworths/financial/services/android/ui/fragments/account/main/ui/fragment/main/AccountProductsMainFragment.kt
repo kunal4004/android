cab2297e8936be05f1f25e1a469f38e3e42b1178
Mohamed Magdy <mohamed.magdy@yoyowallet.com>
2022-04-26 15:40:15 +0200

    [WOP-11836] Adding popups

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/ui/fragment/main/AccountProductsMainFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/ui/fragment/main/AccountProductsMainFragment.kt
@@ -10,7 +10,6 @@ import androidx.core.os.bundleOf
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -19,38 +18,29 @@ import com.awfs.coordination.databinding.AccountProductLandingMainFragmentBindin
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.bpi_covered_tag_layout.*
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.observeOn
 import za.co.woolworths.financial.services.android.models.dto.EligibilityPlan
-import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.landing.AccountProductsHomeViewModel
 import za.co.woolworths.financial.services.android.ui.base.ViewBindingFragment
 import za.co.woolworths.financial.services.android.ui.fragments.account.main.component.NavigationGraph
 import za.co.woolworths.financial.services.android.ui.fragments.account.main.core.ViewState
 import za.co.woolworths.financial.services.android.ui.fragments.account.main.domain.sealing.AccountOfferingState
-import za.co.woolworths.financial.services.android.ui.fragments.integration.utils.AccountApiResult
+import za.co.woolworths.financial.services.android.ui.fragments.account.main.domain.sealing.DialogData
+import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.landing.AccountProductsHomeViewModel
 import za.co.woolworths.financial.services.android.util.AppConstant.Companion.RED_HEX_COLOR
 import za.co.woolworths.financial.services.android.util.KotlinUtils
 
 @AndroidEntryPoint
 class AccountProductsMainFragment :
-    ViewBindingFragment<AccountProductLandingMainFragmentBinding>() {
-
+    ViewBindingFragment<AccountProductLandingMainFragmentBinding>(AccountProductLandingMainFragmentBinding::inflate) {
     private var childNavController: NavController? = null
     val viewModel by viewModels<AccountProductsHomeViewModel>()
     var navigationGraph: NavigationGraph = NavigationGraph()
-
-    override fun inflateViewBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): AccountProductLandingMainFragmentBinding {
-        return AccountProductLandingMainFragmentBinding.inflate(inflater, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.window?.let { WindowCompat.setDecorFitsSystemWindows(it, false) }
         setToolbar()
         setupLandingScreen()
-        setupObservers()
     }
 
     private fun setToolbar() {
@@ -97,7 +87,7 @@ class AccountProductsMainFragment :
                     }
 
                     is AccountOfferingState.AccountIsChargedOff -> {
-                        when(isCreditCard(getAccountProduct())){
+                        when(isCreditCard(product)){
                             false -> displayPopUp(DialogData.ChargedOff())
                         }
                     }
@@ -111,43 +101,38 @@ class AccountProductsMainFragment :
                     }
 
                     is AccountOfferingState.MakeGetEligibilityCall -> {
-                        lifecycleScope.launchWhenStarted {
-                            viewModel.eligibilityPlanResponse().collect { response ->
-                                when (response) {
-                                    is ViewState.RenderSuccess -> {
-                                        response.output.eligibilityPlan.let {
-                                            displayPopUp(viewModel.getPopUpData(it), it)
-                                        }
-                                    }
-                                    is ViewState.RenderFailure -> {
-                                        displayPopUp(DialogData.AccountInArrDialog())
-                                    }
-                                    is ViewState.Loading -> {
-                                    }
-                                }
-                            }
-                        }
-
+                        callEligibility()
                     }
                 }
             }
         }
     }
 
-    private fun setupObservers() {
-        with(viewModel) {
-            eligibilityPlanResponseLiveData.observe(viewLifecycleOwner) { response ->
-
+    private fun callEligibility() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.eligibilityPlanResponse().collect { response ->
+                when (response) {
+                    is ViewState.RenderSuccess -> {
+                        response.output.eligibilityPlan.let {
+                            displayPopUp(viewModel.getPopUpData(it), it)
+                        }
+                    }
+                    is ViewState.RenderFailure -> {
+                        displayPopUp(DialogData.AccountInArrDialog())
+                    }
+                    is ViewState.Loading -> {
+                    }
+                    ViewState.RenderEmpty -> {}
+                }
             }
         }
     }
 
     fun displayPopUp(dialogData: DialogData, eligibilityPlan: EligibilityPlan? = null) {
         viewModel.apply {
-            val account = getAccountProduct()
             findNavController().navigate(
                 AccountProductsMainFragmentDirections.actionAccountProductsMainFragmentToAccountLandingDialogFragment(
-                    account,
+                    product,
                     dialogData, eligibilityPlan
                 )
             )
