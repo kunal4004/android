92d9c22b38ac6c44aa8791e1abe9ed1f7b1b6223
Mohamed Magdy <mohamed.magdy@yoyowallet.com>
2022-05-14 15:14:54 +0200

    [WOP-16148] Adding firebase events

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/domain/StoreCardNavigator.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/domain/StoreCardNavigator.kt
@@ -10,7 +10,11 @@ import za.co.woolworths.financial.services.android.models.dto.account.AccountsPr
 import za.co.woolworths.financial.services.android.models.dto.account.ApplyNowState
 import za.co.woolworths.financial.services.android.ui.activities.StatementActivity
 import za.co.woolworths.financial.services.android.ui.activities.WTransactionsActivity
+import za.co.woolworths.financial.services.android.ui.fragments.account.available_fund.store_card.StoreCardFragmentDirections
 import za.co.woolworths.financial.services.android.ui.fragments.account.chat.ui.ChatFragment
+import za.co.woolworths.financial.services.android.ui.fragments.account.detail.pay_my_account.PayMyAccountViewModel
+import za.co.woolworths.financial.services.android.ui.fragments.account.helper.FirebaseEventDetailManager
+import za.co.woolworths.financial.services.android.util.ActivityIntentNavigationManager
 import za.co.woolworths.financial.services.android.util.BundleKeysConstants
 import za.co.woolworths.financial.services.android.util.Utils
 import javax.inject.Inject
@@ -26,6 +30,7 @@ interface IStoreCardNavigator {
 class StoreCardNavigator @Inject constructor() : IStoreCardNavigator {
 
     override fun navigateToStatementActivity(activity: Activity?, product: Account?, applyNowState:ApplyNowState) {
+        statementsEvent(activity,applyNowState)
         activity?.apply {
             val mAccountPair: Pair<ApplyNowState, Account?> = Pair(applyNowState,product)
             Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.MYACCOUNTSSTORECARDSTATEMENTS, this)
@@ -36,6 +41,7 @@ class StoreCardNavigator @Inject constructor() : IStoreCardNavigator {
         }
     }
     override fun navigateToRecentTransactionActivity(activity: Activity?, product: Account?, applyNowState:ApplyNowState ,cardType: String) {
+        transactionEvent(activity,applyNowState)
         activity?.let {
             product?.apply {
                 val intent = Intent(it, WTransactionsActivity::class.java)
@@ -47,6 +53,31 @@ class StoreCardNavigator @Inject constructor() : IStoreCardNavigator {
                 intent.putExtra("cardType", cardType)
                 it.startActivityForResult(intent, 0)
                 it.overridePendingTransition(R.anim.slide_up_anim, R.anim.stay)
+            }
+        }
+    }
+
+    private fun transactionEvent(activity: Activity?,applyNowState: ApplyNowState){
+        //TODO:: CreditCard events to be added
+        when(applyNowState){
+            ApplyNowState.STORE_CARD->{
+                activity?.apply { FirebaseEventDetailManager.tapped(FirebaseManagerAnalyticsProperties.MYACCOUNTSSTORECARDTRANSACTIONS, this) }
+
+            }
+            ApplyNowState.PERSONAL_LOAN->{
+                activity?.apply { FirebaseEventDetailManager.tapped(FirebaseManagerAnalyticsProperties.MYACCOUNTSPERSONALLOANTRANSACTIONS ,this) }
+            }
+        }
+    }
+    private fun statementsEvent(activity: Activity?,applyNowState: ApplyNowState){
+        //TODO:: CreditCard events to be added
+        when(applyNowState){
+            ApplyNowState.STORE_CARD->{
+                activity?.apply { Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.MYACCOUNTSSTORECARDSTATEMENTS, this) }
+
+            }
+            ApplyNowState.PERSONAL_LOAN->{
+                activity?.apply { Utils.triggerFireBaseEvents(FirebaseManagerAnalyticsProperties.MYACCOUNTSPERSONALLOANSTATEMENTS, this) }
             }
         }
     }
