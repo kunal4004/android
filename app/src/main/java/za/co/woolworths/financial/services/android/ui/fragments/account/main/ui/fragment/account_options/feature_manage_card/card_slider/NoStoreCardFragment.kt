0fa85a5e5461f44256577aebbca32d3a4b8ac259
Dimitri Jacques <dimitri.jacques@yoyowallet.com>
2022-06-15 11:12:21 +0400

    Genric popup dialog and added default store card

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/ui/fragment/account_options/feature_manage_card/card_slider/NoStoreCardFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/ui/fragment/account_options/feature_manage_card/card_slider/NoStoreCardFragment.kt
@@ -1,14 +1,22 @@
 package za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.account_options.feature_manage_card.card_slider
 
+
 import android.os.Bundle
 import android.view.View
-import com.awfs.coordination.databinding.InstantStoreCardReplacementCardFragmentBinding
-import za.co.woolworths.financial.services.android.ui.base.ViewBindingFragment
+import androidx.fragment.app.Fragment
+import com.awfs.coordination.R
+import za.co.woolworths.financial.services.android.ui.extension.withArgs
+import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.account_options.feature_manage_card.main.StoreCardFeatureType
 
-class NoStoreCardFragment :
-    ViewBindingFragment<InstantStoreCardReplacementCardFragmentBinding>(
-        InstantStoreCardReplacementCardFragmentBinding::inflate
-    ) {
+class NoStoreCardFragment : Fragment(R.layout.instant_store_card_replacement_card_fragment) {
+
+    companion object {
+        private const val STORE_CARD_FEATURE_TYPE = "STORE_CARD_FEATURE_TYPE"
+        fun newInstance(storeCard: StoreCardFeatureType?) =
+            NoStoreCardFragment().withArgs {
+                putParcelable(STORE_CARD_FEATURE_TYPE, storeCard)
+            }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
