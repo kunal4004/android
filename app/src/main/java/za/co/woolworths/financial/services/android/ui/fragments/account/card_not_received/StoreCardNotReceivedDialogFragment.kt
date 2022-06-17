911cc80823c7c69638e2e545635cd0a73dab338b
Dimitri Jacques <dimitri.jacques@yoyowallet.com>
2022-06-17 16:27:27 +0400

    Card Not Received integration

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/card_not_received/StoreCardNotReceivedDialogFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/card_not_received/StoreCardNotReceivedDialogFragment.kt
@@ -8,15 +8,21 @@ import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.awfs.coordination.R
 import com.awfs.coordination.databinding.StoreCardVtscCardNotReceivedPopupDialogBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import za.co.woolworths.financial.services.android.contracts.FirebaseManagerAnalyticsProperties
 import za.co.woolworths.financial.services.android.models.dao.SessionDao
-import za.co.woolworths.financial.services.android.models.dto.Response
+import za.co.woolworths.financial.services.android.models.dto.account.ServerErrorResponse
 import za.co.woolworths.financial.services.android.ui.activities.account.sign_in.viewmodel.MyAccountsRemoteApiViewModel
 import za.co.woolworths.financial.services.android.ui.base.ViewBindingBottomSheetFragment
-import za.co.woolworths.financial.services.android.ui.fragments.integration.utils.ApiResult
+import za.co.woolworths.financial.services.android.ui.fragments.account.main.core.renderFailure
+import za.co.woolworths.financial.services.android.ui.fragments.account.main.core.renderHttpFailureFromServer
+import za.co.woolworths.financial.services.android.ui.fragments.account.main.core.renderLoading
+import za.co.woolworths.financial.services.android.ui.fragments.account.main.core.renderSuccess
 import za.co.woolworths.financial.services.android.util.Utils
 import za.co.woolworths.financial.services.android.util.animation.AnimationUtilExtension
 
@@ -77,17 +83,19 @@ class StoreCardNotReceivedDialogFragment : ViewBindingBottomSheetFragment<StoreC
     }
 
     private fun subscribeObserver() {
-        viewModel.notifyCardNotReceived.observe(viewLifecycleOwner) { result ->
-            when (result) {
-                is ApiResult.Success -> successNotificationView()
-                is ApiResult.Failure -> httpErrorFromServer(result.data)
-                is ApiResult.Error -> errorMessage()
+        lifecycleScope.launch {
+            viewModel.notifyCardNotReceived.collectLatest {
+                with(it){
+                    renderLoading { showProgress(isLoading) }
+                    renderSuccess { successNotificationView() }
+                    renderHttpFailureFromServer { httpErrorFromServer(this.output.response) }
+                    renderFailure { errorMessage() }
+                }
             }
         }
     }
 
-    private fun httpErrorFromServer(response: Response?) {
-        showProgress(false)
+    private fun httpErrorFromServer(response: ServerErrorResponse?) {
         with(binding) {
             headerTextView.text = getString(R.string.oops_err_title)
             descriptionTextView.text = response?.desc
@@ -96,7 +104,6 @@ class StoreCardNotReceivedDialogFragment : ViewBindingBottomSheetFragment<StoreC
     }
 
     private fun errorMessage() {
-        showProgress(false)
         with(binding) {
             headerTextView.text = getString(R.string.oops_err_title)
             descriptionTextView.text = getString(R.string.oops_error_message)
@@ -109,7 +116,6 @@ class StoreCardNotReceivedDialogFragment : ViewBindingBottomSheetFragment<StoreC
     }
 
     private fun successNotificationView() {
-        showProgress(false)
         Utils.sessionDaoSave(SessionDao.KEY.CARD_NOT_RECEIVED_DIALOG_WAS_SHOWN, "1")
         with(binding) {
             headerTextView.text = getString(R.string.vtsc_card_not_arrived_notified_title)
@@ -138,7 +144,6 @@ class StoreCardNotReceivedDialogFragment : ViewBindingBottomSheetFragment<StoreC
     }
 
     private fun queryAPIServiceGetCardNotReceived() {
-        showProgress(true)
         viewModel.queryServiceCardNotYetReceived()
     }
 }
