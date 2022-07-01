e2e06cf43786833574921a08e4ae7aec007dc043
46964 <siddharajnirmale@woolworths.co.za>
2022-07-01 19:07:41 +0530

    APP2-109

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/SSOActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/SSOActivity.java
@@ -103,6 +103,7 @@ public class SSOActivity extends WebViewActivity {
 	public static final String FORGOT_PASSWORD = "FORGOT_PASSWORD";
 	public static final String FORGOT_PASSWORD_VALUE = "PASSWORD";
 	private String forgotPasswordLogin = "login=true&source=oneapp";
+	private String TNC_TITLE = "Woolworths.co.za";
 
 	public static final String TAG_EXTRA_QUERYSTRING_PARAMS = "TAG_EXTRA_QUERYSTRING_PARAMS";
 	//Default redirect url used by LOGIN AND LINK CARDS
@@ -727,7 +728,7 @@ public class SSOActivity extends WebViewActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case android.R.id.home:
-				if (this.webView != null && webView.canGoBack()) {
+				if (this.webView != null && webView.canGoBack() && toolbarTextView.getText().toString().contains(TNC_TITLE)) {
 					webView.goBack();
 				} else {
 					finishActivity();
