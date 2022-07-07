b72210dcbffd0d933e901e59d77d6c2f73ed1eb3
MU_Arif <MuhammedArif@woolworths.co.za>
2022-07-07 23:43:26 +0530

    chat ui (partially done)

--- app/src/main/java/za/co/woolworths/financial/services/android/getstream/chat/ChatViewModel.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/getstream/chat/ChatViewModel.kt
@@ -2,7 +2,8 @@ package za.co.woolworths.financial.services.android.getstream.chat
 
 import androidx.lifecycle.ViewModel
 import io.getstream.chat.android.client.ChatClient
-import io.getstream.chat.android.livedata.controller.ChannelController
+import io.getstream.chat.android.client.models.Message
+
 
 class ChatViewModel: ViewModel() {
 
@@ -10,13 +11,35 @@ class ChatViewModel: ViewModel() {
 
     private val chatClient: ChatClient by lazy { ChatClient.instance() }
 
-    public fun fetchMessages(){
+     fun fetchMessages(){
         chatClient.channel(channelId!!).watch().enqueue { result ->
             if (result.isSuccess) {
                 var channel = result.data()
-
                 channel.messages
             }
         }
+    }
+    fun sendMessage(messageText: String) {
+        channelId?.let {
+            val message = Message(
+                cid = it,
+                text = messageText
+            )
+            // chatClient.channel(it).sendMessage(message)
+            chatClient.channel(it).sendMessage(message).enqueue { result ->
+                if (result.isSuccess) {
+                    val message: Message = result.data()
+                } else {
+                    // Handle result.error()
+                }
+
+            }
+
+        }
+    }
+
+    fun isMessageMine(message: Message): Boolean {
+        val currentUser = chatClient.getCurrentUser() ?: return false
+        return message.user.id.equals(currentUser.id)
     }
 }
