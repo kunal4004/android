f28945d6fbfafc70437206248ac45833845b4261
MU_Arif <MuhammedArif@woolworths.co.za>
2022-07-26 15:55:44 +0530

    revert one cart in app chat code.

--- app/src/main/java/za/co/woolworths/financial/services/android/getstream/chat/IMessageItemDelegate.kt
+++
@@ -1,7 +0,0 @@
-package za.co.woolworths.financial.services.android.getstream.chat
-
-import io.getstream.chat.android.client.models.Message
-
-interface IMessageItemDelegate {
-    val isMessageOwnedByMe: (message: Message) -> Boolean
-}
--- app/src/main/java/za/co/woolworths/financial/services/android/getstream/chat/MessageItem.kt
+++
@@ -1,5 +0,0 @@
-package za.co.woolworths.financial.services.android.getstream.chat
-
-import io.getstream.chat.android.client.models.Message
-
-data class MessageItem(val isMine: Boolean, val message: Message)
