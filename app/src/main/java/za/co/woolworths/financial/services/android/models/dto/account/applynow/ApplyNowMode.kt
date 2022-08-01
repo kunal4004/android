38601ead02e64dfb990157b73deb7fefc9c52dec
Mohamed Magdy <mohamed.magdy@yoyowallet.com>
2022-08-01 20:22:35 +0200

    [WOP-16570] Adding network layer

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/dto/account/applynow/ApplyNowMode.kt
@@ -0,0 +1,76 @@
+package za.co.woolworths.financial.services.android.models.dto.account.applynow
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import za.co.woolworths.financial.services.android.util.expand.ParentListItem
+
+
+@Parcelize
+data class ApplyNowModel (
+    var content : List<Content>,
+    var response : Response,
+    var httpCode : Int
+):Parcelable
+
+@Parcelize
+data class Children (
+    var order : Int,
+    var title : String,
+    var type : String,
+    var sectionType: ApplyNowSectionType = ApplyNowSectionType.valueOf(type),
+    var children : List<ChildrenItems>
+):Parcelable
+
+@Parcelize
+data class ChildrenItems (
+    var title : String?,
+    var description : String,
+    var imageUrl : String?
+):Parcelable,ParentListItem {
+    override fun getChildItemList(): MutableList<String?> = mutableListOf(description)
+    override fun isInitiallyExpanded(): Boolean = false
+}
+
+@Parcelize
+data class Content (
+
+    var order : Int,
+    var reference : String,
+    var title : String,
+    var description : String,
+    var imageUrl : String,
+    var children : List<Children>
+):Parcelable
+
+@Parcelize
+data class Response (
+
+    var code : Int,
+    var desc : String
+):Parcelable
+
+
+sealed class ApplyNowTypesModel:Parcelable {
+    @Parcelize
+    data class LeftIconContent(
+        val title : String,
+        val description : String,
+        val imageUrl : String
+    ) : ApplyNowTypesModel()
+    @Parcelize
+    data class LeftIconExpandable(
+        val title : String,
+        val description : String,
+        val imageUrl : String
+    ) : ApplyNowTypesModel()
+    @Parcelize
+    data class ListUnordered(
+        val description : String,
+    ) : ApplyNowTypesModel()
+}
+
+enum class ApplyNowSectionType(val value: String) {
+    LEFT_ICON_WITH_CONTENT("LEFT_ICON_WITH_CONTENT"),
+    LEFT_ICON_WITH_CONTENT_EXPANDABLE("LEFT_ICON_WITH_CONTENT_EXPANDABLE"),
+    LIST_UNORDERED("LIST_UNORDERED")
+}
---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/applynow/data/ApplyNowRepo.kt
@@ -0,0 +1,19 @@
+package za.co.woolworths.financial.services.android.ui.fragments.account.applynow.data
+
+import kotlinx.coroutines.flow.Flow
+import za.co.woolworths.financial.services.android.models.dto.account.applynow.ApplyNowModel
+import za.co.woolworths.financial.services.android.ui.fragments.account.applynow.utils.CoreDataSource
+import za.co.woolworths.financial.services.android.ui.fragments.account.applynow.utils.IOTaskResult
+import javax.inject.Inject
+
+interface IApplyNowRepo{
+    suspend fun queryServiceApplyNow(contentId:String): Flow<IOTaskResult<ApplyNowModel>>
+}
+
+class ApplyNowRepo @Inject constructor(private val service : ApplyNowRemoteDataSource) :
+    CoreDataSource(), IApplyNowRepo {
+
+    override suspend fun queryServiceApplyNow(contentId:String) : Flow<IOTaskResult<ApplyNowModel>> = performSafeNetworkApiCall { service.queryServiceApplyNow(contentId) }
+}
+
+
