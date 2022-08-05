fb49625fc4fae60bff7f7a32e51ddbc9a27a457f
Sarith <ext_svasu@Falabella.cl>
2022-08-05 19:08:58 +0530

    added clearPreferredDeliveryLocation method in Utils

--- app/src/main/java/za/co/woolworths/financial/services/android/util/Utils.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/Utils.java
@@ -866,6 +866,12 @@ public class Utils {
         currentUserObject.save();
     }
 
+    public static void clearPreferredDeliveryLocation() {
+        AppInstanceObject.User currentUserObject = AppInstanceObject.get().getCurrentUserObject();
+        currentUserObject.preferredShoppingDeliveryLocation = null;
+        currentUserObject.save();
+    }
+
     public static void addToShoppingDeliveryLocationHistory(ShoppingDeliveryLocation shoppingDeliveryLocation) {
         AppInstanceObject.User currentUserObject = AppInstanceObject.get().getCurrentUserObject();
         currentUserObject.shoppingDeliveryLocationHistory.add(shoppingDeliveryLocation);
