43a8df4552c72c962c68c10864ab824570e03732
Dimitri Jacques <dimitri.jacques@yoyowallet.com>
2022-09-14 10:57:34 +0400

    PMA- Amount Overlay- When a user select over due amount on edit screen to proceed for payment , that amount is not reflecting on amount overlay

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/ui/activities/StoreCardActivity.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/main/ui/activities/StoreCardActivity.kt
@@ -1,5 +1,6 @@
 package za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.annotation.StringRes
@@ -10,7 +11,10 @@ import com.awfs.coordination.R
 import com.awfs.coordination.databinding.AccountProductLandingActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
 import za.co.woolworths.financial.services.android.models.dto.Account
+import za.co.woolworths.financial.services.android.ui.activities.account.sign_in.pay_my_account.PayMyAccountActivity
 import za.co.woolworths.financial.services.android.ui.extension.bindString
+import za.co.woolworths.financial.services.android.ui.fragments.account.detail.pay_my_account.PMA3DSecureProcessRequestFragment
+import za.co.woolworths.financial.services.android.ui.fragments.account.detail.pay_my_account.PayMyAccountViewModel
 import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.account_options.utils.setupGraph
 import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.landing.AccountProductsHomeViewModel
 import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.main.AccountProductsMainFragment
@@ -27,6 +31,7 @@ class StoreCardActivity : AppCompatActivity() {
     lateinit var binding: AccountProductLandingActivityBinding
 
     val homeViewModel: AccountProductsHomeViewModel by viewModels()
+    val payMyAccountViewModel: PayMyAccountViewModel by viewModels()
 
     @Inject lateinit var statusBarCompat: SystemBarCompat
 
@@ -66,5 +71,28 @@ class StoreCardActivity : AppCompatActivity() {
         OneAppSnackbar.make(binding.rootContainer.rootView, bindString(stringId).toUpperCase(
             Locale.getDefault())).show()
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val extras = data?.extras
+        when (requestCode) {
+            PayMyAccountActivity.PAY_MY_ACCOUNT_REQUEST_CODE -> {
+                when (resultCode) {
+                    RESULT_OK, PMA3DSecureProcessRequestFragment.PMA_UPDATE_CARD_RESULT_CODE -> {
+                        extras?.getString(PayMyAccountActivity.PAYMENT_DETAIL_CARD_UPDATE)?.apply {
+                            payMyAccountViewModel.setPMACardInfo(this)
+                        }
+                    }
+
+                    // on back to my account pressed (R.string.back_to_my_account_button)
+                    PMA3DSecureProcessRequestFragment.PMA_TRANSACTION_COMPLETED_RESULT_CODE -> {
+                        extras?.getString(PayMyAccountActivity.PAYMENT_DETAIL_CARD_UPDATE)?.apply {
+                            payMyAccountViewModel.setPMACardInfo(this)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
