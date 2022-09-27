6e38b229774d7969dec084ff874515a3ba0e4f23
Dimitri Jacques <dimitri.jacques@yoyowallet.com>
2022-09-27 15:12:16 +0400

    Merge branch 'dev' of https://bitbucket.org/wiGroup/woolworths-android into revert-pr-4341
    # Conflicts:
    #	app/src/main/java/za/co/woolworths/financial/services/android/startup/view/StartupActivity.kt

--- app/src/main/java/za/co/woolworths/financial/services/android/onecartgetstream/common/constant/OCConstant.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/onecartgetstream/common/constant/OCConstant.kt
@@ -1,20 +1,31 @@
 package za.co.woolworths.financial.services.android.onecartgetstream.common.constant
 import android.content.Context
 import android.content.Intent
+
 import za.co.woolworths.financial.services.android.onecartgetstream.service.DashChatMessageListeningService
 
-object OCConstant {
+class OCConstant {
 
-    const val ORDER_PENDING_PICKING = "PENDING_PICKING"
-    var OC_MESSAGE_COUNT = 0
+    companion object {
+        const val ORDER_PENDING_PICKING = "PENDING_PICKING"
+        var ocChatMessageCount = 0
+        var isOCChatBackgroundServiceRunning = false
 
-    fun startOCChatService(context: Context?){
-        val chatListeningServiceIntent = Intent(context, DashChatMessageListeningService::class.java)
-        context?.startService(chatListeningServiceIntent)
+        fun startOCChatService(context: Context?) {
+            if (!isOCChatBackgroundServiceRunning){
+                val chatListeningServiceIntent =
+                    Intent(context, DashChatMessageListeningService::class.java)
+                context?.startService(chatListeningServiceIntent)
+            }
+
+        }
+
+        fun stopOCChatService(context: Context?){
+            val chatListeningServiceIntent = Intent(context, DashChatMessageListeningService::class.java)
+            context?.stopService(chatListeningServiceIntent)
+        }
+
 
     }
-    fun stopOCChatService(context: Context?){
-        val chatListeningServiceIntent = Intent(context, DashChatMessageListeningService::class.java)
-        context?.stopService(chatListeningServiceIntent)
-    }
+
 }
--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/account/sign_in/AccountSignedInPresenterImpl.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/account/sign_in/AccountSignedInPresenterImpl.kt
@@ -161,6 +161,7 @@ class AccountSignedInPresenterImpl(
         }
         eligibilityPlan = response.eligibilityPlan
 
+
         if (eligibilityPlan?.productGroupCode == eligibleState) {
             when (eligibilityPlan?.actionText) {
                 ActionText.START_NEW_ELITE_PLAN.value -> {
@@ -230,15 +231,19 @@ class AccountSignedInPresenterImpl(
         showPopupIfNeeded: Boolean
     ) {
         val account = getAccount() ?: return
+
         with(ProductOfferingStatus(account)) {
             mainView?.apply {
+
+                // Construct help list when account is in good standing or in arrears
+                showAccountHelp(getCardProductInformation(isAccountInArrearsState() || isChargedOff()))
+
                 state { status ->
                     when (status) {
 
                         AccountOfferingState.AccountInGoodStanding -> {
                             //when productOfferingGoodStanding == true
                             hideAccountInArrears(account)
-                            showAccountHelp(getCardProductInformation(false))
                         }
 
                         AccountOfferingState.AccountIsInArrears -> showAccountInArrears(
@@ -278,8 +283,9 @@ class AccountSignedInPresenterImpl(
                                 },
                                 {
                                     eligibilityImpl?.eligibilityFailed()
-                                    if (showPopupIfNeeded && !isChargedOffCC()) showAccountInArrears(
+                                    if (showPopupIfNeeded && !isChargedOffCC()){ showAccountInArrears(
                                         account)
+                                    }
                                 })
                         }
                     }
