5abeeb65b9e1c03091bf9de6707d7ab06cd9dede
Jean-Paul Manuel <jean-paulm@yoyowallet.com>
2022-09-29 01:44:24 +0400

    VOC - Fill space separator logic added

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/voc/SurveyVocFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/voc/SurveyVocFragment.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -44,7 +49,7 @@ import za.co.woolworths.financial.services.android.ui.views.voc.SurveyQuestionRa
 import za.co.woolworths.financial.services.android.util.Utils
 import za.co.woolworths.financial.services.android.util.analytics.FirebaseManager
 
-class SurveyVocFragment : Fragment(), GenericActionOrCancelDialogFragment.IActionOrCancel { //, SurveyAnswerDelegate {
+class SurveyVocFragment : Fragment(), GenericActionOrCancelDialogFragment.IActionOrCancel {
 
     companion object {
         const val DIALOG_OPT_OUT_ID = 2
@@ -72,59 +77,101 @@ class SurveyVocFragment : Fragment(), GenericActionOrCancelDialogFragment.IActio
         var isSubmitEnabled by rememberSaveable {
             mutableStateOf(isSurveyAnswersValid())
         }
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(colorResource(id = R.color.color_separator_lighter_grey))
-        ) {
-            items(
-                count = questions.size + 1,
-                contentType = { index ->
-                    if (index < questions.size) {
-                        SurveyQuestion.QuestionType.ofType(questions[index].type)?.viewType
-                            ?: SurveyQuestion.QuestionType.FREE_TEXT.viewType
-                    }
-                    VIEW_TYPE_FOOTER
-                },
-                itemContent = { index ->
-                    if (index < questions.size) {
-                        val question = questions[index]
-                        when (SurveyQuestion.QuestionType.ofType(question.type)) {
-                            SurveyQuestion.QuestionType.RATE_SLIDER -> {
-                                // Using an Android View inside of a Compose View
-                                val rateSliderView = SurveyQuestionRateSliderView(LocalContext.current)
-                                rateSliderView.bind(question, getAnswer(question.id)) { questionId, value ->
-                                    onInputRateSlider(questionId, value)
-                                    // No need to update submit button's state here,
-                                    // since slider already has a default value, whether it's required or not
-                                }
-                                AndroidView(factory = { rateSliderView })
-                            }
-                            SurveyQuestion.QuestionType.FREE_TEXT -> {
-                                SurveyQuestionFreeTextView(
-                                    title = question.title,
-                                    initialText = getAnswer(question.id)?.textAnswer,
-                                    placeholder = if (question.required == true) R.string.voc_question_freetext_hint_required else R.string.voc_question_freetext_hint_optional,
-                                    onTextChanged = { value ->
-                                        onInputFreeText(question.id, value)
-                                        isSubmitEnabled = isSurveyAnswersValid()
-                                    }
-                                )
-                            }
-                            else -> {}
+        var positionLazyColumnBottom by remember { mutableStateOf(0f) }
+        var positionBottomSpacer by remember { mutableStateOf(Offset.Zero) }
+        var positionContainerBottomSpacer by remember { mutableStateOf(Offset.Zero) }
+        val density = LocalDensity.current
+
+        Column {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(weight = 1f, fill = true)
+                    .background(colorResource(id = R.color.color_separator_lighter_grey))
+                    .onGloballyPositioned { coordinates ->
+                        if (positionLazyColumnBottom <= 0f) {
+                            positionLazyColumnBottom = coordinates.positionInWindow().y + coordinates.size.height.toFloat()
                         }
-                        Spacer(
-                            modifier = Modifier
-                                .height(8.dp)
-                        )
-                    } else {
-                        SurveyFooterActionView(
-                            isSubmitEnabled = isSubmitEnabled,
-                            onSubmitCallback = ::onSubmit,
-                            onOptOutCallback = ::onOptOut
-                        )
                     }
-                }
+            ) {
+                items(
+                    count = questions.size + 1,
+                    contentType = { index ->
+                        if (index < questions.size) {
+                            SurveyQuestion.QuestionType.ofType(questions[index].type)?.viewType
+                                ?: SurveyQuestion.QuestionType.FREE_TEXT.viewType
+                        }
+                        VIEW_TYPE_FOOTER
+                    },
+                    itemContent = { index ->
+                        if (index < questions.size) {
+                            val question = questions[index]
+                            when (SurveyQuestion.QuestionType.ofType(question.type)) {
+                                SurveyQuestion.QuestionType.RATE_SLIDER -> {
+                                    // Using an Android View inside of a Compose View
+                                    val rateSliderView = SurveyQuestionRateSliderView(LocalContext.current)
+                                    rateSliderView.bind(question, getAnswer(question.id)) { questionId, value ->
+                                        onInputRateSlider(questionId, value)
+                                        // No need to update submit button's state here,
+                                        // since slider already has a default value, whether it's required or not
+                                    }
+                                    AndroidView(factory = { rateSliderView })
+                                }
+                                SurveyQuestion.QuestionType.FREE_TEXT -> {
+                                    SurveyQuestionFreeTextView(
+                                        title = question.title,
+                                        initialText = getAnswer(question.id)?.textAnswer,
+                                        placeholder = if (question.required == true) R.string.voc_question_freetext_hint_required else R.string.voc_question_freetext_hint_optional,
+                                        onTextChanged = { value ->
+                                            onInputFreeText(question.id, value)
+                                            isSubmitEnabled = isSurveyAnswersValid()
+                                        }
+                                    )
+                                }
+                                else -> {}
+                            }
+                            Spacer(
+                                modifier = Modifier
+                                    .height(8.dp)
+                            )
+                        } else {
+                            if (
+                                positionBottomSpacer != Offset.Zero &&
+                                positionContainerBottomSpacer != Offset.Zero &&
+                                positionContainerBottomSpacer.y - positionBottomSpacer.y > 0
+                            ) {
+                                density.run {
+                                    Spacer(
+                                        Modifier.height((positionContainerBottomSpacer.y - positionBottomSpacer.y).toDp())
+                                    )
+                                }
+                            }
+                            SurveyFooterActionView(
+                                isSubmitEnabled = isSubmitEnabled,
+                                onSubmitCallback = ::onSubmit,
+                                onOptOutCallback = ::onOptOut
+                            )
+                            Spacer(
+                                Modifier
+                                    .background(Color.Red)
+                                    .height(0.dp)
+                                    .onGloballyPositioned { coordinates ->
+                                        if (positionBottomSpacer == Offset.Zero) {
+                                            positionBottomSpacer = coordinates.positionInWindow()
+                                        }
+                                    }
+                            )
+                        }
+                    }
+                )
+            }
+            Spacer(
+                Modifier
+                    .background(Color.Red)
+                    .height(0.dp)
+                    .onGloballyPositioned { coordinates ->
+                        positionContainerBottomSpacer = coordinates.positionInWindow()
+                    }
             )
         }
     }
