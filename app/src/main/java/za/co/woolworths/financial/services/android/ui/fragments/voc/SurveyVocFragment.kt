cc6cae048cef897fc8ad7125b27ccb94414ac8d0
Jean-Paul Manuel <jean-paulm@yoyowallet.com>
2022-09-30 17:16:34 +0400

    VOC - Confirmation dialog migrated to Compose

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/voc/SurveyVocFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/voc/SurveyVocFragment.kt
@@ -1,7 +1,6 @@
 package za.co.woolworths.financial.services.android.ui.fragments.voc
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
