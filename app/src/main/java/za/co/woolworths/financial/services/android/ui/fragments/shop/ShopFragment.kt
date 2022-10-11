33400316c38912704414a311c4f576fbdcbb0d61
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2022-10-11 17:51:06 +0530

    Removed Log import.

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shop/ShopFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shop/ShopFragment.kt
@@ -6,7 +6,6 @@ import android.graphics.Typeface
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.MarginLayoutParams
