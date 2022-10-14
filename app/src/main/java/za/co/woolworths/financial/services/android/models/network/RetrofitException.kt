4879a813bfb081189e734cc81ea7b0e24efe6487
AC62703 <anandchincholkar@woolworths.co.za>
2022-10-14 13:52:09 +0530

    SocketTimeOut Exception added at wfsInterceptor class updated CompletionHandler.kt updated RetrofitException.kt

--- app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitException.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitException.kt
@@ -1,16 +1,18 @@
 package za.co.woolworths.financial.services.android.models.network
 
 import za.co.woolworths.financial.services.android.ui.activities.maintenance.NetworkRuntimeExceptionViewController
+import za.co.woolworths.financial.services.android.util.analytics.FirebaseManager
 import java.net.SocketTimeoutException
 
-class RetrofitException(private var url:String, private var exception: Any?) {
+class RetrofitException(private var url:String, private var exceptionCode: Any?, private var exceptionResponse:Any?) {
     fun show(): Boolean {
         return when {
-            exception is SocketTimeoutException -> {
+            exceptionResponse is SocketTimeoutException -> {
+                FirebaseManager.logException(exceptionResponse)
                 NetworkRuntimeExceptionViewController().openSocketTimeOutDialog()
                 true
             }
-            (exception == 404) or (exception == 503) -> {
+            (exceptionCode == 404) or (exceptionCode == 503) -> {
                 if(!url.contains("mobileconfigs")) {
                     NetworkRuntimeExceptionViewController().openMaintenanceView()
                 }
