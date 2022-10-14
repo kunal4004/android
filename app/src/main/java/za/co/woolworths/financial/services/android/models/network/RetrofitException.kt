057d8ea81d3f72baca91659659d2a1df113cfc35
KU59319 <>
2022-10-14 17:57:02 +0530

    Conditions Added for 503 and 404

--- app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitException.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/network/RetrofitException.kt
@@ -1,10 +1,17 @@
 package za.co.woolworths.financial.services.android.models.network
 
+import za.co.woolworths.financial.services.android.models.dto.NetworkErrorResponse
 import za.co.woolworths.financial.services.android.ui.activities.maintenance.NetworkRuntimeExceptionViewController
+import za.co.woolworths.financial.services.android.util.AppConstant.Companion.HTTP_NOT_FOUND_404
+import za.co.woolworths.financial.services.android.util.AppConstant.Companion.HTTP_SERVICE_UNAVAILABLE_503
 import za.co.woolworths.financial.services.android.util.analytics.FirebaseManager
 import java.net.SocketTimeoutException
 
-class RetrofitException(private var url:String, private var exceptionCode: Any?, private var exceptionResponse:Any?) {
+class RetrofitException(
+    private var url: String,
+    private var exceptionCode: Any?,
+    private var exceptionResponse: Any?,
+) {
     fun show(): Boolean {
         return when {
             exceptionResponse is SocketTimeoutException -> {
@@ -12,9 +19,21 @@ class RetrofitException(private var url:String, private var exceptionCode: Any?,
                 NetworkRuntimeExceptionViewController().openSocketTimeOutDialog()
                 true
             }
-            (exceptionCode == 404) or (exceptionCode == 503) -> {
-                if(!url.contains("mobileconfigs")) {
+            (exceptionCode == HTTP_NOT_FOUND_404) -> {
+                if (!url.contains("mobileconfigs")) {
                     NetworkRuntimeExceptionViewController().openMaintenanceView()
+                }
+                true
+            }
+            exceptionResponse is NetworkErrorResponse -> {
+                val networkErrorResponse = exceptionResponse as NetworkErrorResponse
+                if (networkErrorResponse.httpCode == HTTP_SERVICE_UNAVAILABLE_503) {
+                    if (url.contains("mobileconfigs") || networkErrorResponse.redirectURL.isNullOrEmpty()) {
+                        NetworkRuntimeExceptionViewController().openMaintenanceView()
+                    } else {
+                        NetworkRuntimeExceptionViewController().openWebViewErrorScreen(
+                            networkErrorResponse.redirectURL)
+                    }
                 }
                 true
             }
