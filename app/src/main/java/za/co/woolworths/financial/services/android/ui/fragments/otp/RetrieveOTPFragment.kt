6622b997718e23f2dc4d55831d6810ecab1609f6
Jean-Paul Manuel <jean-paulm@yoyowallet.com>
2022-11-15 11:11:16 +0400

    Shop's Search Result adapter migrated to kotlin and view binding

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/otp/RetrieveOTPFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/otp/RetrieveOTPFragment.kt
@@ -1,17 +1,13 @@
 package za.co.woolworths.financial.services.android.ui.fragments.otp
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.awfs.coordination.R
-import kotlinx.android.synthetic.main.retrieve_otp_error_fragment.*
-import kotlinx.android.synthetic.main.retrieve_otp_fragment.*
+import com.awfs.coordination.databinding.RetrieveOtpFragmentBinding
 import za.co.woolworths.financial.services.android.contracts.IProgressAnimationState
 import za.co.woolworths.financial.services.android.contracts.IResponseListener
 import za.co.woolworths.financial.services.android.models.dto.npc.OTPMethodType
@@ -20,11 +16,12 @@ import za.co.woolworths.financial.services.android.models.network.CompletionHand
 import za.co.woolworths.financial.services.android.models.network.OneAppService
 import za.co.woolworths.financial.services.android.ui.extension.addFragment
 import za.co.woolworths.financial.services.android.ui.extension.findFragmentByTag
+import za.co.woolworths.financial.services.android.ui.fragments.account.applynow.utils.BaseFragmentBinding
 import za.co.woolworths.financial.services.android.ui.fragments.npc.ProgressStateFragment
 import za.co.woolworths.financial.services.android.util.BundleKeysConstants
 import za.co.woolworths.financial.services.android.util.Utils
 
-class RetrieveOTPFragment : Fragment(), IProgressAnimationState,View.OnClickListener {
+class RetrieveOTPFragment : BaseFragmentBinding<RetrieveOtpFragmentBinding>(RetrieveOtpFragmentBinding::inflate), IProgressAnimationState,View.OnClickListener {
 
     var navController: NavController? = null
     lateinit var absaCardToken: String
@@ -32,10 +29,6 @@ class RetrieveOTPFragment : Fragment(), IProgressAnimationState,View.OnClickList
     var bundle: Bundle? = null
     lateinit var otpMethodType: OTPMethodType
     var retrieveOTPResponse: RetrieveOTPResponse? = null
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.retrieve_otp_fragment, container, false)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,9 +43,9 @@ class RetrieveOTPFragment : Fragment(), IProgressAnimationState,View.OnClickList
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-        retry?.setOnClickListener(this)
-        needHelp?.setOnClickListener(this)
-        startProgress()
+        binding.errorView.retry.setOnClickListener(this)
+        binding.errorView.needHelp.setOnClickListener(this)
+        binding.startProgress()
         initRetrieveOTP(otpMethodType)
     }
 
@@ -99,26 +92,28 @@ class RetrieveOTPFragment : Fragment(), IProgressAnimationState,View.OnClickList
     private fun getProgressState(): ProgressStateFragment? = (activity as? AppCompatActivity)?.findFragmentByTag(ProgressStateFragment::class.java.simpleName) as? ProgressStateFragment
 
     private fun onRequestOTPRetry() {
-        errorView?.visibility = View.GONE
-        pageHeader?.visibility = View.VISIBLE
-        processingLayout?.visibility = View.VISIBLE
-        getProgressState()?.restartSpinning()
-        initRetrieveOTP(otpMethodType)
+        binding.apply {
+            errorView.root.visibility = View.GONE
+            pageHeader.visibility = View.VISIBLE
+            processingLayout.root.visibility = View.VISIBLE
+            getProgressState()?.restartSpinning()
+            initRetrieveOTP(otpMethodType)
+        }
     }
 
-    private fun startProgress() {
+    private fun RetrieveOtpFragmentBinding.startProgress() {
         (activity as? AppCompatActivity)?.addFragment(
-                fragment = ProgressStateFragment.newInstance(this),
+                fragment = ProgressStateFragment.newInstance(this@RetrieveOTPFragment),
                 tag = ProgressStateFragment::class.java.simpleName,
                 containerViewId = R.id.flProgressIndicator
         )
-        processingLayout?.visibility = View.VISIBLE
+        processingLayout.root.visibility = View.VISIBLE
     }
 
-    private fun showErrorView() {
-        pageHeader?.visibility = View.INVISIBLE
-        processingLayout?.visibility = View.GONE
-        errorView?.visibility = View.VISIBLE
+    private fun RetrieveOtpFragmentBinding.showErrorView() {
+        pageHeader.visibility = View.INVISIBLE
+        processingLayout.root.visibility = View.GONE
+        errorView.root.visibility = View.VISIBLE
     }
 
     override fun onClick(v: View?) {
@@ -131,7 +126,7 @@ class RetrieveOTPFragment : Fragment(), IProgressAnimationState,View.OnClickList
     override fun onAnimationEnd(isAnimationFinished: Boolean) {
         super.onAnimationEnd(isAnimationFinished)
         if(!isAnimationFinished){
-            showErrorView()
+            binding.showErrorView()
         }
     }
 }
