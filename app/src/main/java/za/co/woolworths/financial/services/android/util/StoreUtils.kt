68439433ecbece590efb9602c5356868f4abd49f
Udaykumar Kalluri <udayakumarKalluri@woolworths.co.za>
2022-11-22 10:02:02 +0530

    resolved the conflicts

--- app/src/main/java/za/co/woolworths/financial/services/android/util/StoreUtils.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/StoreUtils.kt
@@ -1,10 +1,12 @@
 package za.co.woolworths.financial.services.android.util
 
 import za.co.woolworths.financial.services.android.geolocation.network.model.Store
+import java.util.ArrayList
+import java.util.HashMap
 
 class StoreUtils {
     companion object {
-        const val PARGO: String = "Pargo"
+        const val PARGO : String = "Pargo"
 
         enum class StoreDeliveryType(val type: String) {
             OTHER("other"),
@@ -15,17 +17,13 @@ class StoreUtils {
         fun sortedStoreList(address: List<Store>?): List<Store> {
             val storeArrayList = ArrayList(address)
             val sortRoles: HashMap<String, Int> = hashMapOf(
-                StoreDeliveryType.OTHER.type.lowercase() to 0,
-                StoreDeliveryType.FOOD.type.lowercase() to 1,
-                StoreDeliveryType.FOOD_AND_OTHER.type.lowercase() to 2
+                    StoreDeliveryType.OTHER.type.lowercase() to 0,
+                    StoreDeliveryType.FOOD.type.lowercase() to 1,
+                    StoreDeliveryType.FOOD_AND_OTHER.type.lowercase() to 2
             )
             val comparator = Comparator { s1: Store, s2: Store ->
-                return@Comparator sortRoles[s2.storeDeliveryType?.lowercase()]?.let {
-                    sortRoles[s1.storeDeliveryType?.lowercase()]?.minus(
-                        it
-                    )
-                }
-                    ?: -1
+                return@Comparator sortRoles[s2.storeDeliveryType?.lowercase()]?.let { sortRoles[s1.storeDeliveryType?.lowercase()]?.minus(it) }
+                        ?: -1
             }
             val sortedStoreList = arrayListOf<Store>().apply { addAll(storeArrayList) }
             sortedStoreList.sortWith(comparator)
