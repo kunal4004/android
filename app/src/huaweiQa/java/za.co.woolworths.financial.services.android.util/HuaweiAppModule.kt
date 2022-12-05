765edeb5dbb86f6e24240dba4f3f5b6edb357a56
Jean-Paul Manuel <jean-paulm@yoyowallet.com>
2022-12-05 10:24:32 +0400

    Minor code cleanup

--- app/src/huaweiQa/java/za.co.woolworths.financial.services.android.util/HuaweiAppModule.kt
+++ app/src/huaweiQa/java/za.co.woolworths.financial.services.android.util/HuaweiAppModule.kt
@@ -3,12 +3,7 @@ package za.co.woolworths.financial.services.android.util
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-
-import javax.inject.Singleton
 import dagger.hilt.components.SingletonComponent
-import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.account_options.utils.IMyAccountsUtils
-import za.co.woolworths.financial.services.android.ui.fragments.account.main.ui.fragment.account_options.utils.MyAccountsUtils
-
 
 @Module
 @InstallIn(SingletonComponent::class)
