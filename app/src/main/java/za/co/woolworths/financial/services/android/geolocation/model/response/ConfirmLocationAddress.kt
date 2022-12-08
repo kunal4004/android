357621f0477fcfaf4e75a455da876d2d8abe6693
46964 <siddharajnirmale@woolworths.co.za>
2022-12-08 23:10:02 +0530

    APP2-198

--- app/src/main/java/za/co/woolworths/financial/services/android/geolocation/model/response/ConfirmLocationAddress.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/geolocation/model/response/ConfirmLocationAddress.kt
@@ -2,6 +2,6 @@ package za.co.woolworths.financial.services.android.geolocation.model.response
 
 data class ConfirmLocationAddress (
     val placeId: String?,
-    val nickname: String? = null,
+    val nickname: String? = "",
     val address2: String? = null
 )
