fe3ad2e16f95fba8198c3c89419e10aeee91084e
Sarith <ext_svasu@Falabella.cl>
2023-01-02 13:35:22 +0530

    added to new branch

--- app/src/main/java/za/co/woolworths/financial/services/android/util/CartUtils.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/CartUtils.kt
@@ -31,7 +31,9 @@ class CartUtils {
             productCountMap?.let {
                 if (it.quantityLimit?.foodLayoutColour != null && showBanner && it.totalProductCount ?: 0 > 0) {
                     message?.text = it.quantityLimit.foodLayoutMessage ?: ""
-                    counter?.text = it.totalProductCount.toString() + "/" + it.quantityLimit.foodMaximumQuantity?:""
+                    if(it.quantityLimit.other!=null && it.totalProductCount !=null && it.totalProductCount>it.quantityLimit.other) {
+                        counter?.text = ((it.totalProductCount - it.quantityLimit.other).toString() + "/" + it.quantityLimit.foodMaximumQuantity) ?: ""
+                    }
                     if (it.quantityLimit.foodLayoutColour.isNotEmpty() && !it.quantityLimit.foodLayoutMessage.isNullOrEmpty()) {
                         banner?.visibility = View.VISIBLE
                         banner?.setBackgroundColor(Color.parseColor(it.quantityLimit.foodLayoutColour))
