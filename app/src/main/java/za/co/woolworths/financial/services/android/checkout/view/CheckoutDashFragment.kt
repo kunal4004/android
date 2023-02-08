b1f142c0c15cf80f93188f76cf6a8c3042734180
AC62703 <anandchincholkar@woolworths.co.za>
2023-02-08 20:38:15 +0530

    condiions updated for checkout screen

--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutDashFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutDashFragment.kt
@@ -836,26 +836,29 @@ class CheckoutDashFragment : Fragment(R.layout.fragment_checkout_returning_user_
                 deliveringToAddress.append(defaultAddressNickname)
 
                 // Extract default address display name
-                savedAddresses.addresses?.forEach { address ->
-                    if (savedAddresses.defaultAddressNickname.equals(address.nickname)) {
-                        this.defaultAddress = address
-                        suburbId = address.suburbId ?: ""
-                        placesId = address?.placesId
-                        storeId = address?.storeId
-                        nickName = address?.nickname
-                        val addressName = SpannableString(address.address1)
-                        val typeface1 =
-                            ResourcesCompat.getFont(context, R.font.myriad_pro_regular)
-                        addressName.setSpan(
-                            StyleSpan(typeface1!!.style),
-                            0, addressName.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                        deliveringToAddress.append(addressName)
-                        return@forEach
+
+                    savedAddresses.addresses?.forEach list@{ address ->
+                        if (savedAddresses.defaultAddressNickname.equals(address.nickname)) {
+                            this.defaultAddress = address
+                            suburbId = address.suburbId ?: ""
+                            placesId = address?.placesId
+                            storeId = address?.storeId
+                            nickName = address?.nickname
+                            val addressName = SpannableString(address.address1)
+                            val typeface1 =
+                                ResourcesCompat.getFont(context, R.font.myriad_pro_regular)
+                            addressName.setSpan(
+                                StyleSpan(typeface1!!.style),
+                                0, addressName.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+
+                            deliveringToAddress.append(addressName)
+                            return@list
+                        }
                     }
-                    if (savedAddresses.defaultAddressNickname.isNullOrEmpty()) {
-                        binding.checkoutCollectingFromLayout?.root?.visibility = GONE
-                    }
+
+                if (savedAddresses.defaultAddressNickname.isNullOrEmpty()) {
+                    binding.checkoutCollectingFromLayout?.root?.visibility = GONE
                 }
                 binding.checkoutCollectingFromLayout.tvNativeCheckoutDeliveringValue?.text =
                     deliveringToAddress
