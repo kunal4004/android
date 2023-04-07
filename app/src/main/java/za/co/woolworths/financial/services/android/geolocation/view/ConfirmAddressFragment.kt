e31d76fd01201534618ac040572a19a8c4f95939
Udaykumar Kalluri <udayakumarKalluri@woolworths.co.za>
2023-04-07 20:02:24 +0530

    Removed the unnecessary apply.

--- app/src/main/java/za/co/woolworths/financial/services/android/geolocation/view/ConfirmAddressFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/geolocation/view/ConfirmAddressFragment.kt
@@ -283,9 +283,7 @@ class ConfirmAddressFragment : Fragment(R.layout.confirm_address_bottom_sheet_di
     override fun onAddressSelected(address: Address, position: Int) {
         selectedAddress = address
         mPosition = position
-        binding.currentLocDiv.apply {
-            visibility = if (position == 0) View.INVISIBLE else View.VISIBLE
-        }
+        binding.currentLocDiv.visibility = if (position == 0) View.INVISIBLE else View.VISIBLE
         binding.setButtonUI(true)
         if (address.verified) {
             selectedAddress?.apply {
