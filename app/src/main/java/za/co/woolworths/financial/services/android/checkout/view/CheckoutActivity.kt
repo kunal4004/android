f5b11cdad1a95c3f0e90d299f735dd2a2888d5bd
46964 <siddharajnirmale@woolworths.co.za>
2023-04-18 22:48:07 +0530

    APP2-909 removed unused variable

--- app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutActivity.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/checkout/view/CheckoutActivity.kt
@@ -47,7 +47,6 @@ class CheckoutActivity : AppCompatActivity(), View.OnClickListener {
     private var whoIsCollectingString: String? = null
     private var isComingFromCnc: Boolean? = false
     private var mSavedAddressPosition = 0
-    var isEditAddressScreenNeeded = true
     var cartItemList:ArrayList<CommerceItem>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +56,6 @@ class CheckoutActivity : AppCompatActivity(), View.OnClickListener {
         setActionBar()
         intent?.extras?.apply {
             savedAddressResponse = getSerializable(SAVED_ADDRESS_KEY) as? SavedAddressResponse
-            isEditAddressScreenNeeded = getBoolean(CheckoutAddressConfirmationFragment.IS_EDIT_ADDRESS_SCREEN, false)
             geoSlotSelection = getBoolean(GEO_SLOT_SELECTION, false)
             dashSlotSelection = getBoolean(DASH_SLOT_SELECTION, false)
             cartItemList = getSerializable(CheckoutAddressManagementBaseFragment.CART_ITEM_LIST) as? ArrayList<CommerceItem>?
