539cab402598bf95dd1ca30d38ca51e9a1e15f77
KU59319 <>
2023-04-20 19:41:52 +0530

    No code Changes. Only Lint Issue resolved.

--- app/src/test/java/za/co/woolworths/financial/services/android/enhancedSubstitution/viewmodel/ProductSubstitutionViewModelTest.kt
+++ app/src/test/java/za/co/woolworths/financial/services/android/enhancedSubstitution/viewmodel/ProductSubstitutionViewModelTest.kt
@@ -3,11 +3,11 @@ package za.co.woolworths.financial.services.android.enhancedSubstitution.viewmod
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.awfs.coordination.R
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -15,9 +15,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
-import za.co.woolworths.financial.services.android.enhancedSubstitution.EnhanceSubstitutonHelperTest
+import za.co.woolworths.financial.services.android.enhancedSubstitution.EnhanceSubstitutionHelperTest
 import za.co.woolworths.financial.services.android.enhancedSubstitution.apihelper.SubstitutionApiHelperTest
 import za.co.woolworths.financial.services.android.enhancedSubstitution.getOrAwaitValue
 import za.co.woolworths.financial.services.android.enhancedSubstitution.model.AddSubstitutionRequest
@@ -27,9 +27,10 @@ import za.co.woolworths.financial.services.android.enhancedSubstitution.reposito
 import za.co.woolworths.financial.services.android.models.dto.SkusInventoryForStoreResponse
 import za.co.woolworths.financial.services.android.models.network.Resource
 
+@ExperimentalCoroutinesApi
 class ProductSubstitutionViewModelTest {
 
-    private val testDispathcer = StandardTestDispatcher()
+    private val testDispatcher = StandardTestDispatcher()
 
     @Mock
     private lateinit var productSubstitutionRepository: ProductSubstitutionRepository
@@ -49,86 +50,106 @@ class ProductSubstitutionViewModelTest {
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
-        Dispatchers.setMain(testDispathcer)
+        Dispatchers.setMain(testDispatcher)
     }
 
     @Test
-    fun  test_emptylist_getSubstitutions() = runTest{
-        Mockito.`when`(productSubstitutionRepository
-                .getProductSubstitution("6009195203504")).thenReturn(Resource.success(productSubstitution))
+    fun test_emptyList_getSubstitutions() = runTest {
+        `when`(
+            productSubstitutionRepository
+                .getProductSubstitution("6009195203504")
+        ).thenReturn(Resource.success(productSubstitution))
 
         val sut = ProductSubstitutionViewModel(repository = productSubstitutionRepository)
         sut.getProductSubstitution("6009195203504")
-        testDispathcer.scheduler.advanceUntilIdle()
+        testDispatcher.scheduler.advanceUntilIdle()
         val result = sut.productSubstitution.getOrAwaitValue()
         Assert.assertEquals(0, result.peekContent().data?.data?.size)
     }
 
     @Test
-    fun test_error_getSubstitutions() = runTest{
-        Mockito.`when`(productSubstitutionRepository
-                .getProductSubstitution("6009195203504")).thenReturn(Resource.error(R.string.error_unknown, null))
+    fun test_error_getSubstitutions() = runTest {
+        `when`(
+            productSubstitutionRepository
+                .getProductSubstitution("6009195203504")
+        ).thenReturn(Resource.error(R.string.error_unknown, null))
 
         val sut = ProductSubstitutionViewModel(repository = productSubstitutionRepository)
         sut.getProductSubstitution("6009195203504")
-        testDispathcer.scheduler.advanceUntilIdle()
+        testDispatcher.scheduler.advanceUntilIdle()
         val result = sut.productSubstitution.getOrAwaitValue()
-        Assert.assertEquals(R.string.error_unknown , result.peekContent().message)
+        Assert.assertEquals(R.string.error_unknown, result.peekContent().message)
     }
 
     @Test
-    fun  test_EmptyResponse_getInventory() = runTest{
-        Mockito.`when`(productSubstitutionRepository
-                .getInventoryForSubstitution("473","6001009025692")).thenReturn(Resource.success(skusInventoryForStoreResponse))
+    fun test_EmptyResponse_getInventory() = runTest {
+        `when`(
+            productSubstitutionRepository
+                .getInventoryForSubstitution("473", "6001009025692")
+        ).thenReturn(Resource.success(skusInventoryForStoreResponse))
 
         val sut = ProductSubstitutionViewModel(repository = productSubstitutionRepository)
-        sut.getInventoryForSubstitution("473","6001009025692")
-        testDispathcer.scheduler.advanceUntilIdle()
+        sut.getInventoryForSubstitution("473", "6001009025692")
+        testDispatcher.scheduler.advanceUntilIdle()
         val result = sut.inventorySubstitution.getOrAwaitValue()
         Assert.assertEquals(skusInventoryForStoreResponse, result.peekContent().data)
         Assert.assertEquals(null, result.peekContent().data?.storeId)
     }
 
     @Test
-    fun test_error_getInventory() = runTest{
-        Mockito.`when`(productSubstitutionRepository
-                .getInventoryForSubstitution("473","6001009025692")).thenReturn(Resource.error(R.string.error_unknown, null))
+    fun test_error_getInventory() = runTest {
+        `when`(
+            productSubstitutionRepository
+                .getInventoryForSubstitution("473", "6001009025692")
+        ).thenReturn(Resource.error(R.string.error_unknown, null))
 
         val sut = ProductSubstitutionViewModel(repository = productSubstitutionRepository)
-        sut.getInventoryForSubstitution("473","6001009025692")
-        testDispathcer.scheduler.advanceUntilIdle()
+        sut.getInventoryForSubstitution("473", "6001009025692")
+        testDispatcher.scheduler.advanceUntilIdle()
         val result = sut.inventorySubstitution.getOrAwaitValue()
-        Assert.assertEquals(R.string.error_unknown , result.peekContent().message)
+        Assert.assertEquals(R.string.error_unknown, result.peekContent().message)
     }
 
 
     @Test
-    fun  test_EmptyResponse_addSubstitution() = runTest{
-        val addSubstitutionRequest = AddSubstitutionRequest(SubstitutionApiHelperTest.USER_CHOICE, EnhanceSubstitutonHelperTest.SUBSTITUTION_ID, EnhanceSubstitutonHelperTest.COMMARCE_ITEM_ID)
+    fun test_EmptyResponse_addSubstitution() = runTest {
+        val addSubstitutionRequest = AddSubstitutionRequest(
+            SubstitutionApiHelperTest.USER_CHOICE,
+            EnhanceSubstitutionHelperTest.SUBSTITUTION_ID,
+            EnhanceSubstitutionHelperTest.COMMARCE_ITEM_ID
+        )
 
-        Mockito.`when`(productSubstitutionRepository
-                .addSubstitution(addSubstitutionRequest)).thenReturn(Resource.success(addSubstitutionResponse))
+        `when`(
+            productSubstitutionRepository
+                .addSubstitution(addSubstitutionRequest)
+        ).thenReturn(Resource.success(addSubstitutionResponse))
 
         val sut = ProductSubstitutionViewModel(repository = productSubstitutionRepository)
         sut.addSubstitutionForProduct(addSubstitutionRequest)
-        testDispathcer.scheduler.advanceUntilIdle()
+        testDispatcher.scheduler.advanceUntilIdle()
         val result = sut.addSubstitutionResponse.getOrAwaitValue()
         Assert.assertEquals(addSubstitutionResponse, result.peekContent().data)
         Assert.assertEquals(0, result.peekContent().data?.data?.size)
     }
 
     @Test
-    fun test_error_addSubstitution() = runTest{
-        val addSubstitutionRequest = AddSubstitutionRequest(SubstitutionApiHelperTest.USER_CHOICE, EnhanceSubstitutonHelperTest.SUBSTITUTION_ID, EnhanceSubstitutonHelperTest.COMMARCE_ITEM_ID)
+    fun test_error_addSubstitution() = runTest {
+        val addSubstitutionRequest = AddSubstitutionRequest(
+            SubstitutionApiHelperTest.USER_CHOICE,
+            EnhanceSubstitutionHelperTest.SUBSTITUTION_ID,
+            EnhanceSubstitutionHelperTest.COMMARCE_ITEM_ID
+        )
 
-        Mockito.`when`(productSubstitutionRepository
-                .addSubstitution(addSubstitutionRequest)).thenReturn(Resource.error(R.string.error_unknown, null))
+        `when`(
+            productSubstitutionRepository
+                .addSubstitution(addSubstitutionRequest)
+        ).thenReturn(Resource.error(R.string.error_unknown, null))
 
         val sut = ProductSubstitutionViewModel(repository = productSubstitutionRepository)
         sut.addSubstitutionForProduct(addSubstitutionRequest)
-        testDispathcer.scheduler.advanceUntilIdle()
+        testDispatcher.scheduler.advanceUntilIdle()
         val result = sut.addSubstitutionResponse.getOrAwaitValue()
-        Assert.assertEquals(R.string.error_unknown , result.peekContent().message)
+        Assert.assertEquals(R.string.error_unknown, result.peekContent().message)
     }
 
     @After
