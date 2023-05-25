1dd73a9cc2b105d5075cae2bf0d7fa01de4a0530
Udaykumar Kalluri <udayakumarKalluri@woolworths.co.za>
2023-05-25 13:59:11 +0530

    Added Got It button feature in PayFlex Bottom sheet.

---
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/payflex/PayFlexBottomSheetDialog.kt
@@ -0,0 +1,39 @@
+package za.co.woolworths.financial.services.android.ui.fragments.payflex
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.awfs.coordination.databinding.PayflexBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import za.co.woolworths.financial.services.android.ui.views.actionsheet.WBottomSheetDialogFragment
+import za.co.woolworths.financial.services.android.util.AppConstant
+
+
+class PayFlexBottomSheetDialog : WBottomSheetDialogFragment() {
+
+    private lateinit var binding: PayflexBottomSheetBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = PayflexBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val bottomSheetDialog = dialog as BottomSheetDialog
+        val bottomSheetBehavior = bottomSheetDialog.behavior
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.isDraggable = false
+        var maxHeight: Int = (resources.displayMetrics.heightPixels * 0.75).toInt()
+        binding?.payFlexWebView?.layoutParams?.height = maxHeight
+        binding?.payFlexWebView?.settings?.javaScriptEnabled = true
+        binding?.payFlexWebView?.loadUrl(AppConstant.PAYFLEX_POP_UP_URL)
+        binding?.gotIt?.setOnClickListener {
+            dismiss()
+        }
+    }
+}
