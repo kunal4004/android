bd9e9eb0ffd886cd4ba2b818f75306969bc2b0e8
Udaykumar Kalluri <udayakumarKalluri@woolworths.co.za>
2023-05-26 08:44:38 +0530

    Fixed the scrolling issues.

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/payflex/PayFlexBottomSheetDialog.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/payflex/PayFlexBottomSheetDialog.kt
@@ -1,5 +1,6 @@
 package za.co.woolworths.financial.services.android.ui.fragments.payflex
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import za.co.woolworths.financial.services.android.util.AppConstant
 
 class PayFlexBottomSheetDialog : WBottomSheetDialogFragment() {
     private lateinit var binding: PayflexBottomSheetBinding
+    private var mCurrentWebViewScrollY = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -24,17 +26,42 @@ class PayFlexBottomSheetDialog : WBottomSheetDialogFragment() {
         val bottomSheetDialog = dialog as BottomSheetDialog
         val bottomSheetBehavior = bottomSheetDialog.behavior
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-        bottomSheetBehavior.isDraggable = false
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_DRAGGING && mCurrentWebViewScrollY > 0) {
+                    // this is where we check if webview can scroll up or not and based on that we let BottomSheet close on scroll down
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+                } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    dismiss()
+                }
+            }
+        })
+
         binding.apply {
             val maxHeight: Int = (resources.displayMetrics.heightPixels * 0.8).toInt()
             payFlexWebView.apply {
                 layoutParams.height = maxHeight
-                settings.javaScriptEnabled = true
                 loadUrl(AppConstant.PAYFLEX_POP_UP_URL)
             }
             gotIt.setOnClickListener {
                 dismiss()
             }
         }
+        addOnScrollToWebview()
+    }
+
+
+    private fun addOnScrollToWebview() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.payFlexWebView.setOnScrollChangeListener { view, scrollX, scrollY, oldScrollX, oldScrollY ->
+                mCurrentWebViewScrollY = scrollY
+            }
+        }
     }
 }
+
+
