77fdcbb52919eb0e3bc1c289b6613a0f1ca95d8e
KU59319 <>
2023-05-31 16:14:25 +0530

    unused imports

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/dashboard/BottomNavigationActivity.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/activities/dashboard/BottomNavigationActivity.java
@@ -105,7 +105,6 @@ import za.co.woolworths.financial.services.android.ui.base.BaseActivity;
 import za.co.woolworths.financial.services.android.ui.base.SavedInstanceFragment;
 import za.co.woolworths.financial.services.android.ui.fragments.RefinementDrawerFragment;
 import za.co.woolworths.financial.services.android.ui.fragments.account.AccountMasterCache;
-import za.co.woolworths.financial.services.android.ui.fragments.account.MyAccountsFragment;
 import za.co.woolworths.financial.services.android.ui.fragments.account.chat.ChatAWSAmplify;
 import za.co.woolworths.financial.services.android.ui.fragments.account.chat.helper.AmplifyInit;
 import za.co.woolworths.financial.services.android.ui.fragments.product.detail.updated.ProductDetailsFragment;
