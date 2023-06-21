5c0ccd6fea4f14dfa4d6fbbfa154ede281ff653f
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2023-06-21 13:42:44 +0530

    Created generic function for repeated code logic.

--- app/src/main/java/za/co/woolworths/financial/services/android/domain/usecase/AddToListByOrderIdUC.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/domain/usecase/AddToListByOrderIdUC.kt
@@ -1,14 +1,12 @@
 package za.co.woolworths.financial.services.android.domain.usecase
 
-import com.awfs.coordination.R
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
 import za.co.woolworths.financial.services.android.domain.repository.MyListRepository
 import za.co.woolworths.financial.services.android.models.dto.OrderToListReponse
 import za.co.woolworths.financial.services.android.models.dto.OrderToShoppingListRequestBody
 import za.co.woolworths.financial.services.android.models.network.Resource
-import java.io.IOException
+import za.co.woolworths.financial.services.android.models.network.convertToResource
 import javax.inject.Inject
 
 class AddToListByOrderIdUC @Inject constructor(
@@ -19,19 +17,12 @@ class AddToListByOrderIdUC @Inject constructor(
         orderId: String,
         body: OrderToShoppingListRequestBody
     ): Flow<Resource<OrderToListReponse>> = flow {
-        try {
-            emit(Resource.loading(null))
-            val result = myListRepository.addProductsToListByOrderId(
+        convertToResource {
+            myListRepository.addProductsToListByOrderId(
                 orderId, body
             )
-            if (result.isSuccessful && result.body() != null) {
-                emit(Resource.success(result.body()))
-            } else
-                emit(Resource.error(R.string.error_occured, result.body()))
-        } catch (e: HttpException) {
-            emit(Resource.error(R.string.error_occured, null))
-        } catch (e: IOException) {
-            emit(Resource.error(R.string.error_internet_connection, null))
+        }.collect {
+            emit(it)
         }
     }
 }
