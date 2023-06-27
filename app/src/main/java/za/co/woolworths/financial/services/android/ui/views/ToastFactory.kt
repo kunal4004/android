1f93cc7541fe6ce8e34f0677095c2ca64c9363dc
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2023-06-27 11:06:48 +0530

    Removed unused toast builder.

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/views/ToastFactory.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/views/ToastFactory.kt
@@ -26,10 +26,7 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.core.text.buildSpannedString
 import com.awfs.coordination.R
 import com.awfs.coordination.databinding.LayoutSnackbarAddToListBinding
-import com.google.gson.JsonArray
-import com.google.gson.JsonElement
 import com.google.gson.JsonObject
-import com.google.gson.JsonParser
 import kotlinx.coroutines.GlobalScope
 import za.co.woolworths.financial.services.android.contracts.IToastInterface
 import za.co.woolworths.financial.services.android.models.WoolworthsApplication
@@ -55,80 +52,6 @@ class ToastFactory {
         private const val POPUP_DELAY_MILLIS_15000 = 15000
         private const val POPUP_3000_DELAY_MILLIS: Long = 3000
         private const val POPUP_MARGIN_BOTTOM = 25
-
-        fun buildShoppingListToast(
-            activity: Activity,
-            viewLocation: View,
-            buttonIsVisible: Boolean,
-            data: Intent?,
-            toastInterface: IToastInterface
-        ): PopupWindow? {
-            val context = WoolworthsApplication.getAppContext()
-
-            val shoppingList = ""
-//                data?.getStringExtra(AddToShoppingListFragment.POST_ADD_TO_SHOPPING_LIST)
-            var shoppingListObject: JsonObject? = null
-            var shoppingListArray: JsonArray? = null
-
-            shoppingList?.let {
-                val element = JsonParser().parse(it)
-                element?.let { item ->
-                    when (element) {
-                        is JsonObject -> shoppingListObject = item as? JsonObject
-                        is JsonArray -> shoppingListArray = item as? JsonArray
-                    }
-                }
-            }
-
-            // inflate your xml layout
-            val inflater =
-                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as? LayoutInflater
-            val layout = inflater?.inflate(R.layout.add_to_cart_success, null)
-            // set the custom display
-            val tvButtonClick = layout?.findViewById<WTextView>(R.id.tvView)
-            val tvBoldTitle = layout?.findViewById<WTextView>(R.id.tvCart)
-            val tvAddedTo = layout?.findViewById<WTextView>(R.id.tvAddToCart)
-            // initialize your popupWindow and use your custom layout as the view
-            val popupWindow = PopupWindow(
-                layout,
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT, true
-            )
-
-            tvButtonClick?.visibility = if (buttonIsVisible) VISIBLE else GONE
-            tvBoldTitle?.visibility = VISIBLE
-            tvAddedTo?.isAllCaps = true
-
-            shoppingListObject?.let {
-                when (it.size()) {
-                    1 -> {
-                        var shoppingList: JsonElement? = null
-                        it.entrySet()?.forEach { item -> shoppingList = item.value }
-                        tvBoldTitle?.setText(shoppingList?.asJsonObject?.get("name")?.asString)
-                    }
-                    else -> tvBoldTitle?.setText(
-                        context.getString(R.string.shopping_list).plus("s")
-                    )
-                }
-            }
-
-            // handle popupWindow click event
-            tvButtonClick?.setOnClickListener {
-                toastInterface.onToastButtonClicked(shoppingListObject ?: shoppingListArray)
-                popupWindow.dismiss() // dismiss the window
-            }
-
-            // dismiss the popup window after 3sec
-            popupWindow.isFocusable = false
-            Handler().postDelayed({ popupWindow.dismiss() }, POPUP_DELAY_MILLIS.toLong())
-            popupWindow.showAtLocation(
-                viewLocation,
-                Gravity.BOTTOM,
-                0,
-                convertDpToPixel(getDeviceHeight(activity), context)
-            )
-            return popupWindow
-        }
 
         fun buildPushNotificationAlertToast(
             activity: Activity,
