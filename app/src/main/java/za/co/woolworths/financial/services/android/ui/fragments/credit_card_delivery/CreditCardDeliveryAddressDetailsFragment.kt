4441cff961325470d6e39163a20527edfda65563
Dimitri Jacques <dimitri.jacques@yoyowallet.com>
2023-07-04 15:33:18 +0400

    OWT-1412 - Clear details textfield overlayed "next" image button.

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/credit_card_delivery/CreditCardDeliveryAddressDetailsFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/credit_card_delivery/CreditCardDeliveryAddressDetailsFragment.kt
@@ -1,10 +1,8 @@
 package za.co.woolworths.financial.services.android.ui.fragments.credit_card_delivery
 
-import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
@@ -94,7 +92,6 @@ class CreditCardDeliveryAddressDetailsFragment : CreditCardDeliveryBaseFragment(
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.submitAddress -> {
-                hideSoftKeyboard(requireActivity(), v)
                 binding.onSubmit()
             }
             R.id.clearDetails -> {
@@ -199,9 +196,4 @@ class CreditCardDeliveryAddressDetailsFragment : CreditCardDeliveryBaseFragment(
         }
     }
 
-
-    fun hideSoftKeyboard(activity: Activity, view: View) {
-        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.applicationWindowToken, 0)
-    }
 }
