6ec125d5bf66ac7ee6e80d7f5c4e06d03d52f488
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2023-07-11 18:24:47 +0530

    Updated pagesize value to 60.

--- app/src/main/java/za/co/woolworths/financial/services/android/models/SearchApiSettings.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/models/SearchApiSettings.kt
@@ -2,8 +2,9 @@ package za.co.woolworths.financial.services.android.models
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import za.co.woolworths.financial.services.android.util.Utils
 
 @Parcelize
 data class SearchApiSettings(
-    val pageSize: Int = 60
+    val pageSize: Int = Utils.PAGE_SIZE
 ): Parcelable
