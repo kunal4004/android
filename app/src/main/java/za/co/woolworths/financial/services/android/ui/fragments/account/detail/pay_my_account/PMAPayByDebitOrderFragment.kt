85b248491babebb2d875ed7409affe93934b3f37
dsofttech <dynamicsofttechnology@gmail.com>
2023-07-19 07:53:47 +0100

    ticket OWT-1440 "move hardcoded text in pay by debit order to config" completed

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/detail/pay_my_account/PMAPayByDebitOrderFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/account/detail/pay_my_account/PMAPayByDebitOrderFragment.kt
@@ -26,7 +26,11 @@ class PMAPayByDebitOrderFragment : Fragment() {
             false,
         )
         binding.payByDebitOrderModel =
+<<<<<<< HEAD
             AppConfigSingleton.mPayMyAccount?.debitOrder?.let { PMAPayByDebitOrderEntity(it) }
+=======
+            AppConfigSingleton.mPayMyAccount?.debitOrder?.let { ConfigPMAPayByDebitOrderEntity(it) }
+>>>>>>> d85a8729d3 (ticket OWT-1440 "move hardcoded text in pay by debit order to config" completed)
                 ?.toDomain()
         binding.executePendingBindings()
         return binding.root
