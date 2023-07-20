868af3df89cbd63159ad2ee120e418f29fc654a3
Rikin <rikinprajapati@woolworths.co.za>
2023-07-20 18:18:53 +0530

    APP3-395, Recommendations in the Shopping list detail page - Created a new class to load the recommendation fragment in other fragments - Updated the recommendation request data classes to support more dynamic request for the shopping list recommendation request - Added the recommendation ui component in the shopping list details screen - Added new unit test cases and updated the existing as per the code changes

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/listitems/ShoppingListDetailFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/shoppinglist/listitems/ShoppingListDetailFragment.kt
@@ -48,6 +48,11 @@ import za.co.woolworths.financial.services.android.models.dto.SkusInventoryForSt
 import za.co.woolworths.financial.services.android.models.network.CompletionHandler
 import za.co.woolworths.financial.services.android.models.network.OneAppService
 import za.co.woolworths.financial.services.android.models.network.Status
+import za.co.woolworths.financial.services.android.recommendations.data.response.request.Product
+import za.co.woolworths.financial.services.android.recommendations.data.response.request.Recommendation
+import za.co.woolworths.financial.services.android.recommendations.presentation.RecommendationLoader
+import za.co.woolworths.financial.services.android.recommendations.presentation.RecommendationLoaderImpl
+import za.co.woolworths.financial.services.android.recommendations.presentation.RecommendationLoadingNotifier
 import za.co.woolworths.financial.services.android.ui.activities.CustomPopUpWindow
 import za.co.woolworths.financial.services.android.ui.activities.dashboard.BottomNavigationActivity
 import za.co.woolworths.financial.services.android.ui.activities.product.ProductSearchActivity
@@ -58,6 +63,10 @@ import za.co.woolworths.financial.services.android.ui.adapters.ShoppingListItems
 import za.co.woolworths.financial.services.android.ui.fragments.product.detail.IOnConfirmDeliveryLocationActionListener
 import za.co.woolworths.financial.services.android.ui.fragments.product.detail.dialog.ConfirmDeliveryLocationFragment
 import za.co.woolworths.financial.services.android.ui.fragments.product.detail.updated.ProductDetailsFragment
+import za.co.woolworths.financial.services.android.ui.fragments.product.shop.usecase.Constants.EVENT_PAGE_TYPE_SHOPPING_LIST
+import za.co.woolworths.financial.services.android.ui.fragments.product.shop.usecase.Constants.EVENT_TYPE_PAGEVIEW
+import za.co.woolworths.financial.services.android.ui.fragments.product.shop.usecase.Constants.EVENT_TYPE_PRODUCT_THUMBNAIL_VIEW
+import za.co.woolworths.financial.services.android.ui.fragments.product.shop.usecase.Constants.EVENT_URL_SHOPPING_LIST
 import za.co.woolworths.financial.services.android.ui.fragments.shoppinglist.search.SearchResultFragment
 import za.co.woolworths.financial.services.android.ui.fragments.shoppinglist.search.SearchResultFragment.Companion.ADDED_TO_SHOPPING_LIST_RESULT_CODE
 import za.co.woolworths.financial.services.android.ui.fragments.shoppinglist.search.SearchResultFragment.Companion.MY_LIST_LIST_ID
@@ -70,6 +79,7 @@ import za.co.woolworths.financial.services.android.ui.views.ToastFactory.Compani
 import za.co.woolworths.financial.services.android.util.AppConstant
 import za.co.woolworths.financial.services.android.util.AppConstant.Companion.HTTP_OK
 import za.co.woolworths.financial.services.android.util.AppConstant.Companion.HTTP_SESSION_TIMEOUT_440
+import za.co.woolworths.financial.services.android.util.BundleKeysConstants
 import za.co.woolworths.financial.services.android.util.CustomTypefaceSpan
 import za.co.woolworths.financial.services.android.util.EmptyCartView
 import za.co.woolworths.financial.services.android.util.EmptyCartView.EmptyCartInterface
@@ -90,7 +100,7 @@ import za.co.woolworths.financial.services.android.util.wenum.Delivery
 @AndroidEntryPoint
 class ShoppingListDetailFragment : Fragment(), View.OnClickListener, EmptyCartInterface,
     NetworkChangeListener, ToastInterface, ShoppingListItemsNavigator, IToastInterface,
-    IOnConfirmDeliveryLocationActionListener {
+    IOnConfirmDeliveryLocationActionListener, RecommendationLoadingNotifier, RecommendationLoader by RecommendationLoaderImpl() {
 
     private val viewModel: ShoppingListDetailViewModel by viewModels()
 
@@ -180,6 +190,7 @@ class ShoppingListDetailFragment : Fragment(), View.OnClickListener, EmptyCartIn
         // Shopping List items
         viewModel.shoppListDetails.observe(viewLifecycleOwner) {
             val response = it.peekContent().data
+            setRecommendationDividerVisibility(visibility = false)
             when (it.peekContent().status) {
                 Status.LOADING -> {
                     mErrorHandlerView?.hideErrorHandler()
@@ -383,6 +394,7 @@ class ShoppingListDetailFragment : Fragment(), View.OnClickListener, EmptyCartIn
 
                 setUpView()
                 shoppingListItemsAdapter?.setList(viewModel.mShoppingListItems)
+                showRecommendedProducts(viewModel.mShoppingListItems)
             }
             HTTP_SESSION_TIMEOUT_440 -> SessionUtilities.getInstance().setSessionState(
                 SessionDao.SESSION_STATE.INACTIVE,
@@ -393,6 +405,24 @@ class ShoppingListDetailFragment : Fragment(), View.OnClickListener, EmptyCartIn
                 onShoppingListItemsResponseError(shoppingListItemsResponse)
             }
         }
+    }
+
+    private fun showRecommendedProducts(shoppingListItems: ArrayList<ShoppingListItem>) {
+        val products = shoppingListItems.filter { !it.productId.isNullOrEmpty() }.map { Product(productId = it.productId) }
+        if(products.isEmpty()) {
+            return
+        }
+        val bundle = Bundle()
+        bundle.putParcelable(
+            BundleKeysConstants.RECOMMENDATIONS_EVENT_DATA,
+            Recommendation.PageView(eventType = EVENT_TYPE_PAGEVIEW, url = EVENT_URL_SHOPPING_LIST, pageType = EVENT_PAGE_TYPE_SHOPPING_LIST)
+        )
+        bundle.putParcelable(
+            BundleKeysConstants.RECOMMENDATIONS_EVENT_DATA_TYPE,
+            Recommendation.ShoppingListEvent(eventType = EVENT_TYPE_PRODUCT_THUMBNAIL_VIEW, products = products)
+        )
+        bundle.putBoolean(BundleKeysConstants.RECOMMENDATIONS_DYNAMIC_TITLE_REQUIRED, true)
+        loadRecommendations(bundle = bundle, fragmentManager = childFragmentManager)
     }
 
     private fun onShoppingListItemsResponseError(response: ShoppingListItemsResponse?) {
@@ -1030,6 +1060,16 @@ class ShoppingListDetailFragment : Fragment(), View.OnClickListener, EmptyCartIn
     override fun onDestroy() {
         (activity as? BottomNavigationActivity)?.showToolbar()
         super.onDestroy()
+    }
+
+    override fun onRecommendationsLoadedSuccessfully() {
+        if(isAdded) {
+            setRecommendationDividerVisibility(visibility = !shoppingListItemsAdapter?.shoppingListItems.isNullOrEmpty())
+        }
+    }
+
+    private fun setRecommendationDividerVisibility(visibility: Boolean) {
+        bindingListDetails.viewRecommendationDivider.visibility = if(visibility) VISIBLE else GONE
     }
 
     companion object {
