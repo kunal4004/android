d771b97ca03565667afe61f534ce9836f8141bff
KU59319 <>
2023-08-03 12:17:02 +0530

    removal of dismiss button from success popup

--- app/src/main/java/za/co/woolworths/financial/services/android/geolocation/view/DeliveryAddressConfirmationFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/geolocation/view/DeliveryAddressConfirmationFragment.kt
@@ -740,7 +740,7 @@ class DeliveryAddressConfirmationFragment : Fragment(R.layout.geo_location_deliv
                 R.string.no_location_desc,
                 R.string.change_location,
                 R.drawable.location_disabled,
-                null)
+                resources.getString(R.string.cancel_underline_html))
         } else {
             if (customBottomSheetDialogFragment != null && customBottomSheetDialogFragment!!.isVisible) {
                 customBottomSheetDialogFragment!!.dismiss()
@@ -767,7 +767,7 @@ class DeliveryAddressConfirmationFragment : Fragment(R.layout.geo_location_deliv
                     R.string.no_location_desc,
                     R.string.change_location,
                     R.drawable.ic_cnc_set_location,
-                    null)
+                    resources.getString(R.string.cancel_underline_html))
             } else {
                 if (customBottomSheetDialogFragment != null && customBottomSheetDialogFragment!!.isVisible) {
                     customBottomSheetDialogFragment?.dismiss()
@@ -801,7 +801,7 @@ class DeliveryAddressConfirmationFragment : Fragment(R.layout.geo_location_deliv
                 R.string.no_location_desc,
                 R.string.change_location,
                 R.drawable.location_disabled,
-                null)
+                resources.getString(R.string.cancel_underline_html))
         } else {
             if (customBottomSheetDialogFragment != null && customBottomSheetDialogFragment!!.isVisible) {
                 customBottomSheetDialogFragment?.dismiss()
--- app/src/main/java/za/co/woolworths/financial/services/android/util/KotlinUtils.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/KotlinUtils.kt
@@ -723,7 +723,7 @@ class KotlinUtils {
                     dialogSubTitle,
                     dialogBtnText,
                     dialogTitleImg,
-                    null
+                    context.resources.getString(R.string.cancel_underline_html)
                 )
             customBottomSheetDialogFragment.show(
                 requireFragmentManager,
