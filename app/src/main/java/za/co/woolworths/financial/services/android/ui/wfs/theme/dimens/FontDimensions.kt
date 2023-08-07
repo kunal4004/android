585e1d2cc1cfcfd0815ae25d4d9f6a9f6d128b71
Jean-Paul Manuel <jean-paulm@yoyowallet.com>
2023-08-07 07:20:00 +0000

    Revert "Revert "OWT-1425 + OWT-1475 - August release (pull request #5325)" (pull request #5373)"

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/wfs/theme/dimens/FontDimensions.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/wfs/theme/dimens/FontDimensions.kt
@@ -36,6 +36,12 @@ val sw360FontDimensions by lazy {
 val sw400FontDimensions by lazy {
     FontDimensions()
 }
+val sw420FontDimensions by lazy {
+    FontDimensions(
+        policyNumberValue15Sp = 14.sp
+
+    )
+}
 
 val sw440FontDimensions by lazy {
     FontDimensions(
