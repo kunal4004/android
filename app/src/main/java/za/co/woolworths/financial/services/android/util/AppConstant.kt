e1051dca2e9fc470e39180799d08f3bbb8347f86
Udaykumar Kalluri <udayakumarKalluri@woolworths.co.za>
2023-09-26 20:30:52 +0530

    Addressed the PR comments and taken pull from dev.

--- app/src/main/java/za/co/woolworths/financial/services/android/util/AppConstant.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/util/AppConstant.kt
@@ -146,9 +146,6 @@ class AppConstant {
         //connect online
         const val PRODUCT_TYPE_DIGITAL = "DIGITAL"
         const val SA_MOBILE_NUMBER_PATTERN="^0\\d{9}$"
-
-
-
     }
 
     class Keys {
