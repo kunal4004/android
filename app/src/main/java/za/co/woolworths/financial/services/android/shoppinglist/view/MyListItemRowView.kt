df2621eadefe1b852e722851b7a55d9392b43e43
Nikesh Mahajan <NikeshVasantrao@woolworths.co.za>
2023-10-12 14:50:02 +0530

    Added spacing in between List name and List items icon

--- app/src/main/java/za/co/woolworths/financial/services/android/shoppinglist/view/MyListItemRowView.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/shoppinglist/view/MyListItemRowView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -94,6 +95,9 @@ fun MyListItemRowView(
                 )
             }
         }
+
+        Spacer(modifier = Modifier.height(15.dp))
+
         ListOfImagesView(listItem, onImageItemClick = {})
     }
 }
