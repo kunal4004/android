35d73f4be5f2f470c30f6083c501f9d54fdb9d74
Dimitri Jacques <dimitri.jacques@yoyogroup.com>
2023-10-16 06:19:42 +0400

    Converting files to Kotlin with safe renaming.

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/wreward/WRewardsVouchersFragment.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/wreward/WRewardsVouchersFragment.kt
@@ -144,7 +144,6 @@ public class WRewardsVouchersFragment extends Fragment {
                 } else {
                     startVoucherDetailsActivity();
                 }
-
             }
 
             @Override
