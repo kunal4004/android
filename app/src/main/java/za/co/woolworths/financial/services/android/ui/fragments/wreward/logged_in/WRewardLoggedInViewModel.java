0ea60c2148a978094d44079f02ec6852665c9eb6
Jyotsna <jyotsnar@yoyowallet.com>
2023-11-09 12:50:31 +0400

    [OWT-1660] Biometric on WRewards Voucher screen

--- app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/wreward/logged_in/WRewardLoggedInViewModel.java
+++ app/src/main/java/za/co/woolworths/financial/services/android/ui/fragments/wreward/logged_in/WRewardLoggedInViewModel.java
@@ -1,9 +1,29 @@
 package za.co.woolworths.financial.services.android.ui.fragments.wreward.logged_in;
 
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.os.Build;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.renderscript.Allocation;
+import androidx.renderscript.Element;
+import androidx.renderscript.RenderScript;
+import androidx.renderscript.ScriptIntrinsicBlur;
+
+import com.awfs.coordination.R;
+
 import za.co.woolworths.financial.services.android.ui.base.BaseViewModel;
 import za.co.woolworths.financial.services.android.util.rx.SchedulerProvider;
 
 public class WRewardLoggedInViewModel extends BaseViewModel<WRewardLoggedInViewModel> {
+
+	float blurRadius = (float) 25.0;
 	public WRewardLoggedInViewModel() {
 		super();
 	}
@@ -11,4 +31,66 @@ public class WRewardLoggedInViewModel extends BaseViewModel<WRewardLoggedInViewM
 	public WRewardLoggedInViewModel(SchedulerProvider schedulerProvider) {
 		super(schedulerProvider);
 	}
+
+	public void setScreenBlurEnabled(Fragment fragment) {
+		View view = fragment.getView();
+		View viewToBlur = view.findViewById(R.id.wrewardsViewPager);
+		ImageView backgroundImage = view.findViewById(R.id.blurEffectImage);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+			viewToBlur.setRenderEffect(RenderEffect.createBlurEffect(blurRadius, blurRadius, Shader.TileMode.CLAMP));
+			viewToBlur.invalidate();
+		}
+		else {
+			Bitmap contentBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+			Canvas canvas = new Canvas(contentBitmap);
+			viewToBlur.draw(canvas);
+
+			// Apply a blur effect to the captured bitmap
+			Bitmap blurredBitmap = applyBlur(fragment.getActivity(), contentBitmap);
+
+			backgroundImage.setImageBitmap(blurredBitmap);
+			backgroundImage.setVisibility(View.VISIBLE);
+			backgroundImage.invalidate();
+			view.invalidate();
+			viewToBlur.invalidate();
+		}
+	}
+
+
+	private Bitmap applyBlur(FragmentActivity fragmentActivity, Bitmap inputBitmap) {
+		RenderScript renderScript = RenderScript.create(fragmentActivity);
+		Allocation input = Allocation.createFromBitmap(renderScript, inputBitmap);
+		Allocation output = Allocation.createTyped(renderScript, input.getType());
+
+		ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+		blurScript.setInput(input);
+		blurScript.setRadius(blurRadius);
+		blurScript.forEach(output);
+		output.copyTo(inputBitmap);
+
+		input.destroy();
+		output.destroy();
+		renderScript.destroy();
+
+		return inputBitmap;
+	}
+
+	public void setScreenBlurDisabled(Fragment fragment) {
+		View view = fragment.getView();
+		View viewToBlur = view.findViewById(R.id.wrewardsViewPager);
+		ImageView backgroundImage = view.findViewById(R.id.blurEffectImage);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+			viewToBlur.setRenderEffect(null);
+		}
+		else{
+			backgroundImage.setVisibility(View.GONE);
+			backgroundImage.invalidate();
+		}
+
+		viewToBlur.invalidate();
+		view.invalidate();
+	}
+
 }
