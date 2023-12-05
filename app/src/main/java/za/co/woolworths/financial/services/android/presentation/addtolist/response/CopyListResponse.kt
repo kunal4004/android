45857198c35ee240b69739f95085955971e5d58b
AC62703 <anandchincholkar@woolworths.co.za>
2023-12-05 11:28:01 +0530

    api end point change for add to list

--- app/src/main/java/za/co/woolworths/financial/services/android/presentation/addtolist/response/CopyListResponse.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/presentation/addtolist/response/CopyListResponse.kt
@@ -1,4 +1,8 @@
 package za.co.woolworths.financial.services.android.presentation.addtolist.response
 
-class CopyListResponse {
-}
+import za.co.woolworths.financial.services.android.models.dto.Response
+
+data class CopyListResponse (
+    val httpCode: Int,
+    val response: Response
+)
