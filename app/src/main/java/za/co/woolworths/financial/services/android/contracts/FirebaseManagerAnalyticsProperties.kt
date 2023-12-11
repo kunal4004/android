508700c7fa2625b1e20f939d2aa4f4a08bbe9f73
Rikin <rikinprajapati@woolworths.co.za>
2023-12-11 19:42:20 +0530

    APP3-2007 - new constant added for the view search result event

--- app/src/main/java/za/co/woolworths/financial/services/android/contracts/FirebaseManagerAnalyticsProperties.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/contracts/FirebaseManagerAnalyticsProperties.kt
@@ -586,6 +586,7 @@ open class FirebaseManagerAnalyticsProperties {
             const val DELIVERY_TYPE = "delivery_type"
 
             const val SEARCH_RESULT_COUNT: String = "search_result_count"
+            const val SEARCH_COUNT: String = "search_count"
             const val LOCATION_ID: String = "location_id"
             const val PRODUCT_NAME: String = "product_name"
         }
