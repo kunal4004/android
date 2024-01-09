9587e3a1ebf860bd796b660f9e6bfb388f40a1ba
AC62703 <anandchincholkar@woolworths.co.za>
2024-01-09 15:50:42 +0530

    share icon hide from list

--- app/src/main/java/za/co/woolworths/financial/services/android/shoppinglist/view/MyListItemRowView.kt
+++ app/src/main/java/za/co/woolworths/financial/services/android/shoppinglist/view/MyListItemRowView.kt
@@ -39,6 +39,7 @@ fun MyListItemRowView(
     modifier: Modifier = Modifier,
     listDataState: ListDataState,
     listItem: ShoppingList,
+    isShareButtonVisible: Boolean = false,
     onDeleteIconClick: (item: ShoppingList) -> Unit,
     onShareIconClick: (item: ShoppingList) -> Unit,
     onDetailsArrowClick: (item: ShoppingList) -> Unit,
@@ -86,15 +87,18 @@ fun MyListItemRowView(
                             }
                     )
                 } else {
-                    Icon(painter = painterResource(id = listDataState.shareIcon),
-                        contentDescription = "Share List",
-                        modifier = Modifier
-                            .padding(5.dp, 0.dp, 7.dp, 0.dp)
-                            .size(16.dp)
-                            .clickable {
-                                onShareIconClick(listItem)
-                            }
-                    )
+                    if(isShareButtonVisible) {
+                        /* will remove later once share list available*/
+                        Icon(painter = painterResource(id = listDataState.shareIcon),
+                            contentDescription = "Share List",
+                            modifier = Modifier
+                                .padding(5.dp, 0.dp, 7.dp, 0.dp)
+                                .size(16.dp)
+                                .clickable {
+                                    onShareIconClick(listItem)
+                                }
+                        )
+                    }
 
                     Icon(
                         painter = painterResource(id = listDataState.openIcon),
